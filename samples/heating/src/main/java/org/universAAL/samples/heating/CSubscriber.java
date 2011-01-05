d20fa57d9029271737215955355cf1c74365f998
Saied Tazari <saied.tazari@igd.fraunhofer.de>
2011-01-05 16:26:09 +0000

    Prepared for further development in universAAL.

---
+++ samples/heating/src/main/java/org/universAAL/samples/heating/CSubscriber.java
@@ -0,0 +1,48 @@
+package org.universAAL.samples.heating;
+
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.context.rdf.ContextEvent;
+import org.universAAL.middleware.context.rdf.ContextEventPattern;
+import org.universAAL.middleware.context.ContextSubscriber;
+import org.universAAL.middleware.owl.Restriction;
+import org.universAAL.ontology.weather.TempSensor;
+
+public class CSubscriber extends ContextSubscriber {
+
+	protected CSubscriber(BundleContext context,
+			ContextEventPattern[] initialSubscriptions) {
+		super(context, getPermanentSubscriptions());
+		// TODO Auto-generated constructor stub
+	}
+
+	public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void handleContextEvent(ContextEvent event) {
+		System.out.println("");
+
+		System.out.println("----------------sub=" + event.getSubjectURI()
+				+ "\n pred=" + event.getRDFPredicate()
+				+ "\n ****THE TEMPERATURE IS :**** obj=" + event.getRDFObject()
+				+ "\n **** extra=" + event.getTimestamp());
+
+	}
+
+	// only one conctructor usually there are 2
+
+	private static ContextEventPattern[] getPermanentSubscriptions() {
+		ContextEventPattern[] ceps = new ContextEventPattern[1];
+
+		ceps[0] = new ContextEventPattern();
+		ceps[0].addRestriction(Restriction.getAllValuesRestriction(
+				ContextEvent.PROP_RDF_SUBJECT, TempSensor.MY_URI));
+		ceps[0].addRestriction(Restriction
+				.getFixedValueRestriction(ContextEvent.PROP_RDF_PREDICATE,
+						TempSensor.PROP_MEASURED_VALUE));
+
+		return ceps;
+	}
+
+}
