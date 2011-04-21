2e1cf525c2da492a7edd4d87402e448ec8b98cce
mr Andrej Grguric <andrej.grguric@ericsson.com>
2011-04-21 16:02:38 +0000

    javadoc added

--- bus.io/src/main/java/org/universAAL/middleware/io/owl/PrivacyLevel.java
+++ bus.io/src/main/java/org/universAAL/middleware/io/owl/PrivacyLevel.java
@@ -23,142 +23,208 @@ import org.universAAL.middleware.owl.ComparableIndividual;
 import org.universAAL.middleware.owl.ManagedIndividual;
 
 /**
+ * Defines privacy levels that can be: insensible, known_people_only,
+ * intimates_only, home_mates_only, personal
+ * 
  * @author mtazari
  * 
+ * @see org.universAAL.middleware.owl.ComparableIndividual
  */
 public class PrivacyLevel extends ComparableIndividual {
-	public static final String MY_URI;
-	static {
-		MY_URI = uAAL_VOCABULARY_NAMESPACE + "PrivacyLevel";
-		register(PrivacyLevel.class);
+    public static final String MY_URI;
+    static {
+	MY_URI = uAAL_VOCABULARY_NAMESPACE + "PrivacyLevel";
+	register(PrivacyLevel.class);
+    }
+
+    public static final int INSENSIBLE = 0;
+    public static final int KNOWN_PEOPLE_ONLY = 1;
+    public static final int INTIMATES_ONLY = 2;
+    public static final int HOME_MATES_ONLY = 3;
+    public static final int uAALL = 4;
+
+    private static final String[] names = { "insensible", "known_people_only",
+	    "intimates_only", "home_mates_only", "personal" };
+
+    public static final PrivacyLevel personal = new PrivacyLevel(uAALL);
+    public static final PrivacyLevel homeMatesOnly = new PrivacyLevel(
+	    HOME_MATES_ONLY);
+    public static final PrivacyLevel intimatesOnly = new PrivacyLevel(
+	    INTIMATES_ONLY);
+    public static final PrivacyLevel knownPeopleOnly = new PrivacyLevel(
+	    KNOWN_PEOPLE_ONLY);
+    public static final PrivacyLevel insensible = new PrivacyLevel(INSENSIBLE);
+
+    /**
+     * Returns the list of all class members guaranteeing that no other members
+     * will be created after a call to this method.
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#
+     */
+    public static ManagedIndividual[] getEnumerationMembers() {
+	return new ManagedIndividual[] { personal, homeMatesOnly,
+		intimatesOnly, knownPeopleOnly, insensible };
+    }
+
+    /**
+     * Returns the privacy level with the given URI.
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getIndividualByURI(String)
+     */
+    public static ManagedIndividual getIndividualByURI(String instanceURI) {
+	return (instanceURI != null && instanceURI
+		.startsWith(uAAL_VOCABULARY_NAMESPACE)) ? valueOf(instanceURI
+		.substring(uAAL_VOCABULARY_NAMESPACE.length())) : null;
+    }
+
+    /**
+     * 
+     * @return max value of privacy level
+     */
+    public static PrivacyLevel getMaxValue() {
+	return personal;
+    }
+
+    /**
+     * 
+     * @return min value of privacy level
+     */
+    public static PrivacyLevel getMinValue() {
+	return insensible;
+    }
+
+    /**
+     * 
+     * @param order
+     *            order
+     * @return privacy level based on received order
+     */
+    public static PrivacyLevel getLevelByOrder(int order) {
+	switch (order) {
+	case INSENSIBLE:
+	    return insensible;
+	case KNOWN_PEOPLE_ONLY:
+	    return knownPeopleOnly;
+	case INTIMATES_ONLY:
+	    return intimatesOnly;
+	case HOME_MATES_ONLY:
+	    return homeMatesOnly;
+	case uAALL:
+	    return personal;
+	default:
+	    return null;
 	}
+    }
 
-	public static final int INSENSIBLE = 0;
-	public static final int KNOWN_PEOPLE_ONLY = 1;
-	public static final int INTIMATES_ONLY = 2;
-	public static final int HOME_MATES_ONLY = 3;
-	public static final int uAALL = 4;
+    /**
+     * Returns the value of the property <code>rdfs:comment</code> on this
+     * <code>owl:Class</code> from the underlying ontology.
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getRDFSComment()
+     */
+    public static String getRDFSComment() {
+	return "An enumeration for specifying the privacy level of information.";
+    }
 
-	private static final String[] names = { "insensible", "known_people_only",
-			"intimates_only", "home_mates_only", "personal" };
+    /**
+     * Returns the value of the property <code>rdfs:label</code> on this
+     * <code>owl:Class</code> from the underlying ontology.
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getResourceLabel()
+     */
+    public static String getRDFSLabel() {
+	return "Privacy Level";
+    }
 
-	public static final PrivacyLevel personal = new PrivacyLevel(uAALL);
-	public static final PrivacyLevel homeMatesOnly = new PrivacyLevel(
-			HOME_MATES_ONLY);
-	public static final PrivacyLevel intimatesOnly = new PrivacyLevel(
-			INTIMATES_ONLY);
-	public static final PrivacyLevel knownPeopleOnly = new PrivacyLevel(
-			KNOWN_PEOPLE_ONLY);
-	public static final PrivacyLevel insensible = new PrivacyLevel(INSENSIBLE);
+    /**
+     * 
+     * @param name
+     *            privacy level name
+     * @return privacy level
+     */
+    public static final PrivacyLevel valueOf(String name) {
+	for (int i = INSENSIBLE; i <= uAALL; i++)
+	    if (names[i].equals(name))
+		return getLevelByOrder(i);
+	return null;
+    }
 
-	/**
-	 * Returns the list of all class members guaranteeing that no other members
-	 * will be created after a call to this method.
-	 */
-	public static ManagedIndividual[] getEnumerationMembers() {
-		return new ManagedIndividual[] { personal, homeMatesOnly,
-				intimatesOnly, knownPeopleOnly, insensible };
-	}
+    private int order;
 
-	/**
-	 * Returns the privacy level with the given URI.
-	 */
-	public static ManagedIndividual getIndividualByURI(String instanceURI) {
-		return (instanceURI != null && instanceURI
-				.startsWith(uAAL_VOCABULARY_NAMESPACE)) ? valueOf(instanceURI
-				.substring(uAAL_VOCABULARY_NAMESPACE.length())) : null;
-	}
+    /**
+     * Usage of default constructor is prevented
+     */
+    private PrivacyLevel() {
 
-	public static PrivacyLevel getMaxValue() {
-		return personal;
-	}
+    }
 
-	public static PrivacyLevel getMinValue() {
-		return insensible;
-	}
+    /**
+     * Constructor receives privacy level order
+     * 
+     * @param order
+     *            order
+     */
+    private PrivacyLevel(int order) {
+	super(uAAL_VOCABULARY_NAMESPACE + names[order]);
+	this.order = order;
+    }
 
-	public static PrivacyLevel getLevelByOrder(int order) {
-		switch (order) {
-		case INSENSIBLE:
-			return insensible;
-		case KNOWN_PEOPLE_ONLY:
-			return knownPeopleOnly;
-		case INTIMATES_ONLY:
-			return intimatesOnly;
-		case HOME_MATES_ONLY:
-			return homeMatesOnly;
-		case uAALL:
-			return personal;
-		default:
-			return null;
-		}
-	}
+    /**
+     * @see org.universAAL.middleware.owl.ComparableIndividual
+     */
+    public int compareTo(Object other) {
+	return (this == other) ? 0
+		: (order < ((PrivacyLevel) other).order) ? -1 : 1;
+    }
 
-	/**
-	 * Returns the value of the property <code>rdfs:comment</code> on this
-	 * <code>owl:Class</code> from the underlying ontology.
-	 */
-	public static String getRDFSComment() {
-		return "An enumeration for specifying the privacy level of information.";
-	}
+    /**
+     * Returns with next privacy level
+     */
+    public ComparableIndividual getNext() {
+	return getLevelByOrder(order + 1);
+    }
 
-	/**
-	 * Returns the value of the property <code>rdfs:label</code> on this
-	 * <code>owl:Class</code> from the underlying ontology.
-	 */
-	public static String getRDFSLabel() {
-		return "Privacy Level";
-	}
+    /**
+     * Returns with previuos privacy level
+     */
+    public ComparableIndividual getPrevious() {
+	return getLevelByOrder(order - 1);
+    }
 
-	public static final PrivacyLevel valueOf(String name) {
-		for (int i = INSENSIBLE; i <= uAALL; i++)
-			if (names[i].equals(name))
-				return getLevelByOrder(i);
-		return null;
-	}
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType(String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
 
-	private int order;
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#isWellFormed()
+     */
+    public boolean isWellFormed() {
+	return true;
+    }
 
-	// prevent the usage of the default constructor
-	private PrivacyLevel() {
+    /**
+     * @return privacy level name based on order defined at the time of
+     *         construction
+     */
+    public String name() {
+	return names[order];
+    }
 
-	}
+    /**
+     * @return order defined at the time of construction
+     */
+    public int ord() {
+	return order;
+    }
 
-	private PrivacyLevel(int order) {
-		super(uAAL_VOCABULARY_NAMESPACE + names[order]);
-		this.order = order;
-	}
-
-	public int compareTo(Object other) {
-		return (this == other) ? 0
-				: (order < ((PrivacyLevel) other).order) ? -1 : 1;
-	}
-
-	public ComparableIndividual getNext() {
-		return getLevelByOrder(order + 1);
-	}
-
-	public ComparableIndividual getPrevious() {
-		return getLevelByOrder(order - 1);
-	}
-
-	public int getPropSerializationType(String propURI) {
-		return PROP_SERIALIZATION_OPTIONAL;
-	}
-
-	public boolean isWellFormed() {
-		return true;
-	}
-
-	public String name() {
-		return names[order];
-	}
-
-	public int ord() {
-		return order;
-	}
-
-	public void setProperty(String propURI, Object o) {
-		// do nothing
-	}
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#setProperty(String,
+     *      Object)
+     */
+    public void setProperty(String propURI, Object o) {
+	// do nothing
+    }
 }
