38af02d87f56bc22da28e8c256f06929df87fde6
Michele Girolami <michele.girolami@isti.cnr.it>
2011-05-31 08:56:27 +0000

    Addded javadoc comments

--- acl.upnp/src/main/java/org/universAAL/middleware/acl/upnp/exporter/services/SodaPopPeerService.java
+++ acl.upnp/src/main/java/org/universAAL/middleware/acl/upnp/exporter/services/SodaPopPeerService.java
@@ -37,7 +37,9 @@ import org.universAAL.middleware.acl.upnp.exporter.stateVariables.PeerIDStateVar
 
 import org.universAAL.middleware.acl.SodaPopPeer;
 
-/* 
+/**
+ * Concrete implementation of the UPnPService for the SodaPopPeerService. This class will be used by the SodaPopProxy in order to 
+ * invoke the UPnP action. 
 * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
 */
 
