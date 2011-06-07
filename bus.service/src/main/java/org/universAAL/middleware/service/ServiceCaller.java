d5efdb6665fa91fe2005fa6d1d13da17905de3f3
Mr. Ivailo Bakalov <i.bakalov@prosyst.com>
2011-06-07 14:10:00 +0000

    javadoc added

--- bus.service/src/main/java/org/universAAL/middleware/service/ServiceCaller.java
+++ bus.service/src/main/java/org/universAAL/middleware/service/ServiceCaller.java
@@ -32,6 +32,10 @@ import org.universAAL.middleware.sodapop.msg.MessageType;
 
 
 /**
+/**
+ * This is an abstract class that the service caller members of the service bus must derive from.
+ * Any <code>ServiceCaller</code> must incorporate the logic of handling service responses
+ * into the implementation of the abstract method <code>handleResponse(String, ServiceResponse)</code>.
  * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied Tazari</a>
  *
  */
@@ -40,6 +44,12 @@ public abstract class ServiceCaller implements Caller {
 	private Hashtable waitingCalls, readyResponses;
 	private String myID;
 	
+	/**
+	 * The default constructor for this class.
+	 * @param context The OSGI bundle context where the ServiceBus is registered. Note that if no service bus is 
+	 * registered within the passed <code>BundleContext</code> at the time of creation, this object will not be operational.
+	 * @param realizedServices The initial set of services that are realized by this callee.
+	 */
 	protected ServiceCaller(BundleContext context) {
 		waitingCalls = new Hashtable();
 		readyResponses = new Hashtable();
@@ -50,6 +60,10 @@ public abstract class ServiceCaller implements Caller {
 		myID = bus.register(this);
 	}
 
+	
+	/* (non-Javadoc)
+	 * @see org.universAAL.middleware.sodapop.BusMember#busDyingOut(org.universAAL.middleware.sodapop.Bus)
+	 */
 	public final void busDyingOut(Bus b) {
 		if (b == bus)
 			communicationChannelBroken();
@@ -75,10 +89,16 @@ public abstract class ServiceCaller implements Caller {
 		return sr;
 	}
 
+	/**
+	 * Unregisters this <code>ServiceCaller</code> from the bus.
+	 */
 	public void close(){
 		bus.unregister(myID, this);
 	}
 
+	/**
+	 * This abstract method is called for each member of the bus when the bus is being stopped.
+	 */
 	public abstract void communicationChannelBroken();
 	
 	public final void handleReply(Message m) {
@@ -120,24 +140,48 @@ public abstract class ServiceCaller implements Caller {
 		return reqMsg.getID();
 	}
 	
+	/**
+	 * A method used to instantly retrieve all services available to this caller.
+	 * @return all available services.
+	 */
 	public ServiceProfile[] getAllServices() {
 		return bus.getAllServices(myID);
 	}
 	
+	/**
+	 * A method used to retrieve a specified service, available to this <code>ServiceCaller</code>.
+	 * @param s the desired service.
+	 * @return the service that is available, or null if no such service is available.
+	 */
 	public ServiceProfile[] getMatchingService(Service s) {
 		return bus.getMatchingService(myID, s);
 	}
 	
+	/**
+	 * A method used to retrieve a service available to this caller, that matches the specified keywords.
+	 * @param keywords the keywords that should be matched by the service.
+	 * @return the service that matches the keywords, or null if no such service is available.
+	 */
 	public ServiceProfile[] getMatchingService(String[] keywords) {
 		return bus.getMatchingService(myID, keywords);
 	}
 	
+	/**
+	 * Adds an availability subscription, in other words a listener, to receive events about the availability of a specified service. 
+	 * @param subscriber the object which will receive events when the appropriate service registers or unregisters.
+	 * @param request the request that describes the desired service.
+	 */
 	public void addAvailabilitySubscription(
 			AvailabilitySubscriber subscriber,
 			ServiceRequest request) {
 		bus.addAvailabilitySubscription(myID, subscriber, request);
 	}
 
+	/**
+	 * Removes an availability subscription for this <code>Caller</code>, which was previously added using <code>addAvailabilitySubscription</code> method.
+	 * @param subscriber the object which used to receive the events.
+	 * @param requestURI the service that was being monitored.
+	 */
 	public void removeAvailabilitySubscription(
 			AvailabilitySubscriber subscriber,
 			String requestURI) {
