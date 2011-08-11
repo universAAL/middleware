894fa8d4a0278603f2b982f2644919f85bf0bce7
Vadim Eisenberg <vadime@il.ibm.com>
2011-08-11 15:51:46 +0000

    testing the pre-commit hook

--- bus.model/src/main/java/org/universAAL/middleware/sodapop/impl/SodaPopImpl.java
+++ bus.model/src/main/java/org/universAAL/middleware/sodapop/impl/SodaPopImpl.java
@@ -42,7 +42,7 @@
  *		- wrapped log service in logger util (TODO service unregistering)
  *		- added sybchronizzation between noticeBusesToPeer and Join
  */
-// Vadim - testing pre-commit hook, adding ".osgi" string, take 4
+// Vadim - testing pre-commit hook, adding ".osgi" string, take 5
 package org.universAAL.middleware.sodapop.impl;
 
 import java.util.Enumeration;
