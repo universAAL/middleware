cd872abda297be3a22c031a5c7d30d4cdff21d91
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-03-08 16:56:05 +0000

    Automatic tag of mw.bus.service version: 1.1.0

---
+++ mw.bus.service/src/main/java/org/universAAL/middleware/service/ServiceCaller.java
@@ -0,0 +1,233 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut für Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.service;
+
+import java.util.Hashtable;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.service.impl.ServiceBusImpl;
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.middleware.sodapop.Bus;
+import org.universAAL.middleware.sodapop.Caller;
+import org.universAAL.middleware.sodapop.msg.Message;
+import org.universAAL.middleware.sodapop.msg.MessageType;
+
+/**
+ * /** This is an abstract class that the service caller members of the service
+ * bus must derive from. Any <code>ServiceCaller</code> must incorporate the
+ * logic of handling service responses into the implementation of the abstract
+ * method <code>handleResponse(String, ServiceResponse)</code>.
+ * 
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public abstract class ServiceCaller implements Caller {
+    private ServiceBus bus;
+    private ModuleContext thisCallerContext;
+    private Hashtable waitingCalls, readyResponses;
+    private String myID, localID;
+
+    /**
+     * The default constructor for this class.
+     * 
+     * @param context
+     *            The OSGI bundle context where the ServiceBus is registered.
+     *            Note that if no service bus is registered within the passed
+     *            <code>BundleContext</code> at the time of creation, this
+     *            object will not be operational.
+     * @param realizedServices
+     *            The initial set of services that are realized by this callee.
+     */
+    protected ServiceCaller(ModuleContext context) {
+	waitingCalls = new Hashtable();
+	readyResponses = new Hashtable();
+
+	thisCallerContext = context;
+	bus = (ServiceBus) context.getContainer().fetchSharedObject(context,
+		ServiceBusImpl.busFetchParams);
+	myID = bus.register(this);
+	localID = myID.substring(myID.lastIndexOf('#') + 1);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.universAAL.middleware.sodapop.BusMember#busDyingOut(org.universAAL
+     * .middleware.sodapop.Bus)
+     */
+    public final void busDyingOut(Bus b) {
+	if (b == bus)
+	    communicationChannelBroken();
+    }
+
+    /**
+     * The "normal" (synchronous) way of calling a service. Use
+     * {@link #sendRequest(ServiceRequest)}, if you want to handle the response
+     * asynchronously in another thread.
+     */
+    public final ServiceResponse call(ServiceRequest request) {
+	ServiceResponse sr = null;
+	synchronized (waitingCalls) {
+	    String callID = sendRequest(request);
+	    waitingCalls.put(callID, this);
+	    while (sr == null) {
+		try {
+		    waitingCalls.wait();
+		    sr = (ServiceResponse) readyResponses.remove(callID);
+		} catch (InterruptedException e) {
+		}
+	    }
+	}
+	return sr;
+    }
+
+    /**
+     * Unregisters this <code>ServiceCaller</code> from the bus.
+     */
+    public void close() {
+	bus.unregister(myID, this);
+    }
+
+    /**
+     * This abstract method is called for each member of the bus when the bus is
+     * being stopped.
+     */
+    public abstract void communicationChannelBroken();
+
+    public final void handleReply(Message m) {
+	if (m.getType() == MessageType.reply
+		&& (m.getContent() instanceof ServiceResponse)) {
+	    LogUtils.logInfo(thisCallerContext, ServiceCaller.class,
+		    "handleReply", new Object[] { localID,
+			    " received service response:\n",
+			    m.getContentAsString() }, null);
+	    String reqID = m.getInReplyTo();
+	    synchronized (waitingCalls) {
+		if (waitingCalls.remove(reqID) == null)
+		    handleResponse(reqID, (ServiceResponse) m.getContent());
+		else {
+		    readyResponses.put(reqID, m.getContent());
+		    waitingCalls.notifyAll();
+		}
+	    }
+	}
+    }
+
+    /**
+     * Will be called automatically in a new thread whenever the response
+     * corresponding to a previous call to {@link #sendRequest(ServiceRequest)}
+     * is ready.
+     * 
+     * @param reqID
+     *            the ID returned by the previous call to
+     *            {@link #sendRequest(ServiceRequest)}.
+     * @param response
+     *            the expected response.
+     */
+    public abstract void handleResponse(String reqID, ServiceResponse response);
+
+    /**
+     * To be used if the caller would like to handle the reply asynchronously
+     * within the method {@link #handleResponse(String, ServiceResponse)}, which
+     * will automatically be called in another thread when the response is
+     * ready.
+     * 
+     * @return a unique ID for this request that will be passed to the method
+     *         {@link #handleResponse(String, ServiceResponse)} for an
+     *         unambiguous mapping of responses to requests.
+     */
+    public final String sendRequest(ServiceRequest request) {
+	request.setProperty(ServiceRequest.PROP_uAAL_SERVICE_CALLER, myID);
+	ServiceBusImpl.assessContentSerialization(request);
+	Message reqMsg = new Message(MessageType.request, request);
+	bus.sendMessage(myID, reqMsg);
+	return reqMsg.getID();
+    }
+
+    /**
+     * A method used to instantly retrieve all services available to this
+     * caller.
+     * 
+     * @return all available services.
+     */
+    public ServiceProfile[] getAllServices() {
+	return bus.getAllServices(myID);
+    }
+
+    /**
+     * A method used to retrieve a specified service, available to this
+     * <code>ServiceCaller</code>.
+     * 
+     * @param s
+     *            the desired service.
+     * @return the service that is available, or null if no such service is
+     *         available.
+     */
+    public ServiceProfile[] getMatchingService(Service s) {
+	return bus.getMatchingService(myID, s);
+    }
+
+    /**
+     * A method used to retrieve a service available to this caller, that
+     * matches the specified keywords.
+     * 
+     * @param keywords
+     *            the keywords that should be matched by the service.
+     * @return the service that matches the keywords, or null if no such service
+     *         is available.
+     */
+    public ServiceProfile[] getMatchingService(String[] keywords) {
+	return bus.getMatchingService(myID, keywords);
+    }
+
+    /**
+     * Adds an availability subscription, in other words a listener, to receive
+     * events about the availability of a specified service.
+     * 
+     * @param subscriber
+     *            the object which will receive events when the appropriate
+     *            service registers or unregisters.
+     * @param request
+     *            the request that describes the desired service.
+     */
+    public void addAvailabilitySubscription(AvailabilitySubscriber subscriber,
+	    ServiceRequest request) {
+	bus.addAvailabilitySubscription(myID, subscriber, request);
+    }
+
+    /**
+     * Removes an availability subscription for this <code>Caller</code>, which
+     * was previously added using <code>addAvailabilitySubscription</code>
+     * method.
+     * 
+     * @param subscriber
+     *            the object which used to receive the events.
+     * @param requestURI
+     *            the service that was being monitored.
+     */
+    public void removeAvailabilitySubscription(
+	    AvailabilitySubscriber subscriber, String requestURI) {
+	bus.removeAvailabilitySubscription(myID, subscriber, requestURI);
+    }
+}
---
+++ mw.bus.service/src/main/java/org/universAAL/middleware/service/owls/process/ProcessResult.java
@@ -0,0 +1,321 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut für Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.service.owls.process;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.universAAL.middleware.owl.TypeURI;
+import org.universAAL.middleware.rdf.PropertyPath;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.AggregatingFilter;
+
+/**
+ * This class represents ProcessResult of OWL-S -
+ * http://www.daml.org/services/owl-s/1.1/Process.owl#Result
+ * 
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public class ProcessResult extends Resource {
+    public static final String PROP_OWLS_RESULT_HAS_EFFECT = ProcessOutput.OWLS_PROCESS_NAMESPACE
+	    + "hasEffect";
+    public static final String PROP_OWLS_RESULT_WITH_OUTPUT = ProcessOutput.OWLS_PROCESS_NAMESPACE
+	    + "withOutput";
+    public static final String TYPE_OWLS_RESULT = ProcessOutput.OWLS_PROCESS_NAMESPACE
+	    + "Result";
+
+    /**
+     * Verify that the process effects of offers match the ones of requests
+     * according to the context
+     * 
+     * @param req
+     *            - a list of requests
+     * @param offer
+     *            - a list of offers
+     * @param context
+     *            - the context
+     * @return true iff the process effects match
+     */
+    public static boolean checkEffects(Resource[] req, Resource[] offer,
+	    Hashtable context) {
+	if (req == null || req.length == 0)
+	    return (offer == null || offer.length == 0);
+	if (offer == null || offer.length != req.length)
+	    return false;
+	for (int i = 0; i < req.length; i++) {
+	    if (!ProcessEffect.findMatchingEffect(req[i], offer, context))
+		return false;
+	}
+	return true;
+    }
+
+    /**
+     * Verify that the output bindings of offers match the ones of requests
+     * according to the context
+     * 
+     * @param req
+     *            - a list of requests
+     * @param offer
+     *            - a list of offers
+     * @param context
+     *            - the context
+     * @return true iff the output bindings match
+     */
+    public static boolean checkOutputBindings(Resource[] req, Resource[] offer,
+	    Hashtable context) {
+	if (req == null || req.length == 0)
+	    return true;
+	if (offer == null || offer.length == 0)
+	    return false;
+	for (int i = 0; i < req.length; i++) {
+	    if (!OutputBinding.findMatchingBinding(req[i], offer, context))
+		return false;
+	}
+	return true;
+    }
+
+    /**
+     * Create an instance of ProcessResult from a resource passed as a parameter
+     * 
+     * @param pr
+     *            - a resource reprenting process result
+     * @return - the created instance of ProcessResult
+     */
+    public static ProcessResult toResult(Resource pr) {
+	if (pr == null)
+	    return null;
+
+	Object effects = pr.getProperty(PROP_OWLS_RESULT_HAS_EFFECT);
+	if (effects instanceof Resource) {
+	    ArrayList l = new ArrayList(1);
+	    l.add(effects);
+	    effects = l;
+	}
+	Object bindings = pr.getProperty(PROP_OWLS_RESULT_WITH_OUTPUT);
+	if (bindings instanceof Resource) {
+	    ArrayList l = new ArrayList(1);
+	    l.add(bindings);
+	    bindings = l;
+	}
+	if ((bindings == null && effects == null)
+		|| (bindings != null && !(bindings instanceof List))
+		|| (effects != null && !(effects instanceof List)))
+	    return null;
+
+	ProcessResult result = new ProcessResult();
+
+	if (effects != null) {
+	    for (int i = 0; i < ((List) effects).size(); i++)
+		if (!ProcessEffect.checkEffect(((List) effects).get(i)))
+		    return null;
+	    result.setProperty(PROP_OWLS_RESULT_HAS_EFFECT, effects);
+	}
+
+	if (bindings != null) {
+	    for (int i = 0; i < ((List) bindings).size(); i++)
+		if (!OutputBinding.checkBinding(((List) bindings).get(i)))
+		    return null;
+	    result.setProperty(PROP_OWLS_RESULT_WITH_OUTPUT, bindings);
+	}
+
+	return result;
+    }
+
+    public ProcessResult() {
+	super();
+	addType(TYPE_OWLS_RESULT, true);
+    }
+
+    /**
+     * Add "add" process effect with property path and value passed as
+     * parameters
+     * 
+     * @param ppath
+     *            - the property path
+     * @param value
+     *            - the value
+     */
+    public void addAddEffect(PropertyPath ppath, Object value) {
+	effects().add(ProcessEffect.constructAddEffect(ppath, value));
+    }
+
+    /**
+     * Add aggregated output binding to a process output parameter
+     * 
+     * @param toParam
+     *            - the parameter to which the binding is done
+     * @param filter
+     *            - the aggregation filter of the output binding
+     */
+    public void addAggregatingOutputBinding(ProcessOutput toParam,
+	    AggregatingFilter filter) {
+	bindings().add(
+		OutputBinding.constructAggregatingBinding(toParam, filter));
+    }
+
+    /**
+     * Add "change" process effect with property path and value passed as
+     * parameters
+     * 
+     * @param ppath
+     *            - the property path
+     * @param value
+     *            - the value
+     */
+    public void addChangeEffect(PropertyPath ppath, Object value) {
+	effects().add(ProcessEffect.constructChangeEffect(ppath, value));
+    }
+
+    /**
+     * Add "remove" process effect with property path and value passed as
+     * parameters
+     * 
+     * @param ppath
+     *            - the property path
+     * @param value
+     *            - the value
+     */
+    public void addRemoveEffect(PropertyPath ppath) {
+	effects().add(ProcessEffect.constructRemoveEffect(ppath));
+    }
+
+    /**
+     * Add class conversion output binding to a process output parameter
+     * 
+     * @param toParam
+     *            - the parameter to which the binding is done
+     * @param sourceProp
+     *            - the property path of the value to convert
+     * @param targetClass
+     *            - the uri of the target class to convert to
+     */
+    public void addClassConversionOutputBinding(ProcessOutput toParam,
+	    PropertyPath sourceProp, TypeURI targetClass) {
+	bindings().add(
+		OutputBinding.constructClassConversionBinding(toParam,
+			sourceProp, targetClass));
+    }
+
+    /**
+     * Add language conversion output binding to a process output parameter
+     * 
+     * @param toParam
+     *            - the parameter to which the binding is done
+     * @param sourceProp
+     *            - the property path of the value to convert
+     * @param targetLang
+     *            - the target language to convert to
+     */
+    public void addLangConversionOutputBinding(ProcessOutput toParam,
+	    PropertyPath sourceProp, String targetLang) {
+	bindings().add(
+		OutputBinding.constructLanguageConversionBinding(toParam,
+			sourceProp, targetLang));
+    }
+
+    /**
+     * Add unit conversion output binding to a process output parameter
+     * 
+     * @param toParam
+     *            - the parameter to which the binding is done
+     * @param sourceProp
+     *            - the property path of the value to convert
+     * @param targetUnit
+     *            - the unit to convert to
+     */
+    public void addUnitConversionOutputBinding(ProcessOutput toParam,
+	    PropertyPath sourceProp, String targetUnit) {
+	bindings().add(
+		OutputBinding.constructUnitConversionBinding(toParam,
+			sourceProp, targetUnit));
+    }
+
+    /**
+     * Add simple output binding to a process output parameter
+     * 
+     * @param toParam
+     *            - the parameter to which the binding is done
+     * @param sourceProp
+     *            - the property path of the value to bind
+     */
+    public void addSimpleOutputBinding(ProcessOutput toParam,
+	    PropertyPath sourceProp) {
+	bindings().add(
+		OutputBinding.constructSimpleBinding(toParam, sourceProp));
+    }
+
+    /**
+     * Return list of output bindings (a copy) of this process result
+     * 
+     * @return list of output bindings
+     */
+    private List bindings() {
+	List result = (List) props.get(PROP_OWLS_RESULT_WITH_OUTPUT);
+	if (result == null) {
+	    result = new ArrayList();
+	    props.put(PROP_OWLS_RESULT_WITH_OUTPUT, result);
+	}
+	return result;
+    }
+
+    /**
+     * Return list of effects (a copy) of this process result
+     * 
+     * @return list of effects
+     */
+    private List effects() {
+	List result = (List) props.get(PROP_OWLS_RESULT_HAS_EFFECT);
+	if (result == null) {
+	    result = new ArrayList();
+	    props.put(PROP_OWLS_RESULT_HAS_EFFECT, result);
+	}
+	return result;
+    }
+
+    /**
+     * Return the list of output bindings ( a reference) of this process result
+     * 
+     * @return
+     */
+    public List getBindings() {
+	return (List) props.get(PROP_OWLS_RESULT_WITH_OUTPUT);
+    }
+
+    /**
+     * Return list of effects (a reference) of this process result
+     * 
+     * @return list of effects
+     */
+    public List getEffects() {
+	return (List) props.get(PROP_OWLS_RESULT_HAS_EFFECT);
+    }
+
+    /**
+     * Return true iff the process result is well formed (contains consistent
+     * properties)
+     */
+    public boolean isWellFormed() {
+	return props.containsKey(PROP_OWLS_RESULT_WITH_OUTPUT)
+		|| props.containsKey(PROP_OWLS_RESULT_HAS_EFFECT);
+    }
+}
