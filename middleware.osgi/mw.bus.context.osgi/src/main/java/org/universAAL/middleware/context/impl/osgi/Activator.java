1a7782cd9aa78d56672fe29f65880d2d323cbed1
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-13 13:19:20 +0000

    Automatic tag of mw.bus.context.osgi version: 1.2.0

---
+++ middleware.osgi/mw.bus.context.osgi/src/main/java/org/universAAL/middleware/context/impl/osgi/Activator.java
@@ -0,0 +1,60 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut für Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.context.impl.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.context.ContextBus;
+import org.universAAL.middleware.context.impl.ContextBusImpl;
+import org.universAAL.middleware.sodapop.SodaPop;
+import org.universAAL.middleware.sodapop.msg.MessageContentSerializer;
+
+/**
+ * 
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public class Activator implements BundleActivator {
+
+    /**
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+	ContextBusImpl.container = uAALBundleContainer.THE_CONTAINER;
+	ContextBusImpl.moduleContext = uAALBundleContainer.THE_CONTAINER
+		.registerModule(new Object[] { context });
+	ContextBusImpl.busFetchParams = ContextBusImpl.busShareParams = new Object[] { ContextBus.class
+		.getName() };
+	ContextBusImpl.contentSerializerParams = new Object[] { MessageContentSerializer.class
+		.getName() };
+	ContextBusImpl.sodapopFetchParams = new Object[] { SodaPop.class
+		.getName() };
+	ContextBusImpl.startModule();
+    }
+
+    /**
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+	ContextBusImpl.stopModule();
+    }
+}
