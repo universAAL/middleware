6d682bda4374191901d27c2a051bca42cb8fda60
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-13 13:19:04 +0000

    Automatic tag of mw.bus.service.aapi.core version: 1.2.0

---
+++ middleware.core/mw.bus.service.aapi.core/src/main/java/org/universAAL/middleware/api/SimpleServiceLocator.java
@@ -0,0 +1,18 @@
+package org.universAAL.middleware.api;
+
+import org.universAAL.middleware.api.exception.SimplifiedRegistrationException;
+import org.universAAL.middleware.api.impl.DynamicServiceProxy;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.service.DefaultServiceCaller;
+
+public class SimpleServiceLocator {
+    public ModuleContext mc;
+
+    public SimpleServiceLocator(ModuleContext mc) {
+	this.mc = mc;
+    }
+    
+    public Object lookupService(Class interfaceClazz) throws IllegalArgumentException, SimplifiedRegistrationException, InstantiationException, IllegalAccessException{
+	return DynamicServiceProxy.newInstance(interfaceClazz, new DefaultServiceCaller(mc));
+    }
+}
---
+++ middleware.core/mw.bus.service.aapi.core/src/main/java/org/universAAL/middleware/api/annotation/Cardinality.java
@@ -0,0 +1,5 @@
+package org.universAAL.middleware.api.annotation;
+
+public enum Cardinality {
+    NOT_SPECIFIED, ONE_TO_ONE, MANY_TO_MANY
+}
