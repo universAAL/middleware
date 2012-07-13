f3e44b6bf0c01913adea75075c9d74a2045ae1ec
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-13 13:19:26 +0000

    Automatic tag of mw.bus.ui.core version: 1.2.0

---
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/UIResponse.java
@@ -0,0 +1,208 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut fuer Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either.ss or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.ui;
+
+import org.universAAL.middleware.owl.supply.AbsLocation;
+import org.universAAL.middleware.rdf.FinalizedResource;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.rdf.Form;
+import org.universAAL.middleware.ui.rdf.SubdialogTrigger;
+import org.universAAL.middleware.ui.rdf.Submit;
+
+/**
+ * Instances of this class can be used to exchange info about user input.
+ * 
+ * @author mtazari
+ * @author Carsten Stockloew
+ */
+public class UIResponse extends FinalizedResource {
+
+    /** The Constant MY_URI. */
+    public static final String MY_URI = UIRequest.uAAL_UI_NAMESPACE
+	    + "UIResponse";
+
+    /** The Constant PROP_DIALOG_DATA. */
+    public static final String PROP_DIALOG_DATA = UIRequest.uAAL_UI_NAMESPACE
+	    + "dialogData";
+
+    /** The Constant PROP_DIALOG_ID. */
+    public static final String PROP_DIALOG_ID = UIRequest.uAAL_UI_NAMESPACE
+	    + "dialogID";
+
+    /** The Constant PROP_SUBMISSION_LOCATION. */
+    public static final String PROP_SUBMISSION_LOCATION = UIRequest.uAAL_UI_NAMESPACE
+	    + "submissionLocation";
+
+    /** The Constant PROP_IS_SUBDIALOG_CALL. */
+    public static final String PROP_IS_SUBDIALOG_CALL = UIRequest.uAAL_UI_NAMESPACE
+	    + "isSubdialogCall";
+
+    /** The Constant PROP_SUBMISSION_ID. */
+    public static final String PROP_SUBMISSION_ID = UIRequest.uAAL_UI_NAMESPACE
+	    + "submissionID";
+
+    /**
+     * This constructor is for the exclusive usage by deserializers.
+     */
+    public UIResponse() {
+	super();
+    }
+
+    /**
+     * This constructor is used in the context of a running dialog.
+     * 
+     * @param user
+     *            reference to the user
+     * @param inputLocation
+     *            location of the user
+     * @param submit
+     *            instance of a submit button that has finished the dialog
+     */
+    public UIResponse(Resource user, AbsLocation inputLocation, Submit submit) {
+	super();
+
+	addType(MY_URI, true);
+	props.put(PROP_uAAL_INVOLVED_HUMAN_USER, user);
+	props.put(PROP_DIALOG_ID, new Resource(submit.getDialogID()));
+	props.put(PROP_SUBMISSION_ID, submit.getID());
+	Form f = submit.getFormObject();
+	props.put(PROP_DIALOG_DATA, f.getData());
+	Resource parentDialog = f.getParentDialogResource();
+	if (parentDialog != null)
+	    props.put(Form.PROP_PARENT_DIALOG_URI, parentDialog);
+	if (inputLocation != null)
+	    props.put(PROP_SUBMISSION_LOCATION, inputLocation);
+	if (submit instanceof SubdialogTrigger)
+	    props.put(PROP_IS_SUBDIALOG_CALL, Boolean.TRUE);
+    }
+
+    /**
+     * Gets the dialog id.
+     * 
+     * @return ID of the dialog in which this input has been provided
+     */
+    public String getDialogID() {
+	Object id = props.get(PROP_DIALOG_ID);
+	return (id instanceof Resource) ? id.toString() : null;
+    }
+
+    /**
+     * Gets the submission location.
+     * 
+     * @return the submission location
+     */
+    public AbsLocation getSubmissionLocation() {
+	Object loc = props.get(PROP_SUBMISSION_LOCATION);
+	return (loc instanceof AbsLocation) ? (AbsLocation) loc : null;
+    }
+
+    /**
+     * Gets the parent dialog uri.
+     * 
+     * @return the ID of the parent dialog in case this event is about dialog
+     *         being finished (which can be checked by calling
+     *         isSubdialogSubmission() method)
+     */
+    public String getParentDialogURI() {
+	Object o = props.get(Form.PROP_PARENT_DIALOG_URI);
+	return (o instanceof Resource) ? o.toString() : null;
+    }
+
+    /**
+     * @see org.universAAL.middleware.rdf.Resource#getPropSerializationType(java.lang.String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return (PROP_uAAL_INVOLVED_HUMAN_USER.equals(propURI) || PROP_SUBMISSION_LOCATION
+		.equals(propURI)) ? PROP_SERIALIZATION_REDUCED
+		: PROP_SERIALIZATION_FULL;
+    }
+
+    /**
+     * Gets the submission id.
+     * 
+     * @return ID of the submit button selected by the user when finishing the
+     *         dialog
+     */
+    public String getSubmissionID() {
+	Object s = props.get(PROP_SUBMISSION_ID);
+	return (s instanceof String) ? (String) s : null;
+    }
+
+    /**
+     * Gets the submitted data.
+     * 
+     * @return root of the tree with form data submitted
+     */
+    public Resource getSubmittedData() {
+	return (Resource) props.get(PROP_DIALOG_DATA);
+    }
+
+    /**
+     * Gets the user.
+     * 
+     * @return the user
+     */
+    public Resource getUser() {
+	Object user = props.get(PROP_uAAL_INVOLVED_HUMAN_USER);
+	return (user instanceof Resource) ? (Resource) user : null;
+    }
+
+    /**
+     * Gets the user input.
+     * 
+     * @param propPath
+     *            array of property URIs, path of a certain expected user input
+     * @return input from the tree with the form data
+     */
+    public Object getUserInput(String[] propPath) {
+	if (propPath == null || propPath.length == 0)
+	    return null;
+	Resource pr = (Resource) props.get(PROP_DIALOG_DATA);
+	if (pr == null)
+	    return null;
+	Object o = pr.getProperty(propPath[0]);
+	for (int i = 1; o != null && i < propPath.length; i++) {
+	    if (!(o instanceof Resource))
+		return null;
+	    pr = (Resource) o;
+	    o = pr.getProperty(propPath[i]);
+	}
+
+	return o;
+    }
+
+    /**
+     * Checks if is subdialog call.
+     * 
+     * @return true, if is subdialog call
+     */
+    public boolean isSubdialogCall() {
+	return Boolean.TRUE.equals(props.get(PROP_IS_SUBDIALOG_CALL));
+    }
+
+    /**
+     * Checks if is subdialog submission.
+     * 
+     * @return true, if is subdialog submission
+     */
+    public boolean isSubdialogSubmission() {
+	return props.containsKey(Form.PROP_PARENT_DIALOG_URI);
+    }
+}
---
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/owl/DialogType.java
@@ -0,0 +1,128 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut fuer Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either.ss or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.ui.owl;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+/**
+ * Defines types of dialog that can be System menu, Message, Subdialog and
+ * Standard Dialog. For their explanation check
+ * {@link org.universAAL.middleware.ui.rdf.Form}
+ * 
+ * @author mtazari
+ * @author Carsten Stockloew
+ * @see org.universAAL.middleware.owl.ManagedIndividual
+ */
+public class DialogType extends ManagedIndividual {
+
+    public static final String MY_URI = uAAL_VOCABULARY_NAMESPACE
+	    + "DialogType";
+
+    public static final int SYS_MENU = 0;
+    public static final int MESSAGE = 1;
+    public static final int SUBDIALOG = 2;
+    public static final int STD_DIALOG = 3;
+
+    private static final String[] names = { "system_menu", "message",
+	    "subdialog", "std_dialog" };
+
+    public static final DialogType sysMenu = new DialogType(SYS_MENU);
+    public static final DialogType message = new DialogType(MESSAGE);
+    public static final DialogType subdialog = new DialogType(SUBDIALOG);
+    public static final DialogType stdDialog = new DialogType(STD_DIALOG);
+
+    private int order;
+
+    public static DialogType getLevelByOrder(int order) {
+	switch (order) {
+	case SYS_MENU:
+	    return sysMenu;
+	case MESSAGE:
+	    return message;
+	case SUBDIALOG:
+	    return subdialog;
+	case STD_DIALOG:
+	    return stdDialog;
+	default:
+	    return null;
+	}
+    }
+
+    public static final DialogType valueOf(String name) {
+	for (int i = SYS_MENU; i <= STD_DIALOG; i++)
+	    if (names[i].equals(name))
+		return getLevelByOrder(i);
+	return null;
+    }
+
+    /** Default constructor is prevented */
+    private DialogType() {
+	// prevented usage of the default constructor
+    }
+
+    /**
+     * Constructor with order (it is private so that instantiation by other
+     * callers is prevented?)
+     * 
+     * @param order
+     *            order of dialog
+     */
+    private DialogType(int order) {
+	super(uAAL_VOCABULARY_NAMESPACE + names[order]);
+	this.order = order;
+    }
+
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType(String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#isWellFormed()
+     */
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    /**
+     * @return name of the DialogType with the order defined at the time of
+     *         construction
+     */
+    public String name() {
+	return names[order];
+    }
+
+    /**
+     * @return order defined at the time of construction
+     */
+    public int ord() {
+	return order;
+    }
+
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#setProperty(String,
+     *      Object)
+     */
+    public void setProperty(String propURI, Object o) {
+	// do nothing
+    }
+}
