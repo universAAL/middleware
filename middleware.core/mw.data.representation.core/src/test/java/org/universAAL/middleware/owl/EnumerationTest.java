978e9cf22a58de7dd71c1a6f014d1509e13f1825
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-09-08 03:59:42 +0000

    added method "size" to Enumeration added javadoc unit tests

--- middleware.core/mw.data.representation.core/src/test/java/org/universAAL/middleware/owl/EnumerationTest.java
+++ middleware.core/mw.data.representation.core/src/test/java/org/universAAL/middleware/owl/EnumerationTest.java
@@ -1,5 +1,6 @@
 package org.universAAL.middleware.owl;
 
+import org.universAAL.middleware.owl.testont.MyClass1;
 import org.universAAL.middleware.rdf.TypeMapper;
 
 import junit.framework.TestCase;
@@ -14,17 +15,20 @@ public class EnumerationTest extends TestCase {
     Object[] a1 = { i1 };
     Object[] a2 = { i2 };
     Object[] a01 = { i0, i1 };
+    Object[] a012 = { i0, i1, i2 };
 
-    Enumeration e0 = new Enumeration(a0);
-    Enumeration e1 = new Enumeration(a1);
-    Enumeration e2 = new Enumeration(a2);
-    Enumeration e01 = new Enumeration(a01);
+    public Enumeration e0 = new Enumeration(a0);
+    public Enumeration e1 = new Enumeration(a1);
+    public Enumeration e2 = new Enumeration(a2);
+    public Enumeration e01 = new Enumeration(a01);
+    public Enumeration e012 = new Enumeration(a012);
 
     protected void setUp() throws Exception {
 	super.setUp();
     }
 
     public void testMethods() {
+	// --
 	assertTrue(e0.getNamedSuperclasses().length == 1);
 	assertTrue(e0.getNamedSuperclasses()[0].equals(TypeMapper
 		.getDatatypeURI(i0)));
@@ -41,6 +45,18 @@ public class EnumerationTest extends TestCase {
 	assertTrue(e01.getUpperEnumeration().length == 2);
 	assertTrue(e01.getUpperEnumeration()[0] == i0);
 	assertTrue(e01.getUpperEnumeration()[1] == i1);
+
+	// --
+	assertTrue(e012.getMinValue() == i0);
+	assertTrue(e012.getMaxValue() == i2);
+	assertFalse(e012.getMinValue() == i1);
+
+	// -- this may change --
+	Enumeration e_1 = new Enumeration(new Object[] { Integer.valueOf(0),
+		MyClass1.MY_URI });
+	assertTrue(e_1.size() == 2);
+	e_1.addValue(Integer.valueOf(1));
+	assertTrue(e_1.size() == 3);
     }
 
     public void testHasMember() {
