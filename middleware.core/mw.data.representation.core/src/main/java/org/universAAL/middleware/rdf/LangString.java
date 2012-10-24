66f7e292e7d903b1e493b863eda99888ae67f01c
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-10-24 17:23:56 +0000

    String with language tag

---
+++ middleware.core/mw.data.representation.core/src/main/java/org/universAAL/middleware/rdf/LangString.java
@@ -0,0 +1,67 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.rdf;
+
+/**
+ * A String literal with optional language tag.
+ * 
+ * @author Carsten Stockloew
+ */
+public class LangString {
+
+    private String str = "";
+    private String lang = "";
+
+    public LangString(String str, String lang) {
+	if (str == null)
+	    this.str = "";
+	else
+	    this.str = str;
+
+	if (lang == null)
+	    this.lang = "";
+	else
+	    this.lang = lang;
+    }
+
+    public String getString() {
+	return str;
+    }
+
+    public String getLang() {
+	return lang;
+    }
+
+    public boolean equals(Object obj) {
+	if (!(obj instanceof LangString)) {
+	    if (obj instanceof String && lang.isEmpty())
+		return true;
+	    return false;
+	}
+	LangString l = (LangString) obj;
+	if (str.equals(l.str) && lang.equals(l.lang))
+	    return true;
+	return false;
+    }
+
+    public String toString() {
+	return super.toString();
+    }
+}
