b3a1622030f7a97fad88edf32883d36a06cedf54
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-27 19:10:01 +0000

    Automatic tag of mw.bus.service.aapi.core version: 1.3.0

---
+++ middleware.core/mw.bus.service.aapi.core/src/main/java/org/universAAL/middleware/api/annotation/Output.java
@@ -0,0 +1,24 @@
+package org.universAAL.middleware.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used for generating ServiceProfile. For each of such annotation a
+ * separate addOutput call is made on SimpleAPIService.
+ * 
+ * @author dzmuda
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Output {
+    String name() default "";
+
+    String[] propertyPaths() default {};
+
+    Class<?> filteringClass() default void.class;
+
+    Cardinality cardinality() default Cardinality.NOT_SPECIFIED;
+}
