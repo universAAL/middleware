b7438c617eb67e017f6b3315daf7ebb51187c353
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-27 19:09:54 +0000

    Automatic tag of mw.bus.service.core version: 1.3.0

---
+++ middleware.core/mw.bus.service.core/src/main/java/org/universAAL/middleware/service/aapi/AapiServiceResponse.java
@@ -0,0 +1,21 @@
+package org.universAAL.middleware.service.aapi;
+
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.ServiceResponse;
+
+public class AapiServiceResponse extends ServiceResponse {
+    public AapiServiceResponse() {
+    }
+
+    public AapiServiceResponse(String uri) {
+	super(uri);
+    }
+
+    public AapiServiceResponse(CallStatus status) {
+	super(status);
+    }
+    
+    public void allowUnboundOutput() {
+	super.allowUnboundOutput();
+    }
+}
