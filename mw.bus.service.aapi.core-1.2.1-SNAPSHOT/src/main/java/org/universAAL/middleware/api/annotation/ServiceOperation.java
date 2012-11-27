9fe0767526f1ebd794650d406b37725cc6ef567b
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-27 17:59:03 +0000

    Automatic tag of mw.bus.service.aapi.core version: 1.2.1-SNAPSHOT

---
+++ mw.bus.service.aapi.core-1.2.1-SNAPSHOT/src/main/java/org/universAAL/middleware/api/annotation/ServiceOperation.java
@@ -0,0 +1,26 @@
+package org.universAAL.middleware.api.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+/**
+ * Annotation which marks method as UniversAAL service. For each of those
+ * methods a separate ServiceProfile is generated. If no value is specified a
+ * method name is taken by default.
+ * 
+ * @author dzmuda
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ServiceOperation {
+    String value() default "";
+
+    MatchMakingType type() default MatchMakingType.NOT_SPECIFIED;
+
+    enum MatchMakingType {
+	ONTOLOGICAL, BY_URI, NOT_SPECIFIED
+    }
+}
