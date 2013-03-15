cc9cacc1251f04789aa9469732868cc02f729acc
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-03-15 12:43:37 +0000

    Automatic tag of mw.bus.model.core version: 1.3.1-SNAPSHOT

---
+++ mw.bus.model.core-1.3.1-SNAPSHOT/src/main/java/org/universAAL/middleware/sodapop/IRegistryListener.java
@@ -0,0 +1,11 @@
+package org.universAAL.middleware.sodapop;
+
+public interface IRegistryListener {
+
+    void busMemberAdded(BusMember busMember);
+
+    void busMemberRemoved(BusMember busMember);
+
+    void busCleared();
+
+}
