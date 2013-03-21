95d94fa0a632c43e958c2580989f1c304d6d1cf2
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-03-21 21:44:34 +0000

    fixed URIs in unit tests

--- middleware.osgi/mw.bus.context.osgi/src/test/java/org/universAAL/middleware/context/test/ont/Location.java
+++ middleware.osgi/mw.bus.context.osgi/src/test/java/org/universAAL/middleware/context/test/ont/Location.java
@@ -1,21 +1,21 @@
 package org.universAAL.middleware.context.test.ont;
 
 import org.universAAL.middleware.owl.ManagedIndividual;
+import org.universAAL.middleware.rdf.Resource;
 
 public class Location extends ManagedIndividual {
 
-    public static final String MY_URI = "org.universAAL.middleware.context.test.ont#Location";
+    public static final String MY_URI = TestOntology.NAMESPACE + "Location";
 
     public Location(String uri) {
 	super(uri);
     }
 
     public int getPropSerializationType(String propURI) {
-	return 0;
+	return Resource.PROP_SERIALIZATION_FULL;
     }
 
     public String getClassURI() {
 	return MY_URI;
     }
-
 }
