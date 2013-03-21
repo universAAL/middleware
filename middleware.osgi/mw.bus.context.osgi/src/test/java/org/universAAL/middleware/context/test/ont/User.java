9113ba58d53e1b84cb3c3fb9f4b8343a3952247f
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-03-21 21:44:34 +0000

    fixed URIs in unit tests

--- middleware.osgi/mw.bus.context.osgi/src/test/java/org/universAAL/middleware/context/test/ont/User.java
+++ middleware.osgi/mw.bus.context.osgi/src/test/java/org/universAAL/middleware/context/test/ont/User.java
@@ -1,18 +1,20 @@
 package org.universAAL.middleware.context.test.ont;
 
 import org.universAAL.middleware.owl.ManagedIndividual;
+import org.universAAL.middleware.rdf.Resource;
 
 public class User extends ManagedIndividual {
 
-    public static final String MY_URI = "org.universAAL.middleware.context.test.ont#User";
-    public static final String PROP_PHYSICAL_LOCATION = "org.universAAL.middleware.context.test.ont#PROP_PHYSICAL_LOCATION";
+    public static final String MY_URI = TestOntology.NAMESPACE + "User";
+    public static final String PROP_PHYSICAL_LOCATION = TestOntology.NAMESPACE
+	    + "PROP_PHYSICAL_LOCATION";
 
     public User(String uri) {
 	super(uri);
     }
 
     public int getPropSerializationType(String propURI) {
-	return 0;
+	return Resource.PROP_SERIALIZATION_FULL;
     }
 
     public void setLocation(Location location) {
