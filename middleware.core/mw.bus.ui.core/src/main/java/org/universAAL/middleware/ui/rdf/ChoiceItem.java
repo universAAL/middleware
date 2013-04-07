fa271b7c1f5d4dffa768ea8e3b7c4dcfe19a25a2
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-04-07 21:27:39 +0000

    uaal directives (mostly fixed license header)

--- middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/rdf/ChoiceItem.java
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/rdf/ChoiceItem.java
@@ -1,6 +1,6 @@
 /*	
-	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
-	Fraunhofer Gesellschaft - Institut fuer Graphische Datenverarbeitung 
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
 	
 	See the NOTICE file distributed with this work for additional 
 	information regarding copyright ownership
@@ -13,7 +13,7 @@
 	
 	Unless required by applicable law or agreed to in writing, software
 	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either.ss or implied.
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
  */
