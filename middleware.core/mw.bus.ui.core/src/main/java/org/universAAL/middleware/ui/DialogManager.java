6a4ac5146315ed30cad5f872c50c10ef3360a590
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-05-06 14:57:59 +0000

    ui bus and dm javadoc and polishing

--- middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/DialogManager.java
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/DialogManager.java
@@ -25,21 +25,21 @@ import org.universAAL.middleware.owl.supply.AbsLocation;
 import org.universAAL.middleware.rdf.Resource;
 
 /**
- * Dialog Manager is the main (application-independent) component for handling
- * system dialogs. It (1) represents the whole system by providing system menus
- * (a unified view of all services available), possibilities to search for
- * specific services, and by handling context-free user input (user input that
- * cannot be assigned to any running dialog), and (2) assists the UI bus by
- * (2.1) acting as a representative for the whole framework supporting
+ * {@link DialogManager} is the main (application-independent) component for
+ * handling system dialogs. It (1) represents the whole system by providing
+ * system menus (a unified view of all services available), possibilities to
+ * search for specific services, and by handling context-free user input (user
+ * input that cannot be assigned to any running dialog), and (2) assists the UI
+ * bus by (2.1) acting as a representative for the whole framework supporting
  * context-awareness & personalization, and by (2.2) providing user-specific
  * management of dialogs initiated by different applications to protect the user
  * against a mess of parallel dialogs (decides whether some dialog needs to wait
  * for another dialog to finish).
  * 
  * This interface is supposed to be implemented by exactly one component. The
- * first component that registers as a UICaller to the UI bus (and implements
- * this interface) blocks the registration of any further implementation of this
- * interface.
+ * first component that registers as a {@link UICaller} to the UI bus (and
+ * implements this interface) blocks the registration of any further
+ * implementation of this interface.
  * 
  * @author mtazari
  * @author eandgrg
@@ -50,25 +50,26 @@ public interface DialogManager {
      * Check new dialog.
      * 
      * @param request
-     *            UI request to UI bus
-     * @return the decision if the new UI request to UI Bus can be immediately
-     *         forwarded to an UI Handler (returns true) or must wait for a
-     *         higher priority dialog to finish (return false). In case of
-     *         returning true, the Dialog Manager must also add the current
-     *         personal and situational parameters to UI request so that the
-     *         matchmaking on the bus results in adaptive selection of UI
-     *         channel. In case of returning false, the UI Bus ignores the
-     *         request because it trusts that the Dialog Manager will keep the
-     *         request in a queue of suspended dialogs and will re-activate it
-     *         whenever appropriate.
+     *            {@link UIRequest} to {@link UIBus}
+     * @return the decision if the new {@link UIRequest} to {@link UIBus} can be
+     *         immediately forwarded to an {@link UIHandler} (returns true) or
+     *         must wait for a higher priority dialog to finish (return false).
+     *         In case of returning true, the {@link DialogManager} must also
+     *         add the current adaptation parameters to {@link UIRequest} so
+     *         that the matchmaking on the {@link UIBus} results in adaptive
+     *         selection of UI channel. In case of returning false, the
+     *         {@link UIBus} ignores the {@link UIRequest} because it trusts
+     *         that the {@link DialogManager} will keep the {@link UIRequest} in
+     *         a queue of suspended dialogs and will re-activate it whenever
+     *         appropriate.
      */
     public boolean checkNewDialog(UIRequest request);
 
     /**
-     * Informs the Dialog Manager that a running dialog has finished according
-     * to the information received from an UI handler. As a result this may
-     * result in re-activation of previously suspended dialog (by the Dialog
-     * Manager).
+     * Informs the {@link DialogManager} that a running dialog has finished
+     * according to the information received from an {@link UIHandler}. As a
+     * result this may result in re-activation of previously suspended dialog
+     * (by the {@link DialogManager}).
      * 
      * @param dialogID
      *            ID of the dialog
@@ -79,9 +80,10 @@ public interface DialogManager {
      * Show main menu of the system.
      * 
      * @param user
-     *            user of a system
+     *            {@link User} of a system
      * @param loginLocation
-     *            location from which user has logged to the system
+     *            {@link Location} from which {@link User} has logged in to the
+     *            system
      */
     public void getMainMenu(Resource user, AbsLocation loginLocation);
 
@@ -97,9 +99,9 @@ public interface DialogManager {
     public UIRequest getSuspendedDialog(String dialogID);
 
     /**
-     * The bus must use this method in order to inform the Dialog Manager that a
-     * dialog has to be suspended. This is the case when during a dialog is
-     * running the user steps into a subdialog so the parent dialog must be
+     * The bus must use this method in order to inform the {@link DialogManager}
+     * that a dialog has to be suspended. This is the case when during a dialog
+     * is running the user steps into a subdialog so the parent dialog must be
      * suspended until the application receives the user input from the
      * subdialog.
      * 
