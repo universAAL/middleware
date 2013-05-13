9c8e715bf4589c0d0fb4db6c6c4be64fae0a3a93
Stefano Lenzi <stefano.lenzi@isti.cnr.it>
2013-05-13 09:27:32 +0000

    Fixed package name typo

---
+++ middleware.resources/mw.schemas/src/test/java/org/universAAL/middleware/deploymanager/uapp/model/TestDeployMangerModel.java
@@ -0,0 +1,33 @@
+package org.universAAL.middleware.deploymanager.uapp.model;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.junit.Test;
+import org.universAAL.middleware.deploymanager.uapp.model.AalUapp;
+import org.universAAL.middleware.deploymanager.uapp.model.ObjectFactory;
+
+public class TestDeployMangerModel {
+
+    @Test
+    public void testUAPPParsing() {
+        try {
+            JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            Marshaller marshaller = jc.createMarshaller();
+            InputStream is = TestDeployMangerModel.class.getResourceAsStream("./hwo.uapp.xml");
+            AalUapp uAAP = (AalUapp)unmarshaller.unmarshal(is);
+        } catch (JAXBException e) {
+            e.printStackTrace(System.err);
+            fail(e.getMessage());
+        }
+    }
+
+}
