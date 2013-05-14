e8c22457b6260bf724463f414e0c3abafd279386
Stefano Lenzi <stefano.lenzi@isti.cnr.it>
2013-05-14 13:32:37 +0000

    Trying to fix ConcurrentModificationException in uAALBundleContainer Using BundleConfigHome for storing DeployManager status and configuration

--- middleware.core/mw.managers.deploy.core/src/main/java/org/universAAL/middleware/managers/deploy/util/Consts.java
+++ middleware.core/mw.managers.deploy.core/src/main/java/org/universAAL/middleware/managers/deploy/util/Consts.java
@@ -1,34 +1,41 @@
-/*	
-	Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
-	Institute of Information Science and Technologies 
-	of the Italian National Research Council 
+/*
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
 
-	See the NOTICE file distributed with this work for additional 
-	information regarding copyright ownership
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
 
-	  http://www.apache.org/licenses/LICENSE-2.0
+          http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
  */
 package org.universAAL.middleware.managers.deploy.util;
 
 /**
  * Widely-used constants for the DeployManager
- * 
+ *
  * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
  * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
+ * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
+ * @version $LastChangedRevision$ ( $LastChangedDate$ )
  */
 public class Consts {
-    public static String START_INSTALLATION = "startInstallation";
-    public static String MPA_SUFFIX = "mpaSuffix";
-    public static String DEPLOY_DIR = "deployDir";
+    public static final String START_INSTALLATION = "startInstallation";
+    public static final String MPA_SUFFIX = "mpaSuffix";
+    public static final String DEPLOY_DIR = "deployDir";
+    public static final String APPLICATION_CONFIGURATION_PATH = "config";
+    public static final String TMP_DEPLOY_DIR = "tmp";
+    public static final String APPLICATION_BINARYPART_PATH = "bin";
+    public static final String APP_REGISTRY = "installation.registry";
+    public static final String DEPLOY_MANAGER_ID = "uAAL.dm";
 
 }
