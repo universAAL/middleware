73bbf4adbc7fe38d11714d6c4ac8e05e4eb68a15
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 17:08:21 +0000

    Automatic tag of mw.container.osgi version: 1.3.2-SNAPSHOT

---
+++ mw.container.osgi-1.3.2-SNAPSHOT/src/main/java/org/universAAL/middleware/container/osgi/uAALBundleContainer.java
@@ -0,0 +1,324 @@
+/*
+        Copyright 2011-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+        Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
+
+
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+package org.universAAL.middleware.container.osgi;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Iterator;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.universAAL.middleware.container.Container;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.SharedObjectListener;
+import org.universAAL.middleware.container.osgi.run.Activator;
+
+/**
+ * An implementation of the concept of {@link Container} for OSGi.
+ * 
+ * @author mtazari
+ * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
+ * @version $LastChangedRevision$ ( $LastChangedDate$ )
+ * 
+ */
+public class uAALBundleContainer implements Container, ServiceListener {
+    public static final uAALBundleContainer THE_CONTAINER = new uAALBundleContainer();
+
+    private List listeners;
+
+    private uAALBundleContainer() {
+	listeners = Collections.synchronizedList(new ArrayList());
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.Container#fetchSharedObject(ModuleContext,
+     *      Object[], SharedObjectListener)
+     * 
+     * @param fetchParams
+     *            <ul>
+     *            <li>min length = 1, max used length is 2 (elements @ index 2+
+     *            are ignored)</li>
+     *            <li>type of both elements must be {@link java.lang.String}</li>
+     *            <li>if element @ index 0 is null, there must be a not-null
+     *            String object @ index 1</li>
+     *            <li>if length = 1, then element @ index 0 must be not-null</li>
+     *            <li>if length = 1, then
+     *            {@link BundleContext#getServiceReference(String)} will be used
+     *            to fetch a service object</li>
+     *            <li>if length > 1, then
+     *            {@link BundleContext#getServiceReferences(String, String)}
+     *            will be used to fetch the first service object from the
+     *            returned array</li>
+     *            </ul>
+     * 
+     * @return if the given requester is an instance of
+     *         {@link uAALBundleContext} and a {@link ServiceReference} has been
+     *         found according to the explanations given for
+     *         <code>fetchParams</code>, then the result of calling
+     *         {@link BundleContext#getService(ServiceReference)} will be
+     *         returned.
+     */
+    public Object fetchSharedObject(ModuleContext requester,
+	    Object[] fetchParams) {
+	if (requester instanceof uAALBundleContext && fetchParams != null
+		&& fetchParams.length > 0)
+	    if (fetchParams.length == 1) {
+		if (fetchParams[0] instanceof String) {
+		    return ((uAALBundleContext) requester)
+			    .fetchObject((String) fetchParams[0]);
+		}
+	    } else if ((fetchParams[0] == null || fetchParams[0] instanceof String)
+		    && (fetchParams[1] == null || fetchParams[1] instanceof String)) {
+		Object[] result = ((uAALBundleContext) requester).fetchObject(
+			(String) fetchParams[0], (String) fetchParams[1]);
+		if (result != null)
+		    return result[0];
+	    }
+	// problems with parameters
+	return null;
+    }
+
+    public Object[] fetchSharedObject(ModuleContext requester,
+	    Object[] fetchParams, SharedObjectListener listener) {
+	if (requester instanceof uAALBundleContext && fetchParams != null
+		&& fetchParams.length > 0)
+	    if (fetchParams.length == 1) {
+		if (fetchParams[0] instanceof String) {
+		    if (listener != null && !listeners.contains(listener)) {
+			synchronized (listeners) {
+			    listeners.add(listener);
+			}
+
+		    }
+		    return ((uAALBundleContext) requester).fetchObject(
+			    (String) fetchParams[0], null);
+		}
+	    } else if ((fetchParams[0] == null || fetchParams[0] instanceof String)
+		    && (fetchParams[1] == null || fetchParams[1] instanceof String)) {
+		if (listener != null && !listeners.contains(listener))
+		    synchronized (listeners) {
+			listeners.add(listener);
+		    }
+		return ((uAALBundleContext) requester).fetchObject(
+			(String) fetchParams[0], (String) fetchParams[1]);
+	    }
+	// problems with parameters => do not add the listener
+	return null;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.Container#installModule(org.universAAL.middleware.container.ModuleContext,
+     *      java.lang.Object[])
+     * 
+     * @param installParams
+     *            <ul>
+     *            <li>min length = 1, max used length is 2 (elements @ index 2+
+     *            are ignored)</li>
+     *            <li>type of element @ index 0 must be {@link java.lang.String}
+     *            (mandatory)</li>
+     *            <li>if existing, type of element @ index 1 must be
+     *            {@link java.io.InputStream}</li>
+     *            <li>if length = 1, then
+     *            {@link BundleContext#installBundle(String)} will be used to
+     *            install the given bundle</li>
+     *            <li>if length > 1, then
+     *            {@link BundleContext#installBundle(String, java.io.InputStream)}
+     *            will be used to install the given bundle</li>
+     *            </ul>
+     * 
+     * @return a newly created instance of {@link uAALBundleContext} that wraps
+     *         the newly installed bundle if the given requester is an instance
+     *         of {@link uAALBundleContext}, the conditions on
+     *         <code>installParams</code> hold, and the installation is
+     *         successful, null otherwise.
+     */
+    public ModuleContext installModule(ModuleContext requester,
+	    Object[] installParams) {
+	if (!(requester instanceof uAALBundleContext))
+	    return null;
+	if (installParams == null || installParams.length == 0
+		|| !(installParams[0] instanceof String))
+	    return null;
+	return (installParams.length == 1 || !(installParams[1] instanceof InputStream)) ? ((uAALBundleContext) requester)
+		.installBundle((String) installParams[0])
+		: ((uAALBundleContext) requester).installBundle(
+			(String) installParams[0],
+			(InputStream) installParams[1]);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.Container#logListeners()
+     */
+    public Iterator logListeners() {
+	return Activator.logListeners();
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.Container#registerModule(java.lang.Object[])
+     * 
+     * @param regParams
+     *            element @ index 0 must exist and be an instance of
+     *            {@link BundleContext}
+     * 
+     * @return an instance of {@link uAALBundleContext} if the conditions for
+     *         <code>regParams</code> hold, null otherwise
+     */
+    public ModuleContext registerModule(Object[] regParams) {
+	BundleContext bContext = null;
+	if (regParams != null && regParams.length > 0
+		&& regParams[0] instanceof BundleContext) {
+	    bContext = (BundleContext) regParams[0];
+	    // register me as ServiceListener
+	    bContext.addServiceListener(THE_CONTAINER);
+	}
+	return (regParams != null && regParams.length > 0 && regParams[0] instanceof BundleContext) ? new uAALBundleContext(
+		(BundleContext) regParams[0]) : null;
+    }
+
+    /**
+     * @see org.osgi.framework.ServiceListener#serviceChanged(org.osgi.framework.ServiceEvent)
+     */
+    public void serviceChanged(ServiceEvent se) {
+	// NOTE: Not handling ServiceEvent.MODIFIED event
+	if (se == null || se.getType() == ServiceEvent.MODIFIED)
+	    return;
+	ServiceReference sr = se.getServiceReference();
+	if (sr == null)
+	    return;
+	final BundleContext bc = sr.getBundle().getBundleContext();
+	final ArrayList<SharedObjectListener> listenersLocalCopy;
+	synchronized (listeners) {
+	    listenersLocalCopy = new ArrayList<SharedObjectListener>(listeners);
+	}
+	switch (se.getType()) {
+	case ServiceEvent.REGISTERED:
+	    for (Iterator i = listenersLocalCopy.iterator(); i.hasNext();)
+		((SharedObjectListener) i.next()).sharedObjectAdded(
+			bc.getService(sr), sr);
+	    break;
+	case ServiceEvent.UNREGISTERING:
+	    for (Iterator i = listenersLocalCopy.iterator(); i.hasNext();)
+		((SharedObjectListener) i.next()).sharedObjectRemoved(bc
+			.getService(sr));
+
+	    break;
+	}
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.Container#shareObject(org.universAAL.middleware.container.ModuleContext,
+     *      java.lang.Object, java.lang.Object[])
+     * 
+     * @param shareParams
+     *            <ul>
+     *            <li>min length = 1, no upper limit</li>
+     *            <li>all elements (except for an optional last one - see next
+     *            bullet) must be instances of {@link java.lang.String}; these
+     *            strings specify the interfaces that the
+     *            <code>objToShare</code> has implemented (cf.
+     *            {@link org.osgi.framework.BundleContext#registerService(String[], Object, java.util.Dictionary)}
+     *            ).</li>
+     *            <li>optionally, a last element can be an instance of
+     *            {@link java.util.Dictionary}; it can be used for
+     *            property-based search of shared objects (cf.
+     *            {@link org.osgi.framework.BundleContext#registerService(String[], Object, java.util.Dictionary)
+         *            }
+     *            ).</li>
+     *            </ul>
+     */
+    public void shareObject(ModuleContext requester, Object objToShare,
+	    Object[] shareParams) {
+	if (!(requester instanceof uAALBundleContext) || objToShare == null
+		|| shareParams == null || shareParams.length == 0) {
+	    requester
+		    .logWarn(
+			    this.getClass().getName() + "shareObject",
+			    "Parameters passed to 'shareObject' do not satisfy the requirements of mw.container.osgi!",
+			    null);
+	    return;
+	}
+
+	int n = shareParams.length - 1;
+	if (n == 0)
+	    if (shareParams[0] instanceof String)
+		((uAALBundleContext) requester).shareObject(
+			(String) shareParams[0], objToShare, null);
+	    else if (shareParams[0] instanceof Dictionary)
+		((uAALBundleContext) requester).shareObject((String) null,
+			objToShare, (Dictionary) shareParams[0]);
+	    else
+		requester
+			.logWarn(
+				this.getClass().getName() + "shareObject",
+				"'shareParams' passed to 'shareObject' do not satisfy the requirements of mw.container.osgi!",
+				null);
+	else {
+	    for (int i = 0; i < n; i++)
+		if (!(shareParams[i] instanceof String)) {
+		    requester
+			    .logWarn(
+				    this.getClass().getName() + "shareObject",
+				    "'shareParams' passed to 'shareObject' do not satisfy the requirements of mw.container.osgi!",
+				    null);
+		    return;
+		}
+	    if (shareParams[n] instanceof String)
+		((uAALBundleContext) requester).shareObject(
+			(String[]) shareParams, objToShare, null);
+	    else if (shareParams[n] instanceof Dictionary)
+		if (n == 1)
+		    ((uAALBundleContext) requester).shareObject(
+			    (String) shareParams[0], objToShare,
+			    (Dictionary) shareParams[1]);
+		else {
+		    String[] xfaces = new String[n];
+		    for (int i = 0; i < n; i++)
+			xfaces[i] = (String) shareParams[i];
+		    ((uAALBundleContext) requester).shareObject(xfaces,
+			    objToShare, (Dictionary) shareParams[n]);
+		}
+	    else
+		requester
+			.logWarn(
+				this.getClass().getName() + "shareObject",
+				"'shareParams' passed to 'shareObject' do not satisfy the requirements of mw.container.osgi!",
+				null);
+	}
+    }
+
+    public void removeSharedObjectListener(SharedObjectListener listener) {
+	if (listener != null) {
+	    synchronized (listeners) {
+		listeners.remove(listener);
+	    }
+
+	}
+    }
+}
---
+++ mw.container.osgi-1.3.2-SNAPSHOT/src/main/java/org/universAAL/middleware/container/osgi/uAALBundleContext.java
@@ -0,0 +1,322 @@
+/*
+        Copyright 2011-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+        Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
+
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+package org.universAAL.middleware.container.osgi;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.universAAL.middleware.container.Container;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+
+/**
+ * An implementation of the concept of {@link ModuleContext} for OSGi.
+ * 
+ * @author mtazari
+ * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
+ * @version $LastChangedRevision$ ( $LastChangedDate$ )
+ * 
+ */
+public class uAALBundleContext implements ModuleContext {
+    private BundleContext bundle;
+    private Hashtable extension = new Hashtable();
+    private Logger logger;
+    private static BundleConfigHome servicesConfHome = new BundleConfigHome(
+	    "services");
+    private ArrayList confFiles = new ArrayList(2);
+
+    uAALBundleContext(BundleContext bc) {
+	bundle = bc;
+	logger = LoggerFactory.getLogger("org.universAAL."
+		+ bc.getBundle().getSymbolicName());
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#canBeStarted(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean canBeStarted(ModuleContext requester) {
+	// TODO check permissions
+	return bundle.getBundle().getState() == Bundle.RESOLVED;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#canBeStopped(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean canBeStopped(ModuleContext requester) {
+	// TODO check permissions
+	return bundle.getBundle().getState() == Bundle.ACTIVE;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#canBeUninstalled(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean canBeUninstalled(ModuleContext requester) {
+	// TODO check permissions
+	int state = bundle.getBundle().getState();
+	return state == Bundle.RESOLVED || state == Bundle.INSTALLED;
+    }
+
+    public Object fetchObject(String className) {
+	ServiceReference sr = bundle.getServiceReference(className);
+	return (sr == null) ? null : bundle.getService(sr);
+    }
+
+    public Object[] fetchObject(String className, String filter) {
+	ServiceReference[] srs = null;
+	try {
+	    srs = bundle.getServiceReferences(className, filter);
+	} catch (Exception e) {
+	}
+	if (srs == null || srs.length == 0)
+	    return null;
+	else {
+	    Object[] result = new Object[srs.length];
+	    for (int i = 0; i < srs.length; i++)
+		result[i] = bundle.getService(srs[i]);
+	    return result;
+	}
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#getAttribute(java.lang.String)
+     */
+    public Object getAttribute(String attrName) {
+	return (attrName == null) ? null : extension.get(attrName);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#getContainer()
+     */
+    public Container getContainer() {
+	return uAALBundleContainer.THE_CONTAINER;
+    }
+
+    public String getID() {
+	return bundle.getBundle().getSymbolicName();
+    }
+
+    public uAALBundleContext installBundle(String location) {
+	try {
+	    Bundle b = bundle.installBundle(location);
+	    return new uAALBundleContext(b.getBundleContext());
+	} catch (Exception e) {
+	    logError(this.getClass().getName() + "installBundle",
+		    "Exception while installing bundle at " + location, e);
+	    return null;
+	}
+    }
+
+    public uAALBundleContext installBundle(String location, InputStream is) {
+	try {
+	    Bundle b = bundle.installBundle(location, is);
+	    return new uAALBundleContext(b.getBundleContext());
+	} catch (Exception e) {
+	    logError(this.getClass().getName() + "installBundle",
+		    "Exception while installing bundle at " + location, e);
+	    return null;
+	}
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#listConfigFiles(org.universAAL.middleware.container.ModuleContext)
+     */
+    public File[] listConfigFiles(ModuleContext requester) {
+	// TODO check permissions
+	int n = confFiles.size();
+	File[] files = new File[n];
+	for (int i = 0; i < n; i++)
+	    files[i] = (File) ((Object[]) confFiles.get(i))[0];
+	return files;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#logDebug(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void logDebug(String tag, String message, Throwable t) {
+	logger.debug(tag + ": " + message, t);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#logError(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void logError(String tag, String message, Throwable t) {
+	logger.error(tag + ": " + message, t);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#logInfo(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void logInfo(String tag, String message, Throwable t) {
+	logger.info(tag + ": " + message, t);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#logWarn(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void logWarn(String tag, String message, Throwable t) {
+	logger.warn(tag + ": " + message, t);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#logTrace(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void logTrace(String tag, String message, Throwable t) {
+	logger.trace(tag + ": " + message, t);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#registerConfigFile(java.lang.Object[])
+     */
+    public void registerConfigFile(Object[] configFileParams) {
+	// TODO define a convention for the array param
+	// current assumption: 1st param @ index 0 is the
+	// org.osgi.framework.Constants.SERVICE_PID
+	// chosen for a org.osgi.service.cm.ManagedService (type = String)
+	// possible extensions:
+	// 2nd param @ index 1: help string describing the role of the property
+	// file indirectly specified by the first first param @ index 0
+	// 3rd param @ index 2: a hash-table with allowed properties as keys and
+	// a help string about each property as value
+	if (configFileParams != null && configFileParams.length > 0) {
+	    configFileParams[0] = servicesConfHome
+		    .getPropFile(configFileParams[0].toString());
+	    confFiles.add(configFileParams);
+	}
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#setAttribute(java.lang.String,
+     *      java.lang.Object)
+     */
+    public void setAttribute(String attrName, Object attrValue) {
+	if (attrName != null && attrValue != null)
+	    extension.put(attrName, attrValue);
+    }
+
+    public void shareObject(String xface, Object obj, Dictionary props) {
+	bundle.registerService(xface, obj, props);
+    }
+
+    public void shareObject(String[] xface, Object obj, Dictionary props) {
+	bundle.registerService(xface, obj, props);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#start(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean start(ModuleContext requester) {
+	if (canBeStarted(requester)) {
+	    try {
+		bundle.getBundle().start();
+		return true;
+	    } catch (Exception e) {
+		// TODO: log
+	    }
+	}
+	return false;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#stop(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean stop(ModuleContext requester) {
+	if (canBeStopped(requester)) {
+	    try {
+		bundle.getBundle().stop();
+		return true;
+	    } catch (Exception e) {
+		// TODO: log
+	    }
+	}
+	return false;
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.ModuleContext#uninstall(org.universAAL.middleware.container.ModuleContext)
+     */
+    public boolean uninstall(ModuleContext requester) {
+	if (canBeUninstalled(requester)) {
+	    try {
+		bundle.getBundle().uninstall();
+		return true;
+	    } catch (Exception e) {
+		// TODO: log
+	    }
+	}
+	return false;
+    }
+
+    private String[] getBundleList() {
+	Bundle[] bundles = bundle.getBundles();
+	String[] values = new String[bundles.length];
+	for (int i = 0; i < bundles.length; i++) {
+	    values[i] = bundles[i].getSymbolicName() + "-"
+		    + bundles[i].getHeaders(Constants.BUNDLE_VERSION);
+	}
+	return values;
+    }
+
+    public Object getProperty(String name) {
+
+	Object value = getAttribute(name);
+	if (value != null)
+	    return value;
+
+	value = bundle.getProperty(name);
+	if (value != null)
+	    return value;
+
+	value = System.getProperty(name);
+	if (value != null)
+	    return value;
+
+	value = System.getenv(name);
+	if (value != null)
+	    return value;
+
+	return null;
+    }
+
+    public Object getProperty(String name, Object def) {
+	Object value = getProperty(name);
+	if (value == null)
+	    return def;
+	return value;
+    }
+
+}
