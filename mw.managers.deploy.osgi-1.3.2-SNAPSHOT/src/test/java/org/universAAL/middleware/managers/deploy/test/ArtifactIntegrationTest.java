8aba0d54ecb6a9999cb9db4339a9a4bb3f26a4da
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 17:09:08 +0000

    Automatic tag of mw.managers.deploy.osgi version: 1.3.2-SNAPSHOT

---
+++ mw.managers.deploy.osgi-1.3.2-SNAPSHOT/src/test/java/org/universAAL/middleware/managers/deploy/test/ArtifactIntegrationTest.java
@@ -0,0 +1,16 @@
+package org.universAAL.middleware.managers.deploy.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIntegrationTest extends IntegrationTest {
+
+    public void testComposite() {
+	logAllBundles();
+    }
+}
