949f784dc1a1458c816043895d724a3b9de175de
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 17:08:12 +0000

    Automatic tag of mw.bus.service.aapi.core version: 1.3.2-SNAPSHOT

---
+++ mw.bus.service.aapi.core-1.3.2-SNAPSHOT/src/main/java/org/universAAL/middleware/api/annotation/ChangeEffect.java
@@ -0,0 +1,22 @@
+package org.universAAL.middleware.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used for generating ServiceProfile. For each of such annotation a
+ * separate addChangeEffect call is made on SimpleAPIService.
+ * 
+ * @author dzmuda
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ChangeEffect {
+    String[] propertyPaths();
+
+    String value();
+
+    Class<?> valueType();
+}
