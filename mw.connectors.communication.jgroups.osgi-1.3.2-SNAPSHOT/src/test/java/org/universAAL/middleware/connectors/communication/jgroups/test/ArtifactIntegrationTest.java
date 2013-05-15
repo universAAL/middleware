8ac17824719ced2070308c7de7d3507cfeeed2ac
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 17:08:58 +0000

    Automatic tag of mw.connectors.communication.jgroups.osgi version: 1.3.2-SNAPSHOT

---
+++ mw.connectors.communication.jgroups.osgi-1.3.2-SNAPSHOT/src/test/java/org/universAAL/middleware/connectors/communication/jgroups/test/ArtifactIntegrationTest.java
@@ -0,0 +1,24 @@
+package org.universAAL.middleware.connectors.communication.jgroups.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIntegrationTest extends IntegrationTest {
+
+    public ArtifactIntegrationTest() {
+
+	// // Arguments for MW2.0
+	setRunArguments("net.slp.multicastTimeouts", "500,750");
+	setRunArguments("java.net.preferIPv4Stack", "true");
+	setRunArguments("net.slp.port", "7000");
+    }
+
+    public void testComposite() {
+	logAllBundles();
+    }
+}
