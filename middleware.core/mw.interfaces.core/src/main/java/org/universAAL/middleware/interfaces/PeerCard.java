02fae88698c4bae03d7dfb928158d38e2bd9c890
Giancarlo Riolo <giancarlo.riolo@isti.cnr.it>
2013-06-13 14:32:44 +0000

    Added License Header CNR-ISTI

--- middleware.core/mw.interfaces.core/src/main/java/org/universAAL/middleware/interfaces/PeerCard.java
+++ middleware.core/mw.interfaces.core/src/main/java/org/universAAL/middleware/interfaces/PeerCard.java
@@ -18,6 +18,8 @@
         See the License for the specific language governing permissions and
         limitations under the License.
  */
+
+
 package org.universAAL.middleware.interfaces;
 
 import java.util.UUID;
