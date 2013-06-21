f5b57d25b2cb4739b2018768170cad0a6e2d81e6
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 13:15:47 +0000

    Automatic tag of mw.bus.service.aapi.core version: 2.0.0

---
+++ middleware.core/mw.bus.service.aapi.core/src/main/java/org/universAAL/middleware/api/annotation/UniversAALService.java
@@ -0,0 +1,46 @@
+/*
+Copyright 2011-2014 AGH-UST, http://www.agh.edu.pl
+Faculty of Computer Science, Electronics and Telecommunications
+Department of Computer Science 
+
+See the NOTICE file distributed with this work for additional
+information regarding copyright ownership
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.universAAL.middleware.api.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+/**
+ * Annotation which marks interface as UniversAALService. This means that all
+ * methods will be scanned for @ServiceOperation occurences and on its base a
+ * ServiceProfiles will be generated.
+ * 
+ * - namespace - it is a service provider namespace which act as a prefix for
+ * other annotation values - especially @Inputs, @Ouputs, and @ChangeEffects; -
+ * name - name of service provider. If it is not provided then a interface
+ * simple name is used as default.
+ * 
+ * @author dzmuda
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface UniversAALService {
+    String namespace();
+
+    String name() default "";
+}
