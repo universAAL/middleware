c0b47efaa1287bed993967810436882edd76239c
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 13:14:57 +0000

    Automatic tag of mw.data.representation.core version: 2.0.0

---
+++ middleware.core/mw.data.representation.core/src/main/java/org/universAAL/middleware/rdf/FinalizedResource.java
@@ -0,0 +1,111 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.rdf;
+
+import java.util.Enumeration;
+
+/**
+ * This class finalizes some of the methods of {@link Resource} so that they
+ * cannot be overwritten by subclasses.
+ * 
+ * @author Carsten Stockloew
+ */
+public class FinalizedResource extends Resource {
+
+    /** @see org.universAAL.middleware.rdf.Resource#Resource() */
+    protected FinalizedResource() {
+	super();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#Resource(boolean) */
+    public FinalizedResource(boolean isXMLLiteral) {
+	super(isXMLLiteral);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#Resource(String) */
+    protected FinalizedResource(String uri) {
+	super(uri);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#Resource(String, boolean) */
+    public FinalizedResource(String uri, boolean isXMLLiteral) {
+	super(uri, isXMLLiteral);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#Resource(String, int) */
+    protected FinalizedResource(String uriPrefix, int numProps) {
+	super(uriPrefix, numProps);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getProperty(String) */
+    public final Object getProperty(String propURI) {
+	return super.getProperty(propURI);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#numberOfProperties() */
+    public final int numberOfProperties() {
+	return super.numberOfProperties();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#isAnon() */
+    public final boolean isAnon() {
+	return super.isAnon();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#hasQualifiedName() */
+    public final boolean hasQualifiedName() {
+	return super.hasQualifiedName();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getURI() */
+    public final String getURI() {
+	return super.getURI();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getPropertyURIs() */
+    public final Enumeration getPropertyURIs() {
+	return super.getPropertyURIs();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#addType(String, boolean) */
+    public final boolean addType(String typeURI, boolean blockFurtherTypes) {
+	return super.addType(typeURI, blockFurtherTypes);
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getLocalName() */
+    public final String getLocalName() {
+	return super.getLocalName();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getNamespace() */
+    public final String getNamespace() {
+	return super.getNamespace();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getType() */
+    public final String getType() {
+	return super.getType();
+    }
+
+    /** @see org.universAAL.middleware.rdf.Resource#getTypes() */
+    public final String[] getTypes() {
+	return super.getTypes();
+    }
+}
---
+++ middleware.core/mw.data.representation.core/src/main/java/org/universAAL/middleware/util/GraphIteratorElement.java
@@ -0,0 +1,72 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.util;
+
+import java.util.List;
+import org.universAAL.middleware.rdf.Resource;
+
+public class GraphIteratorElement {
+    private Resource subject = null;
+    private String predicate = null;
+    private Object object = null;
+    private int depth = 0;
+    private boolean isList = false;
+    private int listIndex = 0;
+    private List theList = null;
+
+    public GraphIteratorElement(Resource subject, String predicate, Object object, int depth,
+	    boolean isList, int listIndex, List theList) {
+	this.subject = subject;
+	this.predicate = predicate;
+	this.object = object;
+	this.depth = depth;
+	this.isList = isList;
+	this.listIndex = listIndex;
+	this.theList = theList;
+    }
+    
+    public List getList() {
+	return theList;
+    }
+
+    public boolean isList() {
+	return isList;
+    }
+
+    public int getListIndex() {
+	return listIndex;
+    }
+
+    public int getDepth() {
+	return depth;
+    }
+
+    public Resource getSubject() {
+	return subject;
+    }
+
+    public String getPredicate() {
+	return predicate;
+    }
+
+    public Object getObject() {
+	return object;
+    }
+}
