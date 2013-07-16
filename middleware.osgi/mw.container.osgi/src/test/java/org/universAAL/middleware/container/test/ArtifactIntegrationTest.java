5feb4c5db6925e7e5e789905c865ca139c57e62e
Benny Sattler <benjamin.sattler@igd.fraunhofer.de>
2013-07-16 14:57:28 +0000

    * added unit tests for isssue [#269] "Conatiner OSGI: remote shared object"

--- middleware.osgi/mw.container.osgi/src/test/java/org/universAAL/middleware/container/test/ArtifactIntegrationTest.java
+++ middleware.osgi/mw.container.osgi/src/test/java/org/universAAL/middleware/container/test/ArtifactIntegrationTest.java
@@ -1,6 +1,9 @@
 package org.universAAL.middleware.container.test;
 
 import org.universAAL.itests.IntegrationTest;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.osgi.run.Activator;
 
 /**
  * Here developer's of this artifact should code their integration tests.
@@ -9,9 +12,71 @@ import org.universAAL.itests.IntegrationTest;
  * 
  */
 public class ArtifactIntegrationTest extends IntegrationTest {
+    
+    private class ShareObjectTestClass {
+	
+    }
 
     public void testComposite() {
 	logAllBundles();
     }
+    
+    public void testAddRemoveSharedObject() {
+   	ModuleContext mc = Activator.mc;
+   	ShareObjectTestClass o = new ShareObjectTestClass();
+   	uAALBundleContainer.THE_CONTAINER.shareObject(mc, o, new Object[]{o.getClass().getName()});
+   	uAALBundleContainer.THE_CONTAINER.removeSharedObject(mc,o, new Object[]{o.getClass().getName()});
+    }
+    
+    public void testRemoveSharedObjectTwice() {
+   	ModuleContext mc = Activator.mc;
+   	ShareObjectTestClass o = new ShareObjectTestClass();
+   	uAALBundleContainer.THE_CONTAINER.shareObject(mc, o, new Object[]{o.getClass().getName()});
+   	uAALBundleContainer.THE_CONTAINER.removeSharedObject(mc,o, new Object[]{o.getClass().getName()});
+   	try {
+   	    uAALBundleContainer.THE_CONTAINER.removeSharedObject(mc,o, new Object[]{o.getClass().getName()});
+   	} catch (Exception e) {
+   	   return;
+   	}
+   	fail();
+    }
+    
+    public void testRemoveNonexistentSharedObject() {
+   	ModuleContext mc = Activator.mc;  	
+   	ShareObjectTestClass o = new ShareObjectTestClass();
+   	try {
+   	    uAALBundleContainer.THE_CONTAINER.removeSharedObject(mc,o, new Object[]{o.getClass().getName()});
+   	} catch (Exception e) {
+   	   return;
+   	}
+   	fail();
+    }
 
+    public void testCheckReturnedSharedObject() {
+	ModuleContext mc = Activator.mc;
+   	ShareObjectTestClass o = new ShareObjectTestClass();
+   	uAALBundleContainer.THE_CONTAINER.shareObject(mc, o, new Object[]{o.getClass().getName()});
+   	Object re = null;
+   	
+   	try {
+   	    re = uAALBundleContainer.THE_CONTAINER.fetchSharedObject(mc, new Object[]{o.getClass().getName()});
+   	    if (re == null) {
+   		fail();
+   	    }
+   	    if (!o.getClass().isInstance(re)) {
+   		fail();
+   	    }
+   	} catch(Exception e) {
+   	    fail();
+   	}
+   	uAALBundleContainer.THE_CONTAINER.removeSharedObject(mc,o, new Object[]{o.getClass().getName()});
+   	re = null;
+   	re = uAALBundleContainer.THE_CONTAINER.fetchSharedObject(mc, new Object[]{o.getClass().getName()});
+   	if (re != null) {
+   	    fail();
+   	}
+   	if (o.getClass().isInstance(re)) {
+   	    fail();
+   	}
+    }
 }
