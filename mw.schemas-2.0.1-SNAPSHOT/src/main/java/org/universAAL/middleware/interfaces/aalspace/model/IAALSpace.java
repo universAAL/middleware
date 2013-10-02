95251bfb3273b7080a9b648ff636bab27576df2c
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-02 13:04:14 +0000

    Automatic tag of mw.schemas version: 2.0.1-SNAPSHOT

---
+++ mw.schemas-2.0.1-SNAPSHOT/src/main/java/org/universAAL/middleware/interfaces/aalspace/model/IAALSpace.java
@@ -0,0 +1,123 @@
+/*
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
+
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+package org.universAAL.middleware.interfaces.aalspace.model;
+
+import org.universAAL.middleware.interfaces.aalspace.xml.model.Aalspace;
+
+/**
+ *
+ *
+ * @author <a href="mailto:sterfano.lenzi@isti.cnr.it">Stefano Lenzi</a>
+ * @version $LastChangedRevision$ ($LastChangedDate$)
+ * @since 2.0.1
+ */
+public interface IAALSpace {
+
+    /**
+     * Gets the value of the spaceDescriptor property.
+     *
+     * @return
+     *     possible object is
+     *     {@link Aalspace.SpaceDescriptor }
+     *
+     */
+    public abstract ISpaceDescriptor getSpaceDescriptor();
+
+    /**
+     * Sets the value of the spaceDescriptor property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link Aalspace.SpaceDescriptor }
+     *
+     */
+    public abstract void setSpaceDescriptor(ISpaceDescriptor value);
+
+    public abstract boolean isSetSpaceDescriptor();
+
+    /**
+     * Gets the value of the peeringChannel property.
+     *
+     * @return
+     *     possible object is
+     *     {@link Aalspace.PeeringChannel }
+     *
+     */
+    public abstract IPeeringChannel getPeeringChannel();
+
+    /**
+     * Sets the value of the peeringChannel property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link Aalspace.PeeringChannel }
+     *
+     */
+    public abstract void setPeeringChannel(IPeeringChannel value);
+
+    public abstract boolean isSetPeeringChannel();
+
+    /**
+     * Gets the value of the communicationChannels property.
+     *
+     * @return
+     *     possible object is
+     *     {@link Aalspace.CommunicationChannels }
+     *
+     */
+    public abstract ICommunicationChannels getCommunicationChannels();
+
+    /**
+     * Sets the value of the communicationChannels property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link Aalspace.CommunicationChannels }
+     *
+     */
+    public abstract void setCommunicationChannels(
+            ICommunicationChannels value);
+
+    public abstract boolean isSetCommunicationChannels();
+
+    /**
+     * Gets the value of the security property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public abstract String getSecurity();
+
+    /**
+     * Sets the value of the security property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public abstract void setSecurity(String value);
+
+    public abstract boolean isSetSecurity();
+
+}
---
+++ mw.schemas-2.0.1-SNAPSHOT/src/main/java/org/universAAL/middleware/interfaces/aalspace/model/IChannelDescriptor.java
@@ -0,0 +1,71 @@
+package org.universAAL.middleware.interfaces.aalspace.model;
+
+public interface IChannelDescriptor {
+
+    /**
+     * Gets the value of the channelName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public abstract String getChannelName();
+
+    /**
+     * Sets the value of the channelName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public abstract void setChannelName(String value);
+
+    public abstract boolean isSetChannelName();
+
+    /**
+     * Gets the value of the channelURL property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public abstract String getChannelURL();
+
+    /**
+     * Sets the value of the channelURL property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public abstract void setChannelURL(String value);
+
+    public abstract boolean isSetChannelURL();
+
+    /**
+     * Gets the value of the channelValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public abstract String getChannelValue();
+
+    /**
+     * Sets the value of the channelValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public abstract void setChannelValue(String value);
+
+    public abstract boolean isSetChannelValue();
+
+}
