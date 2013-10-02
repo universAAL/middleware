74c9ec0c2ac891264e77287bb34e723044b36129
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-02 13:04:17 +0000

    Automatic tag of mw.interfaces.core version: 2.0.1-SNAPSHOT

---
+++ mw.interfaces.core-2.0.1-SNAPSHOT/src/main/java/org/universAAL/middleware/interfaces/PeerRole.java
@@ -0,0 +1,32 @@
+/*
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
+
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+
+package org.universAAL.middleware.interfaces;
+
+/**
+ * List of roles available for the Peers
+ * 
+ * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
+ * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
+ */
+public enum PeerRole {
+    COORDINATOR, AALSPACE_GATEWAY, SENSOR_NETWORK_GATEWAY, PEER
+}
