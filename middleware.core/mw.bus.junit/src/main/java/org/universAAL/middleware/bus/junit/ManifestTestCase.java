c4bd120f54559628c73865a79759fd388e872402
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-10-08 11:06:47 +0000

    renamed package for junit bus

--- middleware.core/mw.bus.junit/src/main/java/org/universAAL/middleware/bus/junit/ManifestTestCase.java
+++ middleware.core/mw.bus.junit/src/main/java/org/universAAL/middleware/bus/junit/ManifestTestCase.java
@@ -17,7 +17,7 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
  */
-package org.universAAL.container.JUnit;
+package org.universAAL.middleware.bus.junit;
 
 import java.io.IOException;
 import java.util.Iterator;
