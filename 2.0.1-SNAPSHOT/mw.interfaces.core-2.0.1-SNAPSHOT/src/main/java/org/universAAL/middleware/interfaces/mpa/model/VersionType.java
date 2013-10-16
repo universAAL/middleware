ed986b1691126fddec3087980450ecbde38be7cb
Stefano Lenzi <stefano.lenzi@isti.cnr.it>
2013-10-16 15:47:34 +0000

    Moved to better location the branch for the lifecycle

---
+++ 2.0.1-SNAPSHOT/mw.interfaces.core-2.0.1-SNAPSHOT/src/main/java/org/universAAL/middleware/interfaces/mpa/model/VersionType.java
@@ -0,0 +1,181 @@
+/*
+        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+        Institute of Information Science and Technologies
+        of the Italian National Research Council
+
+        See the NOTICE file distributed with this work for additional
+        information regarding copyright ownership
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+package org.universAAL.middleware.interfaces.mpa.model;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>
+ * Java class for VersionType complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType name="VersionType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="major" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="minor" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="micro" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="build" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="string" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "VersionType", propOrder = { "major", "minor", "micro",
+	"build", "string" })
+public class VersionType implements Serializable {
+
+    private final static long serialVersionUID = 12343L;
+    @XmlElement(defaultValue = "0")
+    protected int major;
+    @XmlElement(defaultValue = "0")
+    protected int minor;
+    @XmlElement(defaultValue = "0")
+    protected int micro;
+    protected String build;
+    @XmlElement(required = true)
+    protected String string;
+
+    /**
+     * Gets the value of the major property.
+     * 
+     */
+    public int getMajor() {
+	return major;
+    }
+
+    /**
+     * Sets the value of the major property.
+     * 
+     */
+    public void setMajor(int value) {
+	this.major = value;
+    }
+
+    public boolean isSetMajor() {
+	return true;
+    }
+
+    /**
+     * Gets the value of the minor property.
+     * 
+     */
+    public int getMinor() {
+	return minor;
+    }
+
+    /**
+     * Sets the value of the minor property.
+     * 
+     */
+    public void setMinor(int value) {
+	this.minor = value;
+    }
+
+    public boolean isSetMinor() {
+	return true;
+    }
+
+    /**
+     * Gets the value of the micro property.
+     * 
+     */
+    public int getMicro() {
+	return micro;
+    }
+
+    /**
+     * Sets the value of the micro property.
+     * 
+     */
+    public void setMicro(int value) {
+	this.micro = value;
+    }
+
+    public boolean isSetMicro() {
+	return true;
+    }
+
+    /**
+     * Gets the value of the build property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getBuild() {
+	return build;
+    }
+
+    /**
+     * Sets the value of the build property.
+     * 
+     * @param value
+     *            allowed object is {@link String }
+     * 
+     */
+    public void setBuild(String value) {
+	this.build = value;
+    }
+
+    public boolean isSetBuild() {
+	return (this.build != null);
+    }
+
+    /**
+     * Gets the value of the string property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getString() {
+	return string;
+    }
+
+    /**
+     * Sets the value of the string property.
+     * 
+     * @param value
+     *            allowed object is {@link String }
+     * 
+     */
+    public void setString(String value) {
+	this.string = value;
+    }
+
+    public boolean isSetString() {
+	return (this.string != null);
+    }
+
+}
