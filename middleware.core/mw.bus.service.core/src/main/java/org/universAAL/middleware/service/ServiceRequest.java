3b9c01c228fa5c7f4e7d2528308a7907d36e9974
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-10-23 17:38:24 +0000

    implemented matches()-method

--- middleware.core/mw.bus.service.core/src/main/java/org/universAAL/middleware/service/ServiceRequest.java
+++ middleware.core/mw.bus.service.core/src/main/java/org/universAAL/middleware/service/ServiceRequest.java
@@ -20,24 +20,24 @@
 package org.universAAL.middleware.service;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.universAAL.middleware.bus.model.matchable.Advertisement;
 import org.universAAL.middleware.bus.model.matchable.Matchable;
 import org.universAAL.middleware.bus.model.matchable.Request;
-import org.universAAL.middleware.bus.model.matchable.UtilityAdvertisement;
+import org.universAAL.middleware.bus.msg.BusMessage;
 import org.universAAL.middleware.owl.MergedRestriction;
 import org.universAAL.middleware.rdf.FinalizedResource;
 import org.universAAL.middleware.rdf.PropertyPath;
 import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.impl.ServiceMatcher;
+import org.universAAL.middleware.service.impl.ServiceWrapper;
 import org.universAAL.middleware.service.owl.Service;
 import org.universAAL.middleware.service.owls.process.OutputBinding;
 import org.universAAL.middleware.service.owls.process.ProcessOutput;
 import org.universAAL.middleware.service.owls.process.ProcessResult;
-import org.universAAL.middleware.service.owls.profile.ServiceProfile;
 
 /**
  * A class that represents a service request resource, which is used by the
@@ -338,7 +338,7 @@ public class ServiceRequest extends FinalizedResource implements Request {
      * {@link #addAggregatingFilter(AggregatingFilter)}. The service bus will be
      * the main user of this method.
      */
-    @SuppressWarnings( { "unchecked", "rawtypes" })
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public List<AggregatingFilter> getFilters() {
 	Object propAggregatingFilterList = props.get(PROP_AGGREGATING_FILTER);
 	if (propAggregatingFilterList instanceof List<?>) {
@@ -489,58 +489,26 @@ public class ServiceRequest extends FinalizedResource implements Request {
     }
 
     /**
-     * @see #matches(Advertisement)
+     * @see Matchable#matches(Matchable)
      */
     public boolean matches(Matchable other) {
-	return false;
-    }
-
-    /**
-     * Will never match (any matching advertisement would be of a (sub)-Type of
-     * {@link UtilityAdvertisement}), so <tt>false</tt> is returned.
-     * 
-     * @param advertisement
-     *            the advertisement to be matched against
-     * @return <tt>false</tt> as described above
-     */
-    public boolean matches(Advertisement advertisement) {
-	return false;
-    }
-
-    /**
-     * Switches over different types of {@link UtilityAdvertisement}, calls the
-     * appropriate methods for each type.
-     * 
-     * @param advertisement
-     *            the advertisement to be matched against
-     * @return <tt>true</tt> if the advertisement matches, <tt>false</tt> if not
-     */
-    public boolean matches(UtilityAdvertisement advertisement) {
-	if (advertisement instanceof ServiceProfile) {
-	    return isMatchingServiceProfile((ServiceProfile) advertisement);
-	} else {
+	// only ServiceRequests can match
+	if (!(other instanceof ServiceRequest))
 	    return false;
+	ServiceWrapper superset = ServiceWrapper.create(this);
+	ServiceWrapper subset = ServiceWrapper.create((ServiceRequest) other);
+
+	boolean res = new ServiceMatcher().matches(superset, subset,
+		new Hashtable(), null);
+
+	if (res == false) {
+	    System.out.println(" -- matching error: trying to match "
+		    + BusMessage.trySerializationAsContent(this)
+		    + "\n -- with: "
+		    + BusMessage.trySerializationAsContent(other));
+	    new ServiceMatcher().matches(superset, subset, new Hashtable(),
+		    null);
 	}
-    }
-
-    private boolean isMatchingServiceProfile(ServiceProfile advertisement) {
-	// TODO unimplemented method stub
-	return false;
-    }
-
-    @SuppressWarnings("unchecked")
-    public boolean matches(ServiceRequest other) {
-	boolean matches = true;
-	for (Object current : Collections.list(getPropertyURIs())) {
-	    String propertyURI = (String) current;
-	    Object thisProperty = getProperty(propertyURI);
-	    Object otherProperty = other.getProperty(propertyURI);
-
-	    // TODO add good matching algorithm
-	    if (!thisProperty.equals(otherProperty)) {
-		matches = false;
-	    }
-	}
-	return matches;
+	return res;
     }
 }
