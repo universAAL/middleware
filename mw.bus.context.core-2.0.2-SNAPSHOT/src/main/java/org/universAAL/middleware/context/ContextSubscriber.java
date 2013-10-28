827bb7e3d8ef5dc923cf8de54eddbd6f00531623
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-28 10:00:52 +0000

    Automatic tag of mw.bus.context.core version: 2.0.2-SNAPSHOT

---
+++ mw.bus.context.core-2.0.2-SNAPSHOT/src/main/java/org/universAAL/middleware/context/ContextSubscriber.java
@@ -0,0 +1,142 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.context;
+
+import org.universAAL.middleware.bus.model.AbstractBus;
+import org.universAAL.middleware.bus.member.Subscriber;
+import org.universAAL.middleware.bus.msg.BusMessage;
+import org.universAAL.middleware.bus.permission.AccessControl;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.context.impl.ContextBusImpl;
+
+/**
+ * Provides the interface to be implemented by context subscribers together with
+ * shared code. Only instances of this class can subscribe for context events.
+ * The convention of the context bus regarding the registration parameters is
+ * the following:
+ * <ul>
+ * <li>ContextPublishers provide only at the registration time info about
+ * themselves using
+ * {@link org.universAAL.middleware.context.owl.ContextProvider}.</li>
+ * <li>ContextSubscribers may pass an array of
+ * {@link org.universAAL.middleware.context.ContextEventPattern}s as their
+ * initial subscriptions and can always add new (and remove old) subscriptions
+ * dynamically.</li>
+ * </ul>
+ * 
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public abstract class ContextSubscriber extends Subscriber {
+
+    /**
+     * Creates a Context Subscriber and immediately registers a set of Context
+     * Event Patterns for it, so it receives the matching events.
+     * 
+     * @param connectingModule
+     *            The module context of the module creating the Publisher
+     * @param initialSubscriptions
+     *            Array of ContextEventPattern that are immediately registered
+     *            for this Subscriber
+     */
+    protected ContextSubscriber(ModuleContext connectingModule,
+	    ContextEventPattern[] initialSubscriptions) {
+	super(connectingModule, ContextBusImpl.getContextBusFetchParams());
+	initialSubscriptions = AccessControl.INSTANCE.checkPermission(owner,
+		getURI(), initialSubscriptions);
+	addNewRegParams(initialSubscriptions);
+    }
+
+    /**
+     * Registers more ContextEventPattern for this Subscriber in addition to
+     * those that might have passed initially
+     * 
+     * @param newSubscriptions
+     *            The additional array of ContextEventPattern
+     */
+    protected final void addNewRegParams(ContextEventPattern[] newSubscriptions) {
+	newSubscriptions = AccessControl.INSTANCE.checkPermission(owner,
+		getURI(), newSubscriptions);
+	((ContextBus) theBus).addNewRegParams(busResourceURI, newSubscriptions);
+    }
+
+    /**
+     * Unregisters a set of ContextEventPattern that had been previously
+     * registered for this Subscriber. The Subscriber will no longer receive
+     * Events matching these Patterns.
+     * 
+     * @param oldSubscriptions
+     */
+    protected final void removeMatchingRegParams(
+	    ContextEventPattern[] oldSubscriptions) {
+	((ContextBus) theBus).removeMatchingRegParams(busResourceURI,
+		oldSubscriptions);
+    }
+
+    /**
+     * Method to be called when the communication of the Subsccriber with the
+     * Context Bus is lost.
+     */
+    public abstract void communicationChannelBroken();
+
+    public final void busDyingOut(AbstractBus b) {
+	if (b == theBus)
+	    communicationChannelBroken();
+    }
+
+    /**
+     * Returns all provisions registered by all {@link ContextPublisher}s on all
+     * instances of context bus in the current AAL Space.
+     */
+    public ContextEventPattern[] getAllProvisions() {
+	return ((ContextBus) theBus).getAllProvisions(busResourceURI);
+    }
+
+    /**
+     * Method to be called when an Event forwarded in the Context Bus matches
+     * one of the Patterns registered by this Subscriber.
+     * 
+     * @param event
+     *            The Context Event that matched the registered Patterns
+     */
+    public abstract void handleContextEvent(ContextEvent event);
+
+    public final void handleEvent(BusMessage m) {
+	if (m.getContent() instanceof ContextEvent) {
+	    ContextEvent ce = (ContextEvent) (m.getContent());
+	    if (AccessControl.INSTANCE.checkPermission(owner, getURI(), ce)) {
+		LogUtils.logInfo(
+			owner,
+			ContextSubscriber.class,
+			"handleEvent",
+			new Object[] { busResourceURI,
+				" received context event:\n",
+				m.getContentAsString() }, null);
+		handleContextEvent((ContextEvent) m.getContent());
+	    }
+	}
+    }
+
+    public String getMyID() {
+	return busResourceURI;
+    }
+}
---
+++ mw.bus.context.core-2.0.2-SNAPSHOT/src/main/java/org/universAAL/middleware/context/impl/ContextBusImpl.java
@@ -0,0 +1,172 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.context.impl;
+
+import org.universAAL.middleware.bus.model.AbstractBus;
+import org.universAAL.middleware.bus.model.BusStrategy;
+import org.universAAL.middleware.bus.msg.BusMessage;
+import org.universAAL.middleware.bus.msg.MessageType;
+import org.universAAL.middleware.connectors.exception.CommunicationConnectorException;
+import org.universAAL.middleware.connectors.util.ChannelMessage;
+import org.universAAL.middleware.container.Container;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.context.ContextBus;
+import org.universAAL.middleware.context.ContextEvent;
+import org.universAAL.middleware.context.ContextEventPattern;
+import org.universAAL.middleware.context.ContextPublisher;
+import org.universAAL.middleware.context.ContextSubscriber;
+import org.universAAL.middleware.context.owl.ContextBusOntology;
+import org.universAAL.middleware.modules.CommunicationModule;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.util.Constants;
+import org.universAAL.middleware.util.ResourceComparator;
+
+/**
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public class ContextBusImpl extends AbstractBus implements ContextBus {
+
+    private static Object[] busFetchParams;
+    private static ContextBusImpl theContextBus;
+    private static ContextBusOntology contextBusOntology = new ContextBusOntology();
+
+    public static Object[] getContextBusFetchParams() {
+	return busFetchParams.clone();
+    }
+
+    public synchronized void assessContentSerialization(Resource content) {
+	if (Constants.debugMode()) {
+	    LogUtils
+		    .logDebug(
+			    context,
+			    ContextBusImpl.class,
+			    "assessContentSerialization",
+			    new Object[] { "Assessing message content serialization:" },
+			    null);
+	    // System.out.println(new RuntimeException().getStackTrace()[1]);
+
+	    String str = BusMessage.trySerializationAsContent(content);
+	    LogUtils
+		    .logDebug(
+			    context,
+			    ContextBusImpl.class,
+			    "assessContentSerialization",
+			    new Object[] { "\n      1. serialization dump\n",
+				    str,
+				    "\n      2. deserialize & compare with the original resource\n" },
+			    null);
+	    new ResourceComparator().printDiffs(content, (Resource) BusMessage
+		    .deserializeAsContent(str));
+	}
+    }
+
+    public static void startModule(Container c, ModuleContext mc,
+	    Object[] contextBusShareParams, Object[] contextBusFetchParams) {
+	if (theContextBus == null) {
+	    OntologyManagement.getInstance().register(mc, contextBusOntology);
+	    theContextBus = new ContextBusImpl(mc);
+	    busFetchParams = contextBusFetchParams;
+	    c.shareObject(mc, theContextBus, contextBusShareParams);
+	}
+    }
+
+    public static void stopModule() {
+	if (theContextBus != null) {
+	    OntologyManagement.getInstance().unregister(theContextBus.context, contextBusOntology);
+	    theContextBus.dispose();
+	    theContextBus = null;
+	}
+    }
+
+    private ContextBusImpl(ModuleContext mc) {
+	super(mc, "mw.bus.context.osgi");
+	busStrategy.setBus(this);
+    }
+
+    protected BusStrategy createBusStrategy(CommunicationModule commModule) {
+	return new ContextStrategy(commModule);
+    }
+
+    public void addNewRegParams(String memberID,
+	    ContextEventPattern[] registrParams) {
+	if (memberID != null && registrParams != null) {
+	    Object o = registry.getBusMemberByID(memberID);
+	    if (o instanceof ContextSubscriber) {
+		((ContextStrategy) busStrategy).addRegParams(
+			(ContextSubscriber) o, registrParams);
+	    } else if (o instanceof ContextPublisher) {
+		((ContextStrategy) busStrategy).addRegParams(
+			(ContextPublisher) o, registrParams);
+	    }
+	}
+    }
+
+    public ContextEventPattern[] getAllProvisions(String publisherID) {
+	if (publisherID != null) {
+	    Object o = registry.getBusMemberByID(publisherID);
+	    if (o instanceof ContextPublisher) {
+		return ((ContextStrategy) busStrategy)
+			.getAllProvisions((ContextPublisher) o);
+	    }
+	}
+	return null;
+    }
+
+    public void removeMatchingRegParams(String memberID,
+	    ContextEventPattern[] oldRegistrParams) {
+	if (memberID != null && oldRegistrParams != null) {
+	    Object o = registry.getBusMemberByID(memberID);
+	    if (o instanceof ContextSubscriber) {
+		((ContextStrategy) busStrategy).removeMatchingRegParams(
+			(ContextSubscriber) o, oldRegistrParams);
+	    } else if (o instanceof ContextPublisher) {
+		((ContextStrategy) busStrategy).removeMatchingRegParams(
+			(ContextPublisher) o, oldRegistrParams);
+	    }
+	}
+    }
+
+    public void brokerContextEvent(String publisherID, ContextEvent msg) {
+	assessContentSerialization(msg);
+	if (publisherID != null) {
+	    super.brokerMessage(publisherID, new BusMessage(MessageType.event,
+		    msg, this));
+	}
+    }
+
+    public void unregister(String publisherID, ContextPublisher publisher) {
+	super.unregister(publisherID, publisher);
+	((ContextStrategy) busStrategy).removeRegParams(publisher);
+    }
+
+    public void unregister(String subscriberID, ContextSubscriber subscriber) {
+	super.unregister(subscriberID, subscriber);
+	((ContextStrategy) busStrategy).removeRegParams(subscriber);
+    }
+
+    public void handleSendError(ChannelMessage message,
+	    CommunicationConnectorException e) {
+	// TODO Auto-generated method stub
+    }
+}
