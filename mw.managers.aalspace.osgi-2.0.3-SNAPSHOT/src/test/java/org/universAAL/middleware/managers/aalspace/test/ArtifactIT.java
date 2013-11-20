981fa3d5d6a320a9e49472b0c04190df4baff5fc
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-11-20 13:35:24 +0000

    Automatic tag of mw.managers.aalspace.osgi version: 2.0.3-SNAPSHOT

---
+++ mw.managers.aalspace.osgi-2.0.3-SNAPSHOT/src/test/java/org/universAAL/middleware/managers/aalspace/test/ArtifactIT.java
@@ -0,0 +1,22 @@
+package org.universAAL.middleware.managers.aalspace.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIT extends IntegrationTest {
+
+    public ArtifactIT() {
+	setRunArguments("net.slp.port", "7000");
+	setRunArguments("net.slp.multicastTimeouts", "500,750");
+	setRunArguments("java.net.preferIPv4Stack", "true");
+    }
+
+    public void testComposite() {
+	logAllBundles();
+    }
+}
