cb10fa8c27ed56adee9d5ec6ecd110cef443a882
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 13:38:51 +0000

    Automatic tag of jslp.core version: 3.0.0

---
+++ thirdparty/jslp.core/src/main/java/ch/ethz/iks/slp/impl/RequestMessage.java
@@ -0,0 +1,52 @@
+/* Copyright (c) 2005-2008 Jan S. Rellermeyer
+ * Systems Group,
+ * Department of Computer Science, ETH Zurich.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of ETH Zurich nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package ch.ethz.iks.slp.impl;
+
+import java.util.List;
+
+/**
+ * Abstract base class for all request messages.
+ *
+ * @author Jan S. Rellermeyer, ETH Zurich
+ * @since 0.1
+ */
+abstract class RequestMessage extends SLPMessage {
+    /**
+     * the list of previous responders. If a peer receives a request message and
+     * is already in the previous responder list, it will silently drop the
+     * message.
+     */
+    List prevRespList;
+
+    /**
+     * a list of scopes that will be included.
+     */
+    List scopeList;
+    
+}
