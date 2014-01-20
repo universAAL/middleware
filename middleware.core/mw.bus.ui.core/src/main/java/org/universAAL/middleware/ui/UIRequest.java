30a5adb4f91bc1f95d7189847feee745c26a7aa8
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 13:39:26 +0000

    Automatic tag of mw.bus.ui.core version: 3.0.0

---
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/UIRequest.java
@@ -0,0 +1,383 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	Copyright 2013-2014 Ericsson Nikola Tesla d.d., www.ericsson.com/hr/
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.ui;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import org.universAAL.middleware.bus.model.matchable.Matchable;
+import org.universAAL.middleware.bus.model.matchable.Request;
+import org.universAAL.middleware.owl.supply.AbsLocation;
+import org.universAAL.middleware.owl.supply.LevelRating;
+import org.universAAL.middleware.rdf.FinalizedResource;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.owl.AccessImpairment;
+import org.universAAL.middleware.ui.owl.DialogType;
+import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.middleware.ui.owl.Preference;
+import org.universAAL.middleware.ui.owl.PrivacyLevel;
+import org.universAAL.middleware.owl.ComparableIndividual;
+import org.universAAL.middleware.ui.rdf.Form;
+
+/**
+ * Instances of this class can be used to user interaction requests.
+ * Applications just need to provide the dialog's {@link Form} object along with
+ * the addressed {@link User}, the priority of the dialog, and the language and
+ * privacy level of the content. The {@link IUIBus} will then add the current
+ * adaptation parameters to the call (by asking the Dialog Manager) before
+ * selecting the appropriate {@link UIHandler} and forwarding the call to it.
+ * 
+ * @author mtazari
+ * @author eandgrg
+ * @author Carsten Stockloew
+ */
+public class UIRequest extends FinalizedResource implements Request {
+
+    /** The Constant uAAL_UI_NAMESPACE. */
+    public static final String uAAL_UI_NAMESPACE = uAAL_NAMESPACE_PREFIX
+	    + "UI.owl#";
+
+    /** The Constant MY_URI. */
+    public static final String MY_URI = uAAL_UI_NAMESPACE + "UIRequest";
+
+    // ////////////////////////////////////////////////////////////////
+    // Data added by applications when creating UIRequest
+    // ////////////////////////////////////////////////////////////////
+    /** The Constant PROP_ADDRESSED_USER. */
+    public static final String PROP_ADDRESSED_USER = uAAL_UI_NAMESPACE
+	    + "addressedUser";
+
+    /** The Constant PROP_DIALOG_FORM. */
+    public static final String PROP_DIALOG_FORM = uAAL_UI_NAMESPACE
+	    + "dialogForm";
+
+    /** The Constant PROP_DIALOG_PRIORITY. */
+    public static final String PROP_DIALOG_PRIORITY = uAAL_UI_NAMESPACE
+	    + "dialogPriority";
+
+    /** The Constant PROP_DIALOG_LANGUAGE. */
+    public static final String PROP_DIALOG_LANGUAGE = uAAL_UI_NAMESPACE
+	    + "dialogLanguage";
+
+    /** The Constant PROP_DIALOG_PRIVACY_LEVEL. */
+    public static final String PROP_DIALOG_PRIVACY_LEVEL = uAAL_UI_NAMESPACE
+	    + "dialogPrivacyLevel";
+
+    // ///////////////////////////////////////////////////////////////
+    // Additional data (added by ui.dm)
+    // ///////////////////////////////////////////////////////////////
+
+    /** The Constant PROP_PRESENTATION_LOCATION. */
+    public static final String PROP_PRESENTATION_LOCATION = uAAL_UI_NAMESPACE
+	    + "presentationLocation";
+
+    /** The Constant PROP_HAS_ACCESS_IMPAIRMENT. */
+    public static final String PROP_HAS_ACCESS_IMPAIRMENT = uAAL_UI_NAMESPACE
+	    + "hasAccessImpairment";
+
+    /** The Constant PROP_HAS_PREFERENCE. */
+    public static final String PROP_HAS_PREFERENCE = uAAL_UI_NAMESPACE
+	    + "hasPreference";
+
+    // also obtainable by accessing
+    // InteractionPreferences.preferredModality contained within
+    // uiRequest.getProperty(
+    // UIPreferencesSubProfile.PROP_INTERACTION_PREFERENCES) but this eases the
+    // checks in UIHandlerProfile.getMatchingDegree()
+    public static final String PROP_PRESENTATION_MODALITY = uAAL_UI_NAMESPACE
+	    + "presentationModality";
+
+    public static final String PROP_PRESENTATION_MODALITY_ALT = uAAL_UI_NAMESPACE
+	    + "altPresentationModality";
+
+    /**
+     * This constructor is for the exclusive usage by deserializers.
+     */
+    public UIRequest() {
+	super();
+    }
+
+    /**
+     * Instantiates a new {@link UIRequest}.
+     * 
+     * @param user
+     *            the {@link User}. It is declared as Resource because the type User is defined 
+     *            in the Profiling Ontology. The type is not needed for for matchmaking Either.
+     * @param dialogForm
+     *            The dialog form.
+     * @param dialogPriority
+     *            The dialog priority.
+     * @param dialogLang
+     *            the dialog {@link Language}.
+     * @param dialogPrivacy
+     *            the dialog privacy.
+     */
+    public UIRequest(Resource user, Form dialogForm,
+	    LevelRating dialogPriority, Locale dialogLang,
+	    PrivacyLevel dialogPrivacy) {
+	super();
+
+	addType(MY_URI, true);
+	props.put(PROP_ADDRESSED_USER, user);
+	props.put(PROP_DIALOG_FORM, dialogForm);
+	props.put(PROP_DIALOG_PRIORITY,
+		dialogPriority == null ? LevelRating.low : dialogPriority);
+	props.put(PROP_DIALOG_LANGUAGE, dialogLang);
+	props.put(PROP_DIALOG_PRIVACY_LEVEL, dialogPrivacy);
+    }
+
+    /**
+     * @see org.universAAL.middleware.rdf.Resource#getPropSerializationType(java.lang.String)
+     */
+    @Override
+    public int getPropSerializationType(String propURI) {
+	return PROP_DIALOG_FORM.equals(propURI) ? PROP_SERIALIZATION_FULL
+		: PROP_SERIALIZATION_REDUCED;
+    }
+
+    /**
+     * Gets the addressed {@link User}.
+     * 
+     * @return the addressed {@link User}, It is declared as Resource because the type User is defined 
+     *            in the Profiling Ontology. The type is not needed for for matchmaking Either.
+     */
+    public Resource getAddressedUser() {
+	return (Resource) props.get(PROP_ADDRESSED_USER);
+    }
+
+    /**
+     * Gets the dialog {@link Form}.
+     * 
+     * @return the dialog {@link Form}
+     */
+    public Form getDialogForm() {
+	return (Form) props.get(PROP_DIALOG_FORM);
+    }
+
+    /**
+     * Gets the dialog id.
+     * 
+     * @return the dialog id
+     */
+    public String getDialogID() {
+	Form f = getDialogForm();
+	return f == null ? null : f.getDialogID();
+    }
+
+    /**
+     * Gets the dialog language.
+     * 
+     * @return the dialog language
+     */
+    public Locale getDialogLanguage() {
+	return (Locale) props.get(PROP_DIALOG_LANGUAGE);
+    }
+
+    /**
+     * Gets the dialog priority.
+     * 
+     * @return the dialog priority
+     */
+    public LevelRating getDialogPriority() {
+	return (LevelRating) props.get(PROP_DIALOG_PRIORITY);
+    }
+
+    /**
+     * Gets the dialog privacy level.
+     * 
+     * @return the dialog privacy level
+     */
+    public PrivacyLevel getDialogPrivacyLevel() {
+	return (PrivacyLevel) props.get(PROP_DIALOG_PRIVACY_LEVEL);
+    }
+
+    /**
+     * Gets the dialog type.
+     * 
+     * @return the dialog type
+     */
+    public DialogType getDialogType() {
+	Form f = getDialogForm();
+	return f == null ? null : f.getDialogType();
+    }
+
+    /**
+     * Gets the impairments.
+     * 
+     * @return the impairments
+     */
+    public AccessImpairment[] getImpairments() {
+	List l = (List) props.get(PROP_HAS_ACCESS_IMPAIRMENT);
+	return l == null ? null : (AccessImpairment[]) l
+		.toArray(new AccessImpairment[l.size()]);
+    }
+
+    /**
+     * Gets the Preferences.
+     * 
+     * @return the preferences
+     */
+    public Preference[] getPreferences() {
+	List l = (List) props.get(PROP_HAS_PREFERENCE);
+	return l == null ? null : (Preference[]) l.toArray(new Preference[l
+		.size()]);
+    }
+
+    /**
+     * Gets the presentation location.
+     * 
+     * @return the presentation location
+     */
+    public AbsLocation getPresentationLocation() {
+	return (AbsLocation) props.get(PROP_PRESENTATION_LOCATION);
+    }
+
+    /**
+     * Sets the collected input.
+     * 
+     * @param data
+     *            the new collected input, The Resource set as form Root data.
+     */
+    public void setCollectedInput(Resource data) {
+	Form f = getDialogForm();
+	if (f != null) {
+	    f.substituteData(data);
+	}
+    }
+
+    /**
+     * Sets the impairments.
+     * 
+     * @param impairments
+     *            the new impairments
+     */
+    public void setImpairments(AccessImpairment[] impairments) {
+	if (impairments != null && impairments.length > 0
+		&& !props.containsKey(PROP_HAS_ACCESS_IMPAIRMENT)) {
+	    props.put(PROP_HAS_ACCESS_IMPAIRMENT, Arrays.asList(impairments));
+	}
+    }
+
+    /**
+     * Sets the preferences.
+     * 
+     * @param preferences
+     *            the new preferences
+     */
+    public void setPreferences(Preference[] preferences) {
+	if (preferences != null && preferences.length > 0
+		&& !props.containsKey(PROP_HAS_PREFERENCE)) {
+	    props.put(PROP_HAS_PREFERENCE, Arrays.asList(preferences));
+	}
+    }
+
+    /**
+     * Sets the presentation location.
+     * 
+     * @param presentationLocation
+     *            the new presentation location
+     */
+    public void setPresentationLocation(AbsLocation presentationLocation) {
+	if (presentationLocation != null
+		&& !props.containsKey(PROP_PRESENTATION_LOCATION)) {
+	    props.put(PROP_PRESENTATION_LOCATION, presentationLocation);
+	}
+    }
+
+    /**
+     * Sets the presentation modality.
+     * 
+     * @param modality
+     *            the new presentation modality
+     */
+    public void setPresentationModality(Modality modality) {
+	if (modality != null && !props.containsKey(PROP_PRESENTATION_MODALITY)) {
+	    props.put(PROP_PRESENTATION_MODALITY, modality);
+	}
+    }
+
+    /**
+     * Gets the alternative presentation modality.
+     * 
+     * @return the alternative presentation modality
+     */
+    public Modality getAltPresentationModality() {
+	return (Modality) props.get(PROP_PRESENTATION_MODALITY_ALT);
+    }
+
+    /**
+     * Sets the alternative presentation modality.
+     * 
+     * @param outputModality
+     *            the new alternative presentation modality
+     */
+    public void setAltPresentationModality(Modality outputModality) {
+	if (outputModality != null
+		&& !props.containsKey(PROP_PRESENTATION_MODALITY_ALT)) {
+	    props.put(PROP_PRESENTATION_MODALITY_ALT, outputModality);
+	}
+    }
+
+    /**
+     * @see Matchable#matches(Matchable)
+     */
+    public boolean matches(Matchable subset) {
+	if (!(subset instanceof UIRequest))
+	    return false;
+	UIRequest sub = (UIRequest) subset;
+	Object o1, o2;
+
+	o1 = getAddressedUser();
+	if (o1 != null) {
+	    o2 = sub.getAddressedUser();
+	    if (o2 != null && !(o1.equals(o2)))
+		return false;
+	}
+
+	o1 = getDialogPriority();
+	if (o1 != null) {
+	    o2 = sub.getDialogPriority();
+	    if (o2 != null
+		    && !(((LevelRating) o1).greaterEqual((LevelRating) o2)))
+		return false;
+	}
+
+	o1 = getDialogLanguage();
+	if (o1 != null) {
+	    o2 = sub.getDialogLanguage();
+	    if (o2 != null && !(o1.equals(o2)))
+		return false;
+	}
+
+	o1 = getDialogPrivacyLevel();
+	if (o1 != null) {
+	    o2 = sub.getDialogPrivacyLevel();
+	    if (o2 != null
+		    && !(((ComparableIndividual) o1)
+			    .greaterEqual((ComparableIndividual) o2)))
+		return false;
+	}
+
+	return true;
+    }
+}
---
+++ middleware.core/mw.bus.ui.core/src/main/java/org/universAAL/middleware/ui/impl/generic/CoordinatedStrategy.java
@@ -0,0 +1,405 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.middleware.ui.impl.generic;
+
+import java.util.Random;
+
+import org.universAAL.middleware.bus.msg.BusMessage;
+import org.universAAL.middleware.container.SharedObjectListener;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.interfaces.PeerCard;
+import org.universAAL.middleware.interfaces.aalspace.AALSpaceDescriptor;
+import org.universAAL.middleware.interfaces.aalspace.AALSpaceStatus;
+import org.universAAL.middleware.managers.api.AALSpaceListener;
+import org.universAAL.middleware.managers.api.AALSpaceManager;
+import org.universAAL.middleware.modules.CommunicationModule;
+import org.universAAL.middleware.owl.Ontology;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+
+/**
+ * Strategy stack dedicated only to manage Coordination of the bus.
+ * 
+ * <br>
+ * This strategy consists of 3 basic events:
+ * <ol>
+ * <li>Coordinator_Broadcast : used to announce coordinator and respond to
+ * coordinator requests
+ * <li>Coordinator_Request: used to request a coordinator when there is not one
+ * yet
+ * <li>Coordinator_Resign: used by the coordinator instance to resign from being
+ * a coordinator.
+ * </ol>
+ * <center> <img style="background-color:white;" src="doc-files/CoordinatedStrategy.png"
+ * alt="UIStrategy messages" width="70%"/> </center>
+ * The strategy subscribes to the {@link AALSpaceManager} to listen to 
+ * {@link CoordinatedStrategy#peerLost(PeerCard) lost peers}, in case they are the coordinator;
+ * or if the Coordinator has left the coordinated space, it automatically surrenders coordination.
+ * Also {@link CoordinatedStrategy#newPeerJoined(PeerCard) new Peers} joining the space will proactively be informed about who is the coordinator.
+ * @author amedrano
+ * 
+ */
+public class CoordinatedStrategy extends CallBasedStrategy implements AALSpaceListener, SharedObjectListener{
+
+    public static final String TYPE_uAAL_UI_BUS_COORDINATOR_BROADCAST = Resource.uAAL_VOCABULARY_NAMESPACE
+	    + "IamCoordinator";
+
+    public static final String TYPE_uAAL_UI_BUS_COORDINATOR_REQUEST = Resource.uAAL_VOCABULARY_NAMESPACE
+	    + "WhoIsCoordinator";
+
+    public static final String TYPE_uAAL_UI_BUS_COORDINATOR_RESIGN = Resource.uAAL_VOCABULARY_NAMESPACE
+	    + "CoordinatorResign";
+
+    private class CoordinatorAnnounceEvent extends Resource implements
+	    EventMessage<CoordinatedStrategy> {
+
+	public CoordinatorAnnounceEvent() {
+	    addType(TYPE_uAAL_UI_BUS_COORDINATOR_BROADCAST, true);
+	}
+
+	/** {@ inheritDoc} */
+	public void onReceived(CoordinatedStrategy strategy, BusMessage m,
+		String senderID) {
+
+	    // this should be a notification from the side of the
+	    // coordinator announcing its role
+	    // => this is the announcement
+	    PeerCard solicitor = m.getSender();
+	    if (solicitor != coordinatorPeer && iAmCoordinator()) {
+		// WOW! another peer has asked to be coordinator!!
+		LogUtils.logInfo(busModule, getClass(), "handle",
+			"detected multiple peers requesing to be coordinator");
+		Random r = new Random(bus.getPeerCard().getPeerID().hashCode());
+		int waitms = 50 + r.nextInt() % 950;
+		if(waitms<50)waitms=50;//Just in case it becomes <0 (!?)
+		try {
+		    // wait between 50 and 1000 ms
+		    Thread.sleep(waitms);
+		    // then retry
+		    requestBecomeACoordinator();
+		} catch (InterruptedException e) {
+		} catch (CoordinatorAlreadyExistsException e) {
+		    LogUtils.logInfo(busModule, getClass(), "handle",
+			    "rejected on second attempt, could not become Coordinator");
+		    sendEventToRemoteBusMember(new CoordinatorResignEvent());
+		}
+	    } else {
+		synchronized (CoordinatedStrategy.this) {
+		    setCoordinatorPeer(solicitor);
+		    CoordinatedStrategy.this.notifyAll();
+		}
+	    }
+	}
+    }
+
+    private class CoordinatorRequestEvent extends Resource implements
+	    EventMessage<CoordinatedStrategy> {
+
+	public CoordinatorRequestEvent() {
+	    addType(TYPE_uAAL_UI_BUS_COORDINATOR_REQUEST, true);
+	}
+
+	/** {@ inheritDoc} */
+	public void onReceived(CoordinatedStrategy strategy, BusMessage m,
+		String senderID) {
+	    if (iAmCoordinator()) {
+		sendEventToRemoteBusMember(new CoordinatorAnnounceEvent());
+	    }
+	}
+    }
+
+    private class CoordinatorResignEvent extends Resource implements
+	    EventMessage<CoordinatedStrategy> {
+
+	public CoordinatorResignEvent() {
+	    addType(TYPE_uAAL_UI_BUS_COORDINATOR_RESIGN, true);
+	}
+
+	/** {@ inheritDoc} */
+	public void onReceived(CoordinatedStrategy strategy, BusMessage m,
+		String senderID) {
+	    if (coordinatorPeer == m.getSender()) {
+		setCoordinatorPeer(null);
+	    }
+	}
+    }
+
+    private class CoordinatorMessageFactory implements ResourceFactory {
+
+	/** {@ inheritDoc}	 */
+	public Resource createInstance(String classURI, String instanceURI,
+		int factoryIndex) {
+	    switch (factoryIndex) {
+	    case 0:
+		return new CoordinatorAnnounceEvent();
+	    case 1:
+		return new CoordinatorResignEvent();
+	    case 2:
+		return new CoordinatorRequestEvent();
+	    default:
+		break;
+	    }
+	    return null;
+	}
+	
+    }
+    
+    private class CoordinatorMessageOnt extends Ontology{
+	private CoordinatorMessageFactory factory;
+
+	/**
+	 * @param ontURI
+	 */
+	public CoordinatorMessageOnt(String ontURI) {
+	    super(ontURI);
+	    factory = new CoordinatorMessageFactory();
+	}
+
+	/** {@ inheritDoc}	 */
+	public void create() {
+	    createNewRDFClassInfo(TYPE_uAAL_UI_BUS_COORDINATOR_BROADCAST, factory, 0);
+	    createNewRDFClassInfo(TYPE_uAAL_UI_BUS_COORDINATOR_RESIGN, factory, 1);
+	    createNewRDFClassInfo(TYPE_uAAL_UI_BUS_COORDINATOR_REQUEST, factory, 2);
+	}
+    }
+    
+    /**
+     * Exception indicating there is already a Coordinator.
+     * 
+     * @author amedrano
+     */
+    public static class CoordinatorAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	PeerCard existingCoordinator;
+
+	/**
+	 * @return the existingCoordinator
+	 */
+	public PeerCard getExistingCoordinator() {
+	    return existingCoordinator;
+	}
+
+	/**
+	 * @param existingCoordinator
+	 *            the existingCoordinator to set
+	 */
+	public void setExistingCoordinator(PeerCard existingCoordinator) {
+	    this.existingCoordinator = existingCoordinator;
+	}
+
+    }
+
+    private PeerCard coordinatorPeer;
+
+    /**
+     * @param coordinatorPeer the coordinatorPeer to set
+     */
+    private final void setCoordinatorPeer(PeerCard coordinatorPeer) {
+        this.coordinatorPeer = coordinatorPeer;
+        if (this.coordinatorPeer == null){
+            lostCoordinator();
+        }
+    }
+
+
+    /**
+     * Extending classes may override this method to perform 
+     * any opertation needed when the coordinator is lost.
+     */
+    protected void lostCoordinator() {  }
+
+    private CoordinatorMessageOnt ontology;
+
+    /**
+     * @param commModule
+     */
+    public CoordinatedStrategy(CommunicationModule commModule) {
+	super(commModule);
+    }
+
+    /**
+     * @param commModule
+     * @param name
+     */
+    public CoordinatedStrategy(CommunicationModule commModule, String name) {
+	super(commModule, name);
+    }
+
+
+    
+    /** {@ inheritDoc}	 */
+    public synchronized void start() {
+	super.start();
+	ontology = new CoordinatorMessageOnt(Resource.uAAL_NAMESPACE_PREFIX + "CoordinatedStrategyMessageOntology");
+	OntologyManagement.getInstance().register(busModule, ontology);
+	if (busModule.getContainer() != null) {
+	    Object o = busModule.getContainer().fetchSharedObject(busModule,
+		    new Object[] { AALSpaceManager.class.getName() }, this);
+	    sharedObjectAdded(o, null);
+	}
+    }
+
+    protected final synchronized boolean iAmCoordinator() {
+	return coordinatorPeer != null
+		&& coordinatorPeer.equals(bus.getPeerCard());
+    }
+
+    /**
+     * Check the conditions and announce this Peer as coordinator.
+     * 
+     * @throws CoordinatorAlreadyExistsException
+     */
+    protected final void requestBecomeACoordinator()
+	    throws CoordinatorAlreadyExistsException {
+	if (coordinatorPeer != null && coordinatorPeer != bus.getPeerCard()) {
+	    CoordinatorAlreadyExistsException ex = new CoordinatorAlreadyExistsException();
+	    ex.setExistingCoordinator(coordinatorPeer);
+	    throw ex;
+	}
+	if (coordinatorPeer == null) {
+	    setCoordinatorPeer(bus.getPeerCard());
+	    sendEventToRemoteBusMember(new CoordinatorAnnounceEvent());
+	}
+	if (!iAmCoordinator()) {
+	    CoordinatorAlreadyExistsException ex = new CoordinatorAlreadyExistsException();
+	    ex.setExistingCoordinator(coordinatorPeer);
+	    throw ex;
+	} else {
+	    synchronized (this) {
+		notifyAll();
+	    }
+	}
+    }
+
+    /**
+     * Check and announce this Peer is no longer the coordinator.
+     * 
+     * @throws CoordinatorAlreadyExistsException
+     *             in case this peer is not the coordinator.
+     */
+    protected final void resignFromCoordinator()
+	    throws CoordinatorAlreadyExistsException {
+	if (coordinatorPeer != null && coordinatorPeer != bus.getPeerCard()) {
+	    CoordinatorAlreadyExistsException ex = new CoordinatorAlreadyExistsException();
+	    ex.setExistingCoordinator(coordinatorPeer);
+	    throw ex;
+	}
+	if (coordinatorPeer != null) {
+	    setCoordinatorPeer(null);
+	    sendEventToRemoteBusMember(new CoordinatorResignEvent());
+	}
+    }
+
+    public final PeerCard getCoordinator() {
+	if (coordinatorPeer != null) {
+	    return coordinatorPeer;
+	}
+	// let's see if any other instance claims to be the coordinator
+	synchronized (this) {
+	    while (coordinatorPeer == null) {
+		sendEventToRemoteBusMember(new CoordinatorRequestEvent());
+		try {
+		    // now wait until either the reply comes and notifies me
+		    // or the dialog manager connects which will also lead
+		    // to awakening this thread
+		    wait();
+		} catch (Exception e) {
+		}
+	    }
+	}
+	return coordinatorPeer;
+    }
+
+    /** {@ inheritDoc}	 */
+    public void aalSpaceJoined(AALSpaceDescriptor spaceDescriptor) {
+	/*
+	 * NOTHING, wait for notification of the coordinator or for 
+	 * one of the busmembers to request a coordinator
+	 */
+    }
+
+    /** {@ inheritDoc}	 */
+    public void aalSpaceLost(AALSpaceDescriptor spaceDescriptor) {
+	/*
+	 * I have left the AALSPace. If i was the coordinator, no longer
+	 */
+	if (iAmCoordinator()){
+	    synchronized (this) {
+		setCoordinatorPeer(null);
+		LogUtils.logInfo(busModule, getClass(), "peerLost", "Lost the space to Coordinate.");
+		// TODO give a notification? so coordination may be reattempted.
+	    }
+	}
+    }
+
+    /** {@ inheritDoc}	 */
+    public void newPeerJoined(PeerCard peer) {
+	/*
+	 * A new Peer has joined, If i am the coordinator 
+	 * show him that I am the king of the place
+	 */
+	if (iAmCoordinator() && peer != bus.getPeerCard()){
+	    //wellcome
+	    sendEventToRemoteBusMember(peer, new CoordinatorAnnounceEvent());
+	    // kneel before my presence!
+	    // muahahaHAHA
+	}
+    }
+
+    /** {@ inheritDoc}	 */
+    public void peerLost(PeerCard peer) {
+	/*
+	 * If the lost peer is the coordinator, well have to wait for a new one
+	 */
+	if (peer == coordinatorPeer){
+	    setCoordinatorPeer(null);
+	    LogUtils.logInfo(busModule, getClass(), "peerLost", "Lost the Coordinator.");
+	}
+    }
+
+    /** {@ inheritDoc}	 */
+    public void aalSpaceStatusChanged(AALSpaceStatus status) {}
+
+    /** {@ inheritDoc}	 */
+    public void sharedObjectAdded(Object sharedObj, Object removeHook) {
+	if (sharedObj instanceof AALSpaceManager){
+	    ((AALSpaceManager)sharedObj).addAALSpaceListener(this);
+	}
+    }
+
+    /** {@ inheritDoc}	 */
+    public void sharedObjectRemoved(Object removeHook) { }
+
+    /** Tearing down */
+    public void close() {
+	if (iAmCoordinator()){
+	    sendEventToRemoteBusMember(new CoordinatorResignEvent());
+	}
+	OntologyManagement.getInstance().unregister(busModule, ontology);
+	Object o = busModule.getContainer().fetchSharedObject(busModule, new Object[]{AALSpaceManager.class.getName()});
+	if (o instanceof AALSpaceManager){
+	    ((AALSpaceManager)o).removeAALSpaceListener(this);
+	}
+    }
+    
+}
