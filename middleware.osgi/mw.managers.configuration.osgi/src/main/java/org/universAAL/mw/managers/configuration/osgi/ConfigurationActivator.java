bc951aeb32bd3c858eba15bf15c5d17588b4625c
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-02-14 16:03:55 +0000

    added configuration manager osgi.

---
+++ middleware.osgi/mw.managers.configuration.osgi/src/main/java/org/universAAL/mw/managers/configuration/osgi/ConfigurationActivator.java
@@ -0,0 +1,53 @@
+package org.universAAL.mw.managers.configuration.osgi;
+
+import java.io.File;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.managers.api.ConfigurationEditor;
+import org.universAAL.middleware.managers.api.ConfigurationManager;
+import org.universAAL.middleware.managers.api.ConfigurationManagerConnector;
+import org.universAAL.middleware.mw.manager.configuration.core.impl.ConfigurationManagerImpl;
+
+public class ConfigurationActivator implements BundleActivator {
+
+	ModuleContext context;
+	
+	ConfigurationManagerImpl cm;
+	
+	public void start(BundleContext arg0) throws Exception {	
+		context = uAALBundleContainer.THE_CONTAINER
+                .registerModule(new Object[] {arg0});	
+		LogUtils.logDebug(context, getClass(), "start", "Starting Configuration Manager.");
+		/*
+		 * uAAL stuff
+		 */
+		BundleConfigHome folder = new BundleConfigHome(context.getID());
+		cm = new ConfigurationManagerImpl(context, new FileProvider(new File(folder.getAbsolutePath())));
+		
+		context.getContainer().shareObject(context, cm, new String[]{
+			ConfigurationManager.class.getName(),
+			ConfigurationEditor.class.getName(),
+			ConfigurationManagerConnector.class.getName(),
+		});
+		
+		LogUtils.logDebug(context, getClass(), "start", "Started.");
+	}
+
+
+	public void stop(BundleContext arg0) throws Exception {
+		LogUtils.logDebug(context, getClass(), "stop", "Stopping.");
+		/*
+		 * close uAAL stuff
+		 */
+		cm.finish();
+		cm = null;
+		LogUtils.logDebug(context, getClass(), "stop", "Stopped.");
+
+	}
+
+}
