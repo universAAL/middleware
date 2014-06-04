2f60e5d0d84ee522efe67d493ce5d4944b2564c0
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-04 15:11:14 +0000

    Automatic tag of mw.interfaces.core version: 3.1.0

---
+++ middleware.core/mw.interfaces.core/src/main/java/org/universAAL/middleware/interfaces/configuration/configurationDefinitionTypes/ConfigurationDefinedElsewhere.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright 2014 Universidad Politécnica de Madrid
+ * Copyright 2014 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.middleware.interfaces.configuration.configurationDefinitionTypes;
+
+import java.util.Locale;
+
+import org.universAAL.middleware.interfaces.configuration.ConfigurableModule;
+import org.universAAL.middleware.interfaces.configuration.scope.Scope;
+
+/**
+ * Use this implementation when the configuration Entity is described elsewhere.
+ * Just provide the scope of the entity to listen for changes on it.
+ * {@link ConfigurableModule#configurationChanged(Scope, Object)} may not be called until the
+ * Entity is registered elsewhere (or found in some configuration repository), thus don't expect
+ * Immediate response.
+ * @author amedrano
+ *
+ */
+public class ConfigurationDefinedElsewhere implements DescribedEntity {
+
+    private Scope scope;
+
+    /**
+     * Provide a {@link Scope} for an entity that is fully defined somewhere else.
+     */
+    public ConfigurationDefinedElsewhere(Scope s) {
+	scope = s;
+    }
+
+    /** {@ inheritDoc}	 */
+    public final Scope getScope() {
+	return scope;
+    }
+
+    /** {@ inheritDoc}	 */
+    public final String getDescription(Locale loc) {
+	return null;
+    }
+
+}
---
+++ middleware.core/mw.interfaces.core/src/main/java/org/universAAL/middleware/interfaces/configuration/configurationDefinitionTypes/DynamicDescribedEntityListener.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright 2014 Universidad Politécnica de Madrid
+ * Copyright 2014 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.middleware.interfaces.configuration.configurationDefinitionTypes;
+
+import java.io.File;
+import java.net.URL;
+
+import org.universAAL.middleware.interfaces.configuration.ConfigurableModule;
+import org.universAAL.middleware.xsd.Base64Binary;
+
+/**
+ * Listener to be called when a {@link DynamicDescribedEntity} changes value.
+ * @author amedrano
+ *
+ */
+public interface DynamicDescribedEntityListener {
+    
+    /**
+     * Call this method when the description of a Described Entity has changed,
+     * so configuration editors can update it.
+     * @param dentity
+     */
+    public void updatedDescription(DescribedEntity dentity);
+
+    /**
+     * Call this method when the entity has updated the value internally,
+     * so persistent value can be stored and configuration editors informed on the new value.<BR>
+     * In case of {@link ConfigurationParameter} type entities the validity of the new value will be checked against the 
+     * restrictions.<br>
+     * In case of {@link ConfigurationFile} type entities the value must be either:
+     * 	<ul>
+     * 	<li> a {@link File}
+     * 	<li> an {@link URL}
+     *  <li> a Base64 coded {@link String}
+     *  <li> a byte[] containing the data of the file
+     *  <li> a {@link Base64Binary}
+     *  </ul>
+     *  <b>DO NOT USE INSIDE {@link ConfigurableModule#configurationChanged(org.universAAL.middleware.interfaces.configuration.scope.Scope, Object)}</b>
+     *  as it will guarantee an infinite loop.
+     * @param deEntity
+     * @param value
+     */
+    public void updatedValue(DescribedEntity deEntity, Object value);
+}
