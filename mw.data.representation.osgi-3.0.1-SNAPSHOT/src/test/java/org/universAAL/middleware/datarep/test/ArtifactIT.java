706b5662b03cdc3fdbd69b542d5a90523ae8773a
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-04 14:32:44 +0000

    Automatic tag of mw.data.representation.osgi version: 3.0.1-SNAPSHOT

---
+++ mw.data.representation.osgi-3.0.1-SNAPSHOT/src/test/java/org/universAAL/middleware/datarep/test/ArtifactIT.java
@@ -0,0 +1,17 @@
+package org.universAAL.middleware.datarep.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIT extends IntegrationTest {
+
+    public void testComposite() {
+	logAllBundles();
+    }
+
+}
