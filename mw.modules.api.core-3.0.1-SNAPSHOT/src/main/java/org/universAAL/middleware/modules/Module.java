26477b35da9fd7917e5f70486a44c62eeb88c768
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-04 14:29:43 +0000

    Automatic tag of mw.modules.api.core version: 3.0.1-SNAPSHOT

---
+++ mw.modules.api.core-3.0.1-SNAPSHOT/src/main/java/org/universAAL/middleware/modules/Module.java
@@ -0,0 +1,54 @@
+/*	
+	Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+	Institute of Information Science and Technologies 
+	of the Italian National Research Council 
+
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	  http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.middleware.modules;
+
+import java.util.Dictionary;
+
+/**
+ * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
+ * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
+ */
+public interface Module {
+
+    public String getDescription();
+
+    public String getProvider();
+
+    public String getVersion();
+
+    public String getName();
+
+    public void loadConfigurations(Dictionary configurations);
+
+    /**
+     * Routine in order to release resources
+     */
+    public void dispose();
+
+    /**
+     * Initialize the module. This method should be called after the loading the
+     * configurations.
+     * 
+     * @return
+     */
+    public boolean init();
+
+}
