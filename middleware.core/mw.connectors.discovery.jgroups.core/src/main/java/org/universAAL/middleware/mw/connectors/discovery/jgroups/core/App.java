b6b39f9fde6ba0c97b33a81b427f2b5d9ad16a2d
Dott. Federico Volpini <federico.volpini@isti.cnr.it>
2014-07-29 13:00:58 +0000

    Initial commit for discovery connector based on jGroups

---
+++ middleware.core/mw.connectors.discovery.jgroups.core/src/main/java/org/universAAL/middleware/mw/connectors/discovery/jgroups/core/App.java
@@ -0,0 +1,13 @@
+package org.universAAL.middleware.mw.connectors.discovery.jgroups.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
