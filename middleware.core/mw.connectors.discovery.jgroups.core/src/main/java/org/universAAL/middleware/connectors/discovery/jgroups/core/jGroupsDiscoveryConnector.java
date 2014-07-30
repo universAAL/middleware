dc69bae34427e8acbb1d1a4a726ae0cef4feeeed
Michele Girolami <michele.girolami@isti.cnr.it>
2014-07-30 07:01:26 +0000

    Renamed

---
+++ middleware.core/mw.connectors.discovery.jgroups.core/src/main/java/org/universAAL/middleware/connectors/discovery/jgroups/core/jGroupsDiscoveryConnector.java
@@ -0,0 +1,13 @@
+package org.universAAL.middleware.connectors.discovery.jgroups.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class jGroupsDiscoveryConnector 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
