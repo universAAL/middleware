ad535b3ef261e7ac3956766a023fa4989d052252
Dott. Federico Volpini <federico.volpini@isti.cnr.it>
2014-08-01 16:08:06 +0000

    First uncomplete implementation of DiscoveryConnector using JGroups

--- middleware.core/mw.connectors.discovery.jgroups.core/src/main/java/org/universAAL/middleware/connectors/discovery/jgroups/core/jGroupsDiscoveryConnector.java
+++ middleware.core/mw.connectors.discovery.jgroups.core/src/main/java/org/universAAL/middleware/connectors/discovery/jgroups/core/jGroupsDiscoveryConnector.java
@@ -2,64 +2,155 @@ package org.universAAL.middleware.connectors.discovery.jgroups.core;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Dictionary;
+
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Set;
 
 import org.jgroups.Address;
+import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.Receiver;
 import org.jgroups.View;
+import org.universAAL.middleware.interfaces.PeerCard;
+import org.universAAL.middleware.interfaces.aalspace.xml.model.ChannelDescriptor;
+import org.universAAL.middleware.brokers.message.gson.GsonParserBuilder;
 import org.universAAL.middleware.connectors.DiscoveryConnector;
 import org.universAAL.middleware.connectors.ServiceListener;
+import org.universAAL.middleware.connectors.discovery.jgroups.core.jGroupsDiscoveryConnector;
+import org.universAAL.middleware.connectors.exception.DiscoveryConnectorErrorCodes;
 import org.universAAL.middleware.connectors.exception.DiscoveryConnectorException;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.container.SharedObjectListener;
+import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.interfaces.aalspace.AALSpaceCard;
+import org.universAAL.middleware.interfaces.aalspace.Consts;
+import org.universAAL.middleware.interfaces.aalspace.model.IAALSpace;
+import org.universAAL.middleware.managers.api.AALSpaceManager;
 
 /**
  * This class implements the AALSpace discovery connector based on jGroups
  * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
  * @author <a href="mailto:federico.volpini@isti.cnr.it">Federico Volpini</a>
  */
-public class jGroupsDiscoveryConnector implements DiscoveryConnector,
-SharedObjectListener, Receiver  {
-
+public class jGroupsDiscoveryConnector 
+	implements DiscoveryConnector, SharedObjectListener, Receiver {
 
 	private ModuleContext context;
-
-
+	private JChannel discoveryChannel;
+	private String name, description, provider, version;
+	
+	private boolean initialized = false;
+	private List<ServiceListener> serviceListeners = new ArrayList<ServiceListener>();
+	private AALSpaceManager aalSpaceManager;
+	private ChannelDescriptor discoveryChannelDescriptor;
+	
 	public jGroupsDiscoveryConnector(ModuleContext context){
 		this.context = context;
-
 	}
+	
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	public String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.version;
 	}
 
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return description;
 	}
 
 	public String getProvider() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.provider;
+	}
+	
+	/**
+	 * This method prints the Connector properties: name, version, description
+	 * and provider
+	 * 
+	 * @return
+	 */
+	public String toString() {
+		return this.name + "-" + this.description + "-" + this.provider + "-"
+				+ this.version;
 	}
 
 	public void loadConfigurations(Dictionary configurations) {
-		// TODO Auto-generated method stub
-
+		this.name = (String) configurations
+			.get(org.universAAL.middleware.connectors.util.Consts.CONNECTOR_NAME);
+		this.version = (String) configurations
+			.get(org.universAAL.middleware.connectors.util.Consts.CONNECTOR_VERSION);
+		this.description = (String) configurations
+			.get(org.universAAL.middleware.connectors.util.Consts.CONNECTOR_DESCRIPTION);
+		this.provider = (String) configurations
+			.get(org.universAAL.middleware.connectors.util.Consts.CONNECTOR_PROVIDER);
 	}
 
 	public boolean init() {
 		// TODO Creo il canale e mi conetto al canale
-		return false;
+		
+		final String METHOD = "init";
+		
+		if (!initialized) {
+
+			// LOG
+			LogUtils.logDebug(
+				context, 
+				jGroupsDiscoveryConnector.class,
+				METHOD,
+				new Object[] { "Initializing JGroups Discovery Connector..." }, null);
+
+			Object[] aalManagers = context.getContainer()
+					.fetchSharedObject(
+							context,
+							new Object[] { AALSpaceManager.class.getName()
+									.toString() }, this);
+			
+			if (aalManagers != null && aalManagers.length > 0) {
+				aalSpaceManager = (AALSpaceManager) aalManagers[0];
+				discoveryChannelDescriptor = aalSpaceManager.getAALSpaceDefaultConfigurartion()
+						.getDiscoveryChannel().getChannelDescriptor();
+				
+				if(configureChannel(discoveryChannelDescriptor)){
+					// LOG
+					LogUtils.logDebug(
+						context, 
+						jGroupsDiscoveryConnector.class,
+						METHOD,
+						new Object[] { "JGroups Discovery Connector initialized" }, null);
+		
+					initialized = true;
+				}
+
+			}
+		}
+		
+		return initialized;
+	}
+
+	private boolean configureChannel(ChannelDescriptor discoveryChannelDescriptor) {
+		
+		final String METHOD = "configureChannel";
+		
+		try {
+			discoveryChannel = new JChannel(discoveryChannelDescriptor.getChannelURL());
+			discoveryChannel.setName(discoveryChannelDescriptor.getChannelName());
+			discoveryChannel.connect(discoveryChannelDescriptor.getChannelName());
+			//Imposto l'istanza di classe come Receiver dei messaggi
+			discoveryChannel.setReceiver(this);
+		} catch (Exception e) {
+			// LOG
+			LogUtils.logDebug(
+				context, 
+				jGroupsDiscoveryConnector.class,
+				METHOD,
+				new Object[] { "Error initializing JGroups Discovery Connector: "+e.getMessage() }, null);
+			return false;
+		}
+		return true;
 	}
 
 	public void dispose() {
@@ -68,8 +159,19 @@ SharedObjectListener, Receiver  {
 	}
 
 	public void sharedObjectAdded(Object sharedObj, Object removeHook) {
-		// TODO Auto-generated method stub
+		
+		final String METHOD = "sharedObjectAdded";
+		
+		if (sharedObj instanceof AALSpaceManager) {
+			// LOG
+			LogUtils.logDebug(
+					context, 
+					jGroupsDiscoveryConnector.class,
+					METHOD,
+					new Object[] { "AALSpaceManager service added" }, null);
+			aalSpaceManager = (AALSpaceManager) sharedObj;
 
+		}
 	}
 
 	public void sharedObjectRemoved(Object removeHook) {
@@ -79,40 +181,193 @@ SharedObjectListener, Receiver  {
 
 	public List<AALSpaceCard> findAALSpace(Dictionary<String, String> filters)
 			throws DiscoveryConnectorException {
-		// TODO // TODO Inviare messaggio in multicast con la query per l'AALSpace e il filtro
-		return null;
+		// TODO Inviare messaggio in multicast con la query per l'AALSpace e il filtro
+		Set<AALSpaceCard> aalSpaceCards = aalSpaceManager.getAALSpaces();
+		
+		List<AALSpaceCard> listOfAALSpaces = new ArrayList(aalSpaceCards);
+		return listOfAALSpaces;
 	}
 
 	public List<AALSpaceCard> findAALSpace() throws DiscoveryConnectorException {
-		// TODO Inviare messaggio in multicast con la query per l'AALSpace
-		return null;
+		return this.findAALSpace(null);
 	}
 
 	public void announceAALSpace(AALSpaceCard spaceCard)
 			throws DiscoveryConnectorException {
 		// TODO Inviare messaggio in mutlicast per annunciare un nuovo AALSpace
+		
+		final String METHOD = "announceAALSpace";
+		
+		// LOG
+		LogUtils.logTrace(
+				context, 
+				jGroupsDiscoveryConnector.class,
+				METHOD,
+				new Object[] { "Announcing the AALSpace..." }, null);
+		
+		if (init()) {
+			try {
+				String serializedMessage = GsonParserBuilder.getInstance().toJson(spaceCard);
+				discoveryChannel.send(null, serializedMessage);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
+	private Address getAddress(PeerCard card){
+		
+		String cardID = card.getPeerID();
+		
+		final String METHOD = "getAddress";
+		
+		Address dst = null;
+        View discoveryChannelView = discoveryChannel.getView();
+		
+        if (discoveryChannelView == null) {
+        	LogUtils.logTrace(
+    				context, 
+    		 		jGroupsDiscoveryConnector.class,
+                    METHOD,
+    		 		"Unable to get the View on the channel " + discoveryChannel.getClusterName()
+                            + " We may not be connected to it");
+            return null;
+        }
+       
+        for (Address address : discoveryChannelView.getMembers()) {
+            if (cardID.equals(discoveryChannel.getName(address))) {
+                dst = address;
+        		return dst;
+        	}
+        }
+    	LogUtils.logTrace(
+				context, 
+		 		jGroupsDiscoveryConnector.class,
+                METHOD,
+		 		"Address of Peer "+card.toString()+" not found");
+        return null;
 	}
 
 	public void deregisterAALSpace(AALSpaceCard spaceCard)
 			throws DiscoveryConnectorException {
+		
 		// TODO Inviare messaggio in mutlicast per cancellare un AALSpace esistente
-
+		
+		final String METHOD = "deregisterAALSpace";
+		
+		// LOG
+		LogUtils.logDebug(
+				context,
+				jGroupsDiscoveryConnector.class,
+				METHOD,
+				new Object[] { "De-Registering the AALSpace: "
+						+ spaceCard.toString() + "..." }, null);
+		if (init()) {
+			try {
+				
+			} catch (Exception e) {
+				// LOG
+				LogUtils.logError(
+						context,
+						jGroupsDiscoveryConnector.class,
+						METHOD,
+						new Object[] { "Unable to de-register space: "
+								+ spaceCard.toString() + " --> " + e.toString() },
+						null);
+				throw new DiscoveryConnectorException(
+						DiscoveryConnectorErrorCodes.DEREGISTER_ERROR,
+						e.toString());
+			}
+		} else {
+			// LOG
+			LogUtils.logWarn(
+					context,
+					jGroupsDiscoveryConnector.class,
+					METHOD,
+					new Object[] { "JGroupDiscoveryConnector is not initialized!" },
+					null);
+		}
 	}
 
+	/**
+	 * This method collects in a dictionary the properties associated with a new
+	 * AAL Space in order to announce them. The properties are read from the
+	 * data structure AalSpace. The properties added to the AALSpace card are
+	 * the name,id,description and coordinator ID and the peering channel
+	 * serialized as XML string
+	 * 
+	 * @param space
+	 * @return
+	 */
+	public static Dictionary<String, String> getAALSpaceProperties(
+			IAALSpace space) {
+		Dictionary<String, String> properties = new Hashtable<String, String>();
+		try {
+			// general purpose properties
+			properties.put(Consts.AALSPaceName, space.getSpaceDescriptor()
+					.getSpaceName());
+			properties.put(Consts.AALSPaceID, space.getSpaceDescriptor()
+					.getSpaceId());
+			properties.put(Consts.AALSPaceDescription, space
+					.getSpaceDescriptor().getSpaceDescription());
+
+			String coordinatorID = space.getSpaceDescriptor()
+					.getSpaceCoordinator();
+			properties.put(Consts.AALSpaceCoordinator, coordinatorID);
+			properties
+					.put(Consts.AALSpacePeeringChannelURL, space
+							.getPeeringChannel().getChannelDescriptor()
+							.getChannelURL());
+			properties.put(Consts.AALSpacePeeringChannelName, space
+					.getPeeringChannel().getChannelDescriptor()
+					.getChannelName());
+			properties.put(Consts.AALSPaceProfile, space.getSpaceDescriptor()
+					.getProfile());
+
+		} catch (NullPointerException e) {
+			return null;
+		} catch (Exception e) {
+			return null;
+		}
+		return properties;
+
+	}
+	
 	public String getSDPPRotocol() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void addAALSpaceListener(ServiceListener listener) {
-		// TODO Auto-generated method stub
+		
+		final String METHOD = "addAALSpaceListener";
+		
+		if (listener != null && !serviceListeners.contains(listener)) {
+			this.serviceListeners.add(listener);
+			
+			// LOG
+			LogUtils.logDebug(context, 
+					jGroupsDiscoveryConnector.class,
+					METHOD,
+					new Object[] { "New AALSpaceListener" }, null);
+		}
 
 	}
 
 	public void removeAALSpaceListener(ServiceListener listener) {
-		// TODO Auto-generated method stub
-
+		
+		final String METHOD = "removeAALSpaceListener";
+		
+		if (listener != null && !serviceListeners.contains(listener)) {
+			this.serviceListeners.remove(listener);
+			
+			// LOG
+			LogUtils.logDebug(context, 
+					jGroupsDiscoveryConnector.class,
+					METHOD,
+					new Object[] { "AALSpaceListener "+listener.toString()+" removed" }, null);
+		}
 	}
 	
 	/**
@@ -123,10 +378,11 @@ SharedObjectListener, Receiver  {
 	private boolean asnwerToAQuery(String query){
 		return true;
 	}
+	
 	public void receive(Message msg) {
 		// TODO Auto-generated method stub
-		
 	}
+
 	public void getState(OutputStream output) throws Exception {
 		// TODO Auto-generated method stub
 		
--- middleware.osgi/mw.connectors.discovery.jgroups.osgi/src/main/java/org/universAAL/middleware/connectors/discovery/jgroups/osgi/Activator.java
+++ middleware.osgi/mw.connectors.discovery.jgroups.osgi/src/main/java/org/universAAL/middleware/connectors/discovery/jgroups/osgi/Activator.java
@@ -21,36 +21,87 @@
 package org.universAAL.middleware.connectors.discovery.jgroups.osgi;
 
 import java.util.Dictionary;
+import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
+import org.universAAL.middleware.connectors.DiscoveryConnector;
+import org.universAAL.middleware.connectors.discovery.jgroups.core.jGroupsDiscoveryConnector;
+import org.universAAL.middleware.connectors.discovery.jgroups.osgi.Activator;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.osgi.uAALBundleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
 
 /**
  * OSGI bundle for the JGroups discovery connector
  * 
  * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
- * @author <a href="mailto:federico.volpini@isti.cnr.it">Francesco Furfari</a>
+ * @author <a href="mailto:federico.volpini@isti.cnr.it">Federico Volpini</a>
  */
 public class Activator implements BundleActivator, ManagedService {
 
 	private static String SERVICE_PID = "mw.connectors.discovery.jgroups.core";
+	private DiscoveryConnector jGroupsDiscoveryConnector;
+	private ServiceRegistration myRegistration;
 
-	public void stop(BundleContext arg0) throws Exception {
+	public void start(BundleContext context) throws Exception {
+		uAALBundleContext moduleContext = (uAALBundleContext) uAALBundleContainer.THE_CONTAINER
+			.registerModule(new Object[] { context });
 
+		// LOG
+		LogUtils.logDebug(moduleContext, Activator.class, "Activator",
+			new Object[] { "Starting the JGroupsDiscoveryConnector..." }, null);
+		
+		jGroupsDiscoveryConnector = new jGroupsDiscoveryConnector(moduleContext);
+		
+		Dictionary props = new Hashtable();
+		props.put(Constants.SERVICE_PID, SERVICE_PID);
+		myRegistration = context.registerService(
+			ManagedService.class.getName(), this, props);
+
+		ConfigurationAdmin configurationAdmin = null;
+		
+		ServiceReference sr = context
+			.getServiceReference(ConfigurationAdmin.class.getName());
+		if (sr != null && context.getService(sr) instanceof ConfigurationAdmin)
+		    configurationAdmin = (ConfigurationAdmin) context.getService(sr);
+		
+		Configuration config = configurationAdmin.getConfiguration(SERVICE_PID);
+		Dictionary jgroupsDConnectorProperties = config.getProperties();
+		if (jgroupsDConnectorProperties != null) {
+			jGroupsDiscoveryConnector.loadConfigurations(jgroupsDConnectorProperties);
+			jGroupsDiscoveryConnector.init();
+		}
+
+		// register the JGroupsDiscoveryConnector
+		uAALBundleContainer.THE_CONTAINER.shareObject(moduleContext,
+			jGroupsDiscoveryConnector,
+			new Object[] { DiscoveryConnector.class.getName() });
+		
+		// LOG
+		LogUtils.logDebug(moduleContext, Activator.class, "startBrokerClient",
+			new Object[] { "Starting the JGroupsDiscoveryConnector..." }, null);
+
+	}
+	
+	public void stop(BundleContext context) throws Exception {
+		jGroupsDiscoveryConnector.dispose();
+		myRegistration.unregister();
 	}
 
 	/**
 	 * Called-back as soon as properties are loaded or modified
 	 */
 	public void updated(Dictionary properties) throws ConfigurationException {
-
+		jGroupsDiscoveryConnector.loadConfigurations(properties);
+		jGroupsDiscoveryConnector.init();
 	}
-
-	public void start(BundleContext arg0) throws Exception {
-		// TODO Auto-generated method stub
 		
-	}
-
 }
