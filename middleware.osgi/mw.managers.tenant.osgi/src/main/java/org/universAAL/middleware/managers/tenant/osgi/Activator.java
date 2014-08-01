11e999665e956f51633a5723d855437c2ee22f5b
Michele Girolami <michele.girolami@isti.cnr.it>
2014-08-01 13:28:28 +0000

    Registration of the Tenant Manager and implementation of basic operations

--- middleware.osgi/mw.managers.tenant.osgi/src/main/java/org/universAAL/middleware/managers/tenant/osgi/Activator.java
+++ middleware.osgi/mw.managers.tenant.osgi/src/main/java/org/universAAL/middleware/managers/tenant/osgi/Activator.java
@@ -1,47 +1,71 @@
-/*
-        Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
-        Institute of Information Science and Technologies
-        of the Italian National Research Council
-
-        See the NOTICE file distributed with this work for additional
-        information regarding copyright ownership
-
-        Licensed under the Apache License, Version 2.0 (the "License");
-        you may not use this file except in compliance with the License.
-        You may obtain a copy of the License at
-
-          http://www.apache.org/licenses/LICENSE-2.0
-
-        Unless required by applicable law or agreed to in writing, software
-        distributed under the License is distributed on an "AS IS" BASIS,
-        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        See the License for the specific language governing permissions and
-        limitations under the License.
- */
 package org.universAAL.middleware.managers.tenant.osgi;
 
-import java.util.Dictionary;
+/*	
+	Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+	Institute of Information Science and Technologies 
+	of the Italian National Research Council 
+
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	  http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.cm.ConfigurationException;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ManagedService;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.managers.api.TenantManager;
+import org.universAAL.middleware.managers.tenant.TenantManagerImpl;
 
-public class Activator implements BundleActivator, ManagedService {
+/**
+ * Activator for the Tenant Manager
+ * 
+ * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
+ * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
+ * 
+ * @version $LastChangedRevision$ ( $LastChangedDate$ )
+ */
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public class Activator implements BundleActivator {
 
-	public void updated(Dictionary properties) throws ConfigurationException {
-		// TODO Auto-generated method stub
+	private TenantManagerImpl tenantManager;
+	private ServiceRegistration myRegistration;
+	private ModuleContext moduleContext;
 
+	public void start(BundleContext context) throws Exception {
+		moduleContext = uAALBundleContainer.THE_CONTAINER
+				.registerModule(new Object[] { context });
+		LogUtils.logDebug(moduleContext, Activator.class, "start",
+				new Object[] { "Starting the TenantManager..." }, null);
+
+		tenantManager = new TenantManagerImpl(moduleContext);
+
+		myRegistration = context.registerService(
+				ManagedService.class.getName(), this, null);
+
+		tenantManager.init();
+		uAALBundleContainer.THE_CONTAINER.shareObject(moduleContext,
+				tenantManager, new Object[] { TenantManager.class.getName() });
 	}
 
-	public void start(BundleContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void stop(BundleContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void stop(BundleContext context) throws Exception {
+		tenantManager.dispose();
+		myRegistration.unregister();
 	}
 
 }
