69b870c6905ae2dd18436be6dbb74c71e91a78bc
Michele Girolami <michele.girolami@isti.cnr.it>
2014-08-01 14:45:38 +0000

    Some fix and added import/export directives

--- middleware.osgi/mw.managers.tenant.osgi/src/main/java/org/universAAL/middleware/managers/tenant/osgi/Activator.java
+++ middleware.osgi/mw.managers.tenant.osgi/src/main/java/org/universAAL/middleware/managers/tenant/osgi/Activator.java
@@ -1,31 +1,28 @@
 package org.universAAL.middleware.managers.tenant.osgi;
 
 /*	
-	Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
-	Institute of Information Science and Technologies 
-	of the Italian National Research Council 
+ Copyright 2007-2014 CNR-ISTI, http://isti.cnr.it
+ Institute of Information Science and Technologies 
+ of the Italian National Research Council 
 
-	See the NOTICE file distributed with this work for additional 
-	information regarding copyright ownership
+ See the NOTICE file distributed with this work for additional 
+ information regarding copyright ownership
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-	  http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
-
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.cm.ManagedService;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.container.osgi.uAALBundleContainer;
 import org.universAAL.middleware.container.utils.LogUtils;
@@ -44,7 +41,6 @@ import org.universAAL.middleware.managers.tenant.TenantManagerImpl;
 public class Activator implements BundleActivator {
 
 	private TenantManagerImpl tenantManager;
-	private ServiceRegistration myRegistration;
 	private ModuleContext moduleContext;
 
 	public void start(BundleContext context) throws Exception {
@@ -55,9 +51,6 @@ public class Activator implements BundleActivator {
 
 		tenantManager = new TenantManagerImpl(moduleContext);
 
-		myRegistration = context.registerService(
-				ManagedService.class.getName(), this, null);
-
 		tenantManager.init();
 		uAALBundleContainer.THE_CONTAINER.shareObject(moduleContext,
 				tenantManager, new Object[] { TenantManager.class.getName() });
@@ -65,7 +58,7 @@ public class Activator implements BundleActivator {
 
 	public void stop(BundleContext context) throws Exception {
 		tenantManager.dispose();
-		myRegistration.unregister();
+
 	}
 
 }
