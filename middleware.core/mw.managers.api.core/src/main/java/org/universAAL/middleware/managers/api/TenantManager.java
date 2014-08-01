15cec1734e6142183f29943ded1dcec6fc11c897
Michele Girolami <michele.girolami@isti.cnr.it>
2014-08-01 13:29:08 +0000

    Definition of basic interfaces

--- middleware.core/mw.managers.api.core/src/main/java/org/universAAL/middleware/managers/api/TenantManager.java
+++ middleware.core/mw.managers.api.core/src/main/java/org/universAAL/middleware/managers/api/TenantManager.java
@@ -20,12 +20,32 @@
  */
 package org.universAAL.middleware.managers.api;
 
+import java.util.Map;
+
 /**
  * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
- * @author michele
  * 
  */
-public interface TenantManager extends Manager{
+public interface TenantManager extends Manager {
+	
+	/**
+	 * Register a new tenant to the manager
+	 * @param tenantID ID of the tenant
+	 * @param tenantDescription description of the tenant
+	 */
+	public void registerTenant(String tenantID, String tenantDescription);
+	
+	/**
+	 * Unregisters an existing tenant
+	 * @param tenantID
+	 */
+	public void unregisterTenant(String tenantID);
+	
+	/**
+	 * Retrieves the map of tenants
+	 * @return
+	 */
+	public Map<String,String> getTenants();
 
 }
