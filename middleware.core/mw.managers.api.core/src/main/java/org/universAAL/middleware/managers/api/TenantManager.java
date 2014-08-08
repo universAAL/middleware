33fe649a687f741e30b9a8e234555d559b12fa8f
Stefano Lenzi <stefano.lenzi@isti.cnr.it>
2014-08-08 09:10:10 +0000

    Updated javadocs

--- middleware.core/mw.managers.api.core/src/main/java/org/universAAL/middleware/managers/api/TenantManager.java
+++ middleware.core/mw.managers.api.core/src/main/java/org/universAAL/middleware/managers/api/TenantManager.java
@@ -25,32 +25,34 @@ import java.util.Map;
 /**
  * @author <a href="mailto:michele.girolami@isti.cnr.it">Michele Girolami</a>
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano Lenzi</a>
- * 
+ * @version $LastChangedRevision$ ( $LastChangedDate$ )
+ * @since 3.2.0
+ *
  */
 public interface TenantManager extends Manager {
-	
-	/**
-	 * Register a new tenant to the manager
-	 * @param tenantID ID of the tenant
-	 * @param tenantDescription description of the tenant
-	 */
-	public void registerTenant(String tenantID, String tenantDescription);
-	
-	/**
-	 * Unregisters an existing tenant
-	 * @param tenantID
-	 */
-	public void unregisterTenant(String tenantID);
-	
-	/**
-	 * Retrieves the map of tenants
-	 * @return
-	 */
-	public Map<String,String> getTenants();
-	
-	
-	public void addTenantListener(TenantListener tenantListener);
-	
-	public void removeTenantListener(TenantListener tenantListener);	
+
+        /**
+         * Register a new tenant to the manager
+         * @param tenantID ID of the tenant
+         * @param tenantDescription description of the tenant
+         */
+        public void registerTenant(String tenantID, String tenantDescription);
+
+        /**
+         * Unregisters an existing tenant
+         * @param tenantID
+         */
+        public void unregisterTenant(String tenantID);
+
+        /**
+         * Retrieves the map of tenants
+         * @return
+         */
+        public Map<String,String> getTenants();
+
+
+        public void addTenantListener(TenantListener tenantListener);
+
+        public void removeTenantListener(TenantListener tenantListener);
 
 }
