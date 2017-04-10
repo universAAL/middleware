59e6e62f886626876e472fcb03000822314c4906
Carsten Stocklöw <stockloew@gmx.de>
2017-04-10 03:30:56 +0200

    working on universAAL/middleware#10 removed the deprecated classes

--- middleware.osgi/mw.container.osgi/src/main/java/org/universAAL/middleware/container/osgi/uAALBundleContext.java
+++ middleware.osgi/mw.container.osgi/src/main/java/org/universAAL/middleware/container/osgi/uAALBundleContext.java
@@ -43,7 +43,6 @@ import org.slf4j.LoggerFactory;
 import org.universAAL.middleware.container.Attributes;
 import org.universAAL.middleware.container.Container;
 import org.universAAL.middleware.container.ModuleContext;
-import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
 
 /**
  * An implementation of the concept of {@link ModuleContext} for OSGi.
