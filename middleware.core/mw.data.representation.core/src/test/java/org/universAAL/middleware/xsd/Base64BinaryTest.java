6fb84b15c9501efbec4deac2d477925454feb631
Carsten Stocklöw <stockloew@gmx.de>
2017-04-12 15:09:02 +0200

    unit tests for Base64Binary

---
+++ middleware.core/mw.data.representation.core/src/test/java/org/universAAL/middleware/xsd/Base64BinaryTest.java
@@ -0,0 +1,24 @@
+package org.universAAL.middleware.xsd;
+
+import junit.framework.TestCase;
+
+public class Base64BinaryTest extends TestCase {
+
+    public void test1() {
+	String s = "";
+	for (int i = 0; i < 100; i++) {
+	    Base64Binary b = new Base64Binary(s.getBytes());
+
+	    boolean bool = b.getDecodedLength() == s.length();
+	    if (!bool) {
+		System.out.println(" ----------- ");
+		System.out.println(b.toString());
+		System.out.println(s);
+		System.out.println(b.getDecodedLength());
+		assertTrue(bool);
+	    }
+
+	    s += "a";
+	}
+    }
+}
