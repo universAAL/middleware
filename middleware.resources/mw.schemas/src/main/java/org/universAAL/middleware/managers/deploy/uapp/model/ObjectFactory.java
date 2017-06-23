9a5b28cfad4684ab383a4f02fa76b0df11bc5f52
cstockloew <stockloew@gmx.de>
2017-06-23 17:33:52 +0200

    changed ucc package names universAAL/platform#42

--- middleware.resources/mw.schemas/src/main/java/org/universAAL/middleware/managers/deploy/uapp/model/ObjectFactory.java
+++ middleware.resources/mw.schemas/src/main/java/org/universAAL/middleware/managers/deploy/uapp/model/ObjectFactory.java
@@ -28,7 +28,7 @@ import javax.xml.namespace.QName;
 
 /**
  * This object contains factory methods for each Java content interface and Java
- * element interface generated in the org.universAAL.ucc.model.uapp package.
+ * element interface generated in the org.universAAL.tools.ucc.model.uapp package.
  * <p>
  * An ObjectFactory allows you to programatically construct new instances of the
  * Java representation for XML content. The Java representation of XML content
@@ -50,7 +50,7 @@ public class ObjectFactory {
 
 	/**
 	 * Create a new ObjectFactory that can be used to create new instances of
-	 * schema derived classes for package: org.universAAL.ucc.model.uapp
+	 * schema derived classes for package: org.universAAL.tools.ucc.model.uapp
 	 *
 	 */
 	public ObjectFactory() {
