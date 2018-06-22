201b368b6b3dd3b6398a42a6b7229ecb34e77200
mtazari <mtazari@igd.fraunhofer.de>
2018-06-22 19:11:45 +0200

    Added a null check to EntityFactory Undid a previous change in ConfigurationManagerImpl.register() Added comments to all the three files for better understanding

--- middleware.core/mw.managers.configuration.core/src/main/java/org/universAAL/middleware/managers/configuration/core/impl/factories/EntityFactory.java
+++ middleware.core/mw.managers.configuration.core/src/main/java/org/universAAL/middleware/managers/configuration/core/impl/factories/EntityFactory.java
@@ -28,15 +28,15 @@ import org.universAAL.middleware.managers.configuration.core.owl.Entity;
 /**
  * Facotry to create and update {@link Entity Entities} from
  * {@link DescribedEntity DescribedEntities}.
- * 
+ *
  * @author amedrano
- * 
+ *
  */
 public class EntityFactory {
 
 	/**
 	 * Given a {@link DescribedEntity} create a new Entity.
-	 * 
+	 *
 	 * @param dentity
 	 *            template
 	 * @param loc
@@ -55,10 +55,12 @@ public class EntityFactory {
 			org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter cp = new org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter(
 					uri);
 			ConfigurationParameter de = (ConfigurationParameter) dentity;
+			
+			// description is mandatory --> we set it without double-checking "!= null"
 			cp.setDescription(dentity.getDescription(loc), loc);
 			
-			// set restriction to type
-			// it is important that this restriction is set before setting any value so that the conformance of the value can be checked in ConfigurationParameter
+			// set the value restriction, which is also mandatory --> we do not check "!= null"
+			// it is important that this restriction is set before setting any value so that the conformance of the value can be checked in cp
 			cp.setValueRestriction(de.getType());
 			
 			Object o = de.getDefaultValue();
@@ -91,7 +93,7 @@ public class EntityFactory {
 
 	/**
 	 * Update an entity from the associated {@link DescribedEntity}.
-	 * 
+	 *
 	 * @param OldEntity
 	 *            the old {@link Entity} to update. if null then
 	 *            {@link EntityFactory#getEntity(DescribedEntity, Locale)} is
@@ -113,20 +115,23 @@ public class EntityFactory {
 		if (oldEntity != null  &&  newEntity != null)
 			if (oldEntity.getClass() == newEntity.getClass()) {
 				newEntity.setVersion(oldEntity.getVersion());
-				
 				String newDescription = dentity.getDescription(loc);
 				if (newDescription != null && !newDescription.isEmpty() && !newDescription.equals(oldEntity.getDescription(loc)))
 					newEntity.incrementVersion();
 
 				if (dentity instanceof ConfigurationParameter) {
-					// newEntity and dentity are assumed to be compatible; so is oldEntity
+					// the type of dentity determines the type of newEntity
 					org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter newCP = (org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter) newEntity;
+					// oldEntity and newEntity are both instances of the same class as checked above
 					org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter oldCP = (org.universAAL.middleware.managers.configuration.core.owl.ConfigurationParameter) oldEntity;
 			
+					// value restriction is mandatory --> we do not need to double check "!= null!
 					if (!newCP.getValueRestriction().equals(oldCP.getValueRestriction()))
 						newEntity.incrementVersion();
 
-					if (!newCP.getDefaultValue().equals(oldCP.getDefaultValue()))
+					Object newVal = newCP.getDefaultValue(), oldVal = oldCP.getDefaultValue();
+					if ((newVal != null  ||  oldVal != null)
+							&&  (newVal == null  ||  !newVal.equals(oldVal)))
 						newEntity.incrementVersion();
 				}
 		
@@ -142,9 +147,9 @@ public class EntityFactory {
 					if (!newCF.getExtensionFilter().equals(oldCF.getExtensionFilter()))
 						newEntity.incrementVersion();
 				}
-			} else
+			} else 
 				newEntity.setVersion(oldEntity.getVersion()+3);
-
+				
 		return newEntity;
 	}
 
