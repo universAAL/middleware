bcdab7964564dc178ce7eb0caf35ce5c1771138a
Eduardo Buhid <ebuhid@lst.tfo.upm.es>
2019-02-27 15:55:36 +0100

    added methods to test more jsonLD cases

--- middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ValidatorTest.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ValidatorTest.java
@@ -8,6 +8,9 @@ import java.io.InputStream;
 import org.junit.Test;
 import org.universAAL.middleware.serialization.json.grammar.JSONLDDocument;
 
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+
 public class ValidatorTest {
 
 	@Test
@@ -16,5 +19,60 @@ public class ValidatorTest {
 		JSONLDDocument doc = new JSONLDDocument(is);
 		is.close();
 		assertTrue(doc.validate());
+	}
+	
+	//este test es para probarel caso de tener un array con context dentro
+	//nota: el json que esta testeando NO ES UN JSON-LD VALIDO, esta asi solamente para comprobar si es correcta mi interpretacion de la documentacion
+	//este caso es solo para probar la clase NodeObject y como la planteo Ale	
+	//TODO el analisis de contextos dentro de arrays no esta correcto. ¿bajo que clave se guarda el array de contextos? no puede existir un array sin clave
+	//a menos que sea el unico elemento del json
+	/*
+	 * 			{
+	 * 				[ "@context": {},"@context": {},"@context": {},"@context": {}],
+	 * 					"":"",
+	 * 					"":"",
+	 * 					"key":{...}
+	 * 			}
+	 * 
+	 * */
+	@Test 
+	public void test2() throws IOException {
+		InputStream is = this.getClass().getClassLoader().getResource("JSONLDexample2.json").openStream();
+		JSONLDDocument doc = new JSONLDDocument(is);
+		is.close();
+		assertTrue(doc.validate());
+	}
+	
+	
+	//---------------to test bad syntax of json-ld
+	
+	/*
+	 * test case if the main son is not an object..is an array
+	 * */
+	@Test (expected = ClassCastException.class)
+	public void test3() throws IOException {
+		InputStream is = this.getClass().getClassLoader().getResource("JSONLDexample3.json").openStream();
+		JSONLDDocument doc = new JSONLDDocument(is);
+		is.close();
+		assertTrue(doc.validate());
+	}
+	
+	/**
+	 * to test if the json has not a correct structure (missing close bracket)
+	 * @throws IOException
+	 */
+	@Test (expected = JsonSyntaxException.class)
+	public void test4() throws IOException {
+		InputStream is = this.getClass().getClassLoader().getResource("JSONLDexample4.json").openStream();
+		JSONLDDocument doc = new JSONLDDocument(is);
+		is.close();
+		assertTrue(doc.validate());
+	}
+
+	
+	@Test
+	public void local() {
+		String t ="edu:ardo";
+		System.out.println(t.substring(0,t.indexOf(":")));
 	}
 }
