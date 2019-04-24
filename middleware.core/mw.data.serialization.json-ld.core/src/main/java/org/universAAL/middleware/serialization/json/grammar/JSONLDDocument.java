2f762748792afa8163974d218192b0b846c0f26e
Eduardo <ebuhid@lst.tfo.upm.es>
2019-04-24 14:44:03 +0200

    deleted useless resources files.  implementation updates. Added library to easy expand/compact and check of json structure

--- middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/grammar/JSONLDDocument.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/grammar/JSONLDDocument.java
@@ -16,9 +16,14 @@
  ******************************************************************************/
 package org.universAAL.middleware.serialization.json.grammar;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.swing.SortingFocusTraversalPolicy;
@@ -30,12 +35,18 @@ import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.serialization.json.JSONLDSerialization;
 import org.universAAL.middleware.serialization.json.JsonLdKeyword;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.github.jsonldjava.core.JsonLdError;
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+
 
 /**
  * Class to represent a entire Json document
@@ -47,9 +58,10 @@ import com.google.gson.JsonSyntaxException;
 public class JSONLDDocument implements JSONLDValidator {
 	private ArrayList<ContextDefinition> contexts = new ArrayList<ContextDefinition>(2) ; 
 	private ContextDefinition activeContext=null, lastContext= null;
-	private JsonObject mainJSON = null;
+	private JsonElement mainJSON = null;
 	private List<Resource> resources=null; 
-
+	private Resource firstResource=null;
+	private String mainJsonString;
 	private JsonParser jp = null;
 
 	/**	
@@ -59,25 +71,25 @@ public class JSONLDDocument implements JSONLDValidator {
 	 * @throws JsonSyntaxException
 	 */
 	public JSONLDDocument(InputStream jsonToBeProcessed) throws JsonParseException, JsonSyntaxException,ClassCastException,NullPointerException,JsonSyntaxException{
+		//maybe its necesary to use jsonld library to expand the json to process it 
 		this.resources = new ArrayList<Resource>();
+		
 		String jsonString = "";
 		Scanner s = new Scanner(jsonToBeProcessed);
 		s.useDelimiter("\\A");
 		jsonString = s.hasNext() ? s.next() : "";
 		s.close();
+		this.mainJsonString=jsonString;
 		this.jp = new JsonParser();
 		//throws MalformedJsonException if the json is mal formed
 		try {
-			
-			this.mainJSON = (JsonObject) jp.parse(jsonString);
+			this.mainJSON = jp.parse(this.processJsonLDFormat("expand"));
 		}catch (JsonSyntaxException e) {
 			throw e;
 		}
 		
 
 	}
-	
-
 
 	/**
 	 * Method to start the Json validation process. A JSON-LD document must be a
@@ -86,67 +98,49 @@ public class JSONLDDocument implements JSONLDValidator {
 	 * top level.
 	 * @return {@link Boolean} value indicating the status of the process
 	 */
+
 	public boolean validate() {
-		JsonObject aux = this.mainJSON;
-		ContextDefinition active=null;
-		Boolean compacted=false;
-		Resource subject=null, predicate=null,resource=null;
-		
-		if(this.mainJSON.has(JsonLdKeyword.CONTEXT.toString())) {
-			compacted=true;
-		}
-	
-		if(compacted) {
-			active = new ContextDefinition( aux.get(JsonLdKeyword.CONTEXT.toString()));
-			if (!active.validate()) 
-				return false;
-			aux.remove(JsonLdKeyword.CONTEXT.toString());
-		}
+		//datamodel especification	https://www.w3.org/TR/2014/REC-json-ld-20140116/#data-model
+		/*
+		A JSON-LD document serializes a generalized RDF Dataset [RDF11-CONCEPTS], 
+		which is a collection of graphs that comprises exactly one default graph and zero or more named graphs.
+		A JSON-LD document MUST be a single node object or an array whose elements are each node objects at the top level.
+		*/
+		//add the default graph as resource 
+		if(this.mainJSON.isJsonArray()){
+			//elements are each node objects at the top level.
+			
+			//JsonObject objAux = this.mainJSON .getAsJsonArray().iterator().next().getAsJsonObject();
+					//node object check
+					//iterate over internal graphs. Named graphs
+					for (Entry<String, JsonElement> namedGraph: this.mainJSON .getAsJsonArray().iterator().next().getAsJsonObject().entrySet()) {
+						//each entry element represents a named graph	
+						if(IRI.isAbsolute(namedGraph.getKey())) {
+							//check the child nodes
+							//build a resource with the key of this node
+							//check the child nodes
+							new Resource(namedGraph.getKey());
+							
+							if(namedGraph.getValue().isJsonArray() || namedGraph.getValue().isJsonArray()) {
+								//parseCollection()
+							}
+						}else if(namedGraph.getKey().equals(JsonLdKeyword.BLANK_NODE)){
+							new Resource();
+						}else
+							return false;
+					}
+				}
 
-		return this.validateBody(aux);
-	}
-	
-	private boolean validateBody(JsonElement toValidate) {
+		else if(this.mainJSON .isJsonObject()) {
+			//TODO interpret this case
+		}else if(this.mainJSON .isJsonPrimitive()) {
+			//TODO interpret this case			
+		} else
+			//TODO check documentation
+			return false;
 
-		
-		if(toValidate.isJsonObject()){
-			
-			if(this.resources.isEmpty()) {
-			//validate key before generate resource(?)
-				
-			}
-				
-			for (Entry<String, JsonElement> item : toValidate.getAsJsonObject().entrySet()) {
-				
-				if(item.getValue().isJsonArray()) {
-					//TODO control the item key to check if it is valid
-					//the key must be a valid URI
-					if(!IRI.isAbsolute(item.getKey())) return false; //TODO if URI is relative
-					//may be parse as collection
-				}
-				if(item.getValue().isJsonObject()) {
-					//parse as triple(?)
-					//TODO check the jsonLD format
-				}
-				if(item.getValue().isJsonPrimitive()) {
-					//parse as triple(?)
-					//TODO check the jsonLD format
-				}
-		}
-			
-		}
-		
-		if(toValidate.isJsonObject()){
-					
-		}
-		
-		if(toValidate.isJsonObject()){
-			
-		}
-	
 		return true;
-	}
-
+}
 	/**
 	 * after get the resource, the method check if the Json is valid 
 	 * @param resourceURI
@@ -159,4 +153,37 @@ public class JSONLDDocument implements JSONLDValidator {
 			return null;
 	}
 	
+	private  String processJsonLDFormat(String out)  {
+		try {
+			// Open a valid json(-ld) input file
+			//InputStream inputStream = new FileInputStream("input.json");
+			// Read the file into an Object (The type of this object will be a List, Map, String, Boolean,
+			// Number or null depending on the root object in the file).
+			Object jsonObject = JsonUtils.fromString(this.mainJsonString);
+			// Create a context JSON map containing prefixes and definitions
+			//Map context = new HashMap();
+			// Customise context...
+			// Create an instance of JsonLdOptions with the standard JSON-LD options
+			JsonLdOptions options = new JsonLdOptions();
+			// Customise options...
+			// Call whichever JSONLD function you want! (e.g. compact)
+			Object compact=null; 
+			switch (out) {
+				case "expand":
+					compact = JsonLdProcessor.expand(jsonObject, options);
+					break;
+				case "compact":
+					compact = JsonLdProcessor.compact(jsonObject, new HashMap(), options);
+					break;
+			}
+			
+			// Print out the result (or don't, it's your call!)
+			System.out.println("algorithm result  " +out+" --> "+JsonUtils.toString(compact));
+			return JsonUtils.toString(compact);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} 
+	}
+
 }
--- middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ValidatorTest.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ValidatorTest.java
@@ -6,13 +6,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map.Entry;
 
 import org.junit.Test;
 import org.universAAL.middleware.serialization.json.grammar.ContextDefinition;
 import org.universAAL.middleware.serialization.json.grammar.JSONLDDocument;
 
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.MalformedJsonException;
 
@@ -49,7 +55,6 @@ public class ValidatorTest {
 		System.out.println(status);
 		assertTrue(status);
 	}
-	
 
 	/**
 	 * to test if the json has not a correct structure (missing close bracket)
@@ -69,9 +74,89 @@ public class ValidatorTest {
 		}
 		
 	}
+	@Test
+	public void GsonTest() throws Exception {
+		JsonParser parser = new JsonParser();
+		JsonObject obj ;
+		String json = "{\r\n" + 
+				"  \"@context\": {\r\n" + 
+				"    \"ical\": \"http://www.w3.org/2002/12/cal/ical#\",\r\n" + 
+				"    \"xsd\": \"http://www.w3.org/2001/XMLSchema#\",\r\n" + 
+				"    \"ical:dtstart\": {\r\n" + 
+				"      \"@type\": \"xsd:dateTime\"\r\n" + 
+				"    },\r\n" + 
+				"    \"chango\":{\"@id\":\"http://cango.com.ar\",\"@type\":\"@id\"}\r\n" + 
+				"    \r\n" + 
+				"  },\r\n" + 
+				"  \"chango\":{ \r\n" + 
+				"    \"ical:summary\": \"Lady Gaga Concert\",\r\n" + 
+				"  \"ical:location\": \"New Orleans Arena, New Orleans, Louisiana, USA\",\r\n" + 
+				"  \"ical:dtstart\": \"2011-04-09T20:00:00Z\"\r\n" + 
+				"  }\r\n" + 
+				"  \r\n" + 
+				"}";
+		obj = (JsonObject)parser.parse(json);
+		System.out.println(obj);
+		obj.remove("@context");
+		System.out.println(obj);
+		System.out.println(obj.isJsonObject());
+		System.out.println(obj.get("chango"));
+		obj.entrySet().stream().forEach(l->System.out.println(l+"\n"));
+	}
 	
-
+	@Test
+	public void libTest() {
+		try {
+			
+				// Open a valid json(-ld) input file
+				//InputStream inputStream = new FileInputStream("input.json");
+				// Read the file into an Object (The type of this object will be a List, Map, String, Boolean,
+				// Number or null depending on the root object in the file).
+				Object jsonObject = JsonUtils.fromInputStream(this.getClass().getClassLoader().getResource("example.json").openStream());
+				// Create a context JSON map containing prefixes and definitions
+				//Map context = new HashMap();
+				// Customise context...
+				// Create an instance of JsonLdOptions with the standard JSON-LD options
+				JsonLdOptions options = new JsonLdOptions();
+				// Customise options...
+				// Call whichever JSONLD function you want! (e.g. compact)
+				Object compact = JsonLdProcessor.compact(jsonObject,new HashMap(),options);
+				Object expand = JsonLdProcessor.expand(jsonObject, options);
+				// Print out the result (or don't, it's your call!)
+				//System.out.println("algorithm result compacted --> "+JsonUtils.toString(compact));
+				System.out.println("algorithm result expanded --> "+JsonUtils.toString(expand));
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void compactedTest() {
+		try {
+			InputStream is = this.getClass().getClassLoader().getResource("compacted.json").openStream();
+			JSONLDDocument doc = new JSONLDDocument(is);
+			is.close();
+			assertTrue(doc.validate());	
+		} catch (Exception e) {
+			
+			assertFalse(Boolean.FALSE);
+			e.printStackTrace();
+		}
+		
+	}
 	
-
-
+	@Test
+	public void expandedTest() {
+		try {
+			InputStream is = this.getClass().getClassLoader().getResource("expanded.json").openStream();
+			JSONLDDocument doc = new JSONLDDocument(is);
+			is.close();
+			assertTrue(doc.validate());	
+		} catch (Exception e) {
+			
+			assertFalse(Boolean.FALSE);
+			e.printStackTrace();
+		}
+	}
 }
