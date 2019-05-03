caf938cc92f78b4d428a00aeab137f99b52da18d
Eduardo <ebuhid@lst.tfo.upm.es>
2019-05-03 14:10:49 +0200

    working in algorithm to expand json ld terms

--- middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/algorithms/ExpandJsonLD.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/algorithms/ExpandJsonLD.java
@@ -29,23 +29,89 @@ public class ExpandJsonLD {
 		this.expandedJsonLD = new JsonArray();
 	}
 	
-	public JsonArray expand() {
+	private String expandElement (String activeContext,String activePropertie, JsonElement elementToExpand, Object flagExpansion) {
+		if(elementToExpand == null)
+			return  null;
+		if(elementToExpand.isJsonPrimitive()) {
+			if(activePropertie.equals(JsonLdKeyword.GRAPH) || activePropertie ==null ) {
+				return null;
+			}
+		}
+		
+		if(elementToExpand.isJsonArray()) {
+			JsonArray aux_result = new JsonArray();
+			Object expanded_item = this.expandElement(activeContext, activePropertie, elementToExpand, flagExpansion);
+			if(activePropertie.equals(JsonLdKeyword.LIST.toString())) {
+				//the expanded item must not be an array or list object (a list object is a JSONObject that has a @list member)
+			}
+			if(expanded_item instanceof JsonArray) {
+				//apend it items to result
+			}
+			
+			
+		}
+		
+		//if isnt any of above cases, its a json object
+		if(elementToExpand.isJsonObject()) {
+			//element is a dictionary
+			JsonObject aux = elementToExpand.getAsJsonObject();
+			if(aux.has(JsonLdKeyword.CONTEXT.toString())) {
+				//update active context "merging" using the appropiate algorithm
+			}else {
+				Object result_dictionary = new Object();
+			
+				for (Map.Entry<String, JsonElement> element : elementToExpand.getAsJsonObject().entrySet()) {
+					
+						String expanded_prop = this.expandIRI(element);
+						String  expaded_value = this.expandValue();
+						if(expanded_prop != null || expanded_prop.contains(":") || JsonLdKeyword.isKeyword(expanded_prop)) {
+							if(JsonLdKeyword.isKeyword(expanded_prop)) {
+								if(activePropertie.equals(JsonLdKeyword.REVERSE.toString())) {
+									//TOOD throw error "invalid_reverse_rpoperty_map"
+								}
+								if(expanded_prop.equals(JsonLdKeyword.TYPE.toString()) && !element.getValue().isJsonPrimitive() && !element.getValue().isJsonArray() ) {
+									//throw error
+								}
+								if(expanded_prop.equals(JsonLdKeyword.VALUE.toString()) && (!element.getValue().isJsonPrimitive() || !element.getValue().isJsonNull()) ) {
+								//throw error	
+								}
+								if(expanded_prop.equals(JsonLdKeyword.LANG.toString())  &&  !element.getValue().isJsonPrimitive()) {
+									//throw error
+								}
+								if(expanded_prop.equals(JsonLdKeyword.INDEX.toString()) && !element.getValue().isJsonPrimitive()) {
+									//throw error
+								}
+								if(expanded_prop.equals(JsonLdKeyword.LIST.toString())) {
+									if(activePropertie ==null || activePropertie.equals(JsonLdKeyword.GRAPH.toString())) {
+										//remove the free floating flag and continue 
+									}else {
+										expaded_value=this.expandValue();
+										if(parser.parse(expaded_value).isJsonObject() && parser.parse(expaded_value).getAsJsonObject().has(JsonLdKeyword.LIST.toString())) {
+											//throw list of lsit error
+										}
+									}
+								}
+								if(expanded_prop.equals(JsonLdKeyword.SET.toString())) {
+									expaded_value = this.expandElement(activeContext, activePropertie, elementToExpand, flagExpansion);
+ 								}
+								
+								if(expanded_prop.equals(JsonLdKeyword.REVERSE.toString()) && !(parser.parse(expaded_value).isJsonObject() && parser.parse(expaded_value).getAsJsonObject().has(JsonLdKeyword.LIST.toString()))  ) {
+									
+								}
+								
+							
+							}
+						}
+						
+				}
+			}
+			
+			
+			
+		}
+			
 		this.initJSONLDDoc();
-		if(this.mainJson==null)
-			return null;
-		JsonObject context= this.mainJson.get(JsonLdKeyword.CONTEXT.toString()).getAsJsonObject();
-		this.activeContext = new ContextDefinition(context);
-		if(this.activeContext.validate()) {
-			//TODO add logging system
-			System.out.println("conext invalid...");
-			return null;
-		}
-		
-		for (Entry<String, JsonElement> jsonElement : this.mainJson.entrySet()) {
-			if (termExpansion(this.activeContext,jsonElement)) return null;
-		}
-		
-		return this.expandedJsonLD;
+		return null;
 	}
 	
 	/**
@@ -83,4 +149,16 @@ public class ExpandJsonLD {
 		}
 		
 	}
+	/**
+	 * method to expand value to absoluteIRI using activeContext
+	 * @return
+	 */
+	private String expandIRI(Entry<String, JsonElement>  jse) {
+		return null;
+	}
+	
+	private String expandValue() {
+		return null;
+	}
+	
 }
