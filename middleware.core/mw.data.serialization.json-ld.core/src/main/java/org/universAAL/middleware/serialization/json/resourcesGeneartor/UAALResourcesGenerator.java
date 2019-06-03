44d025fb094fca63c0b489a45264b04083924740
Eduardo <ebuhid@lst.tfo.upm.es>
2019-06-03 18:42:53 +0200

    minimal updates

--- middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/resourcesGeneartor/UAALResourcesGenerator.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/resourcesGeneartor/UAALResourcesGenerator.java
@@ -1,12 +1,14 @@
 package org.universAAL.middleware.serialization.json.resourcesGeneartor;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
 import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.serialization.json.JsonLdKeyword;
+import org.universAAL.middleware.serialization.json.algorithms.ExpandJSONLD;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -14,32 +16,42 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class UAALResourcesGenerator {
-	private JsonObject jsonToExpand;
+	private JsonObject mainjJson;
+	private JsonArray expandedJson;
 	private JsonParser parser = new JsonParser();
 	private HashMap<String , Resource> generatedResources = new HashMap<String, Resource>();
-	
+	/**
+	 * get {@link InputStream} or {@link JsonObject} or {@link String} input json to extract {@link Resource} objects
+	 * @param jsonToExpand
+	 */
 	public UAALResourcesGenerator(Object jsonToExpand) {
+		
 		if(jsonToExpand instanceof InputStream) {
 			String jsonString = "";
 			Scanner s = new Scanner((InputStream)jsonToExpand);
 			s.useDelimiter("\\A");
 			jsonString = s.hasNext() ? s.next() : "";
 			s.close();
-			this.jsonToExpand = parser.parse(jsonString).getAsJsonObject();
+			this.mainjJson = parser.parse(jsonString).getAsJsonObject();
 		}
 		
 		if(jsonToExpand instanceof JsonObject) {
-			this.jsonToExpand = (JsonObject) jsonToExpand;
+			this.mainjJson = (JsonObject) jsonToExpand;
+		}
+		
+		if(jsonToExpand instanceof JsonArray) {
+			//this.mainjJson = (JsonObject) jsonToExpand;
+			//TODO implement
 		}
 		
 		if(jsonToExpand instanceof String) {
-			this.jsonToExpand = parser.parse((String)jsonToExpand).getAsJsonObject();
+			this.mainjJson = parser.parse((String)jsonToExpand).getAsJsonObject();
 		}
 	}
 	
 	public void generateResources() {
-		
-		for (Entry<String, JsonElement> element : jsonToExpand.entrySet()) {
+		this.expandedJson=this.expandJson(mainjJson);
+		for (Entry<String, JsonElement> element : expandedJson.iterator().next().getAsJsonObject().entrySet()) {
 			
 			this.walkGraph(element);
 			
@@ -48,16 +60,38 @@ public class UAALResourcesGenerator {
 	}
 	
 	private Resource walkGraph(Entry<String, JsonElement> toAnalyze) {
+		String resource_id=null;
+		ArrayList<String> resourceTypesArray = new ArrayList<String>();
 		
-		JsonElement main_resource_uri=null;
+		
+		
+		
+		
 		if(toAnalyze.getValue().isJsonObject()) {
 			JsonElement resource_uri;
 			Resource local_res;
 			JsonObject aux =toAnalyze.getValue().getAsJsonObject();
-			resource_uri = aux.get(JsonLdKeyword.ID.toString());//maybe hasnt ID key
+			
+			/*
+			if(aux.has(JsonLdKeyword.ID.toString())) {
+				local_res = new Resource( aux.get(JsonLdKeyword.ID.toString()).getAsJsonPrimitive().getAsString() );
+			}
+			*/
+			
 			for (Entry<String, JsonElement> iterable_element : toAnalyze.getValue().getAsJsonObject().entrySet()) {
 				if(iterable_element.getValue().isJsonObject()) {
 					local_res=this.walkGraph(iterable_element);
+				}else {
+					//may be here start to build the resource
+					if(iterable_element.getValue().isJsonPrimitive()) {
+						String primitive = iterable_element.getKey();
+						if(primitive.equals(JsonLdKeyword.ID.toString())) {
+							local_res = new Resource(iterable_element.getValue().getAsJsonPrimitive().getAsString());
+						}
+						if(primitive.equals(JsonLdKeyword.VALUE.toString())) {
+							//local_res.setProperty(propURI, value)
+						}
+					}
 					
 				}
 			}
@@ -65,23 +99,43 @@ public class UAALResourcesGenerator {
 		}
 		
 		if(toAnalyze.getValue().isJsonArray()) {
-			JsonElement resource_uri;
-			for (int i = 0; i < toAnalyze.getValue().getAsJsonArray().size(); i++) {
-				
-				for (Entry<String, JsonElement> iterable_element : toAnalyze.getValue().getAsJsonObject().entrySet()) {
-					this.walkGraph(iterable_element);
+			//list detected
+			if(toAnalyze.getKey().equals(JsonLdKeyword.TYPE.toString())) {
+				//add all types to local resources array
+				for (JsonElement element : toAnalyze.getValue().getAsJsonArray()) {
+					//no need to control if is json element (should be)
+					resourceTypesArray.add(element.getAsJsonPrimitive().getAsString());
 				}
+			}else {
+				
+				for (JsonElement element : toAnalyze.getValue().getAsJsonArray()) {
+					for (Entry<String, JsonElement> item : element.getAsJsonObject().entrySet()) {
+						this.walkGraph(item);
+					}
+					
+				}
+				
+				
 			}
+			
+			
 		}
 		
 		if(toAnalyze.getValue().isJsonPrimitive()) {
-			JsonElement resource_uri;
-			//create resource
-			//resource IRI: the ID key contained into the object
-			Resource res = new Resource(main_resource_uri.getAsJsonPrimitive().getAsString());
-			res.setProperty(toAnalyze.getKey(),toAnalyze.getValue().getAsJsonPrimitive().getAsString());
+
+			if(toAnalyze.getKey().equals(JsonLdKeyword.ID.toString())) {
+				//element ID detected
+				resource_id = toAnalyze.getValue().getAsJsonPrimitive().getAsString();
+			}
+		
 		}
 	return null;	
 	}
 
+	
+	private JsonArray expandJson(JsonObject toExpand) {
+		ExpandJSONLD jsonExpansor = new ExpandJSONLD(toExpand);
+		jsonExpansor.expand();
+		return jsonExpansor.getExpandedJson();
+	}
 }
