a67f78bf539a6be8a08a1ee417c64c18341575e8
eduardo <ebuhid@lst.tfo.upm.es>
2019-06-03 14:44:27 +0200

    added test methods

--- middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
@@ -1,9 +1,11 @@
 package org.universAAL.middleware.serialization.json;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.InputStream;
 
-import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
 import org.junit.Test;
 import org.universAAL.middleware.serialization.json.algorithms.ExpandJSONLD;
 
@@ -11,10 +13,59 @@ import org.universAAL.middleware.serialization.json.algorithms.ExpandJSONLD;
 
 public class ExpansionTest {
 
-	@Test
-	public void expand() {
-		
+	
+	@Before
+	public void init() {
 		PropertyConfigurator.configure("src/test/resources/logj4ConfigFile/log4j.properties");
+		
+	}
+	
+	@Test
+	public void simpleMapping() {
+		//testing "key":"value" in context
+		String expected ="[{'http://schema.org/name':[{'@value':'TheEmpireStateBuilding'}]}]";
+		try {
+			InputStream json =  this.getClass().getClassLoader().getResource("./expand/simpleKV.json").openStream();
+			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			expansor.expand();
+			assertTrue(expansor.getExpandedJson().toString().replaceAll("\"", "'").replaceAll(" " , "").equals(expected));
+			System.out.println(expansor.getExpandedJson());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void complexMapping() {
+		//testing "key":{....} in contex
+		
+
+		try {
+			InputStream json =  this.getClass().getClassLoader().getResource("./expand/complexKV.json").openStream();
+			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			expansor.expand();
+			System.out.println(expansor.getExpandedJson());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	} 
+	
+	
+	@Test
+	public void typeAsIDTest() {
+		try {
+			InputStream json =  this.getClass().getClassLoader().getResource("./expand/typeID.json").openStream();
+			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			expansor.expand();
+			System.out.println(expansor.getExpandedJson());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void fullExpand() {
 
 		try {
 			InputStream json =  this.getClass().getClassLoader().getResource("./expand/complex.json").openStream();
