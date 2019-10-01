2a369edc749d7aefb377fc9cd59a86e55cb105b3
Eduardo Buhid <ebuhid@lst.tfo.upm.es>
2019-10-01 09:44:08 +0200

    added test method

--- middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
@@ -137,6 +137,20 @@ public class ExpansionTest {
 		
 	}
 	
+	@Test
+	public void expandedJsonTest() {
+		 String jsonFromturtle ="[{\"@id\":\"_:b0\",\"@type\":[\"http://ontology.universAAL.org/Context.owl#ContextEventPattern\"],\"http://www.w3.org/2000/01/rdf-schema#subClassOf\":[{\"@id\":\"_:b1\"},{\"@id\":\"_:b2\"}]},{\"@id\":\"_:b1\",\"@type\":[\"http://www.w3.org/2002/07/owl#Restriction\"],\"http://www.w3.org/2002/07/owl#allValuesFrom\":[{\"@id\":\"http://ontology.universAAL.org/Profile.owl#User\"}],\"http://www.w3.org/2002/07/owl#onProperty\":[{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#subject\"}]},{\"@id\":\"_:b2\",\"@type\":[\"http://www.w3.org/2002/07/owl#Restriction\"],\"http://www.w3.org/2002/07/owl#allValuesFrom\":[{\"@id\":\"http://ontology.universaal.org/Health.owl#PerformedSession\"}],\"http://www.w3.org/2002/07/owl#onProperty\":[{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#object\"}]},{\"@id\":\"_:b3\",\"http://www.w3.org/1999/02/22-rdf-syntax-ns#first\":[{\"@id\":\"_:b0\"}],\"http://www.w3.org/1999/02/22-rdf-syntax-ns#rest\":[{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#nil\"}]},{\"@id\":\"http://ontology.universAAL.org/Context.owl#ContextEventPattern\"},{\"@id\":\"http://ontology.universAAL.org/Context.owl#ContextProvider\"},{\"@id\":\"http://ontology.universAAL.org/Context.owl#ContextProviderType\"},{\"@id\":\"http://ontology.universAAL.org/Context.owl#gauge\",\"@type\":[\"http://ontology.universAAL.org/Context.owl#ContextProviderType\"]},{\"@id\":\"http://ontology.universAAL.org/Profile.owl#User\",\"@type\":[\"http://www.w3.org/2002/07/owl#Class\"]},{\"@id\":\"http://ontology.universaal.org/Health.owl#PerformedSession\",\"@type\":[\"http://www.w3.org/2002/07/owl#Class\"]},{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#nil\"},{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#object\"},{\"@id\":\"http://www.w3.org/1999/02/22-rdf-syntax-ns#subject\"},{\"@id\":\"http://www.w3.org/2002/07/owl#Class\"},{\"@id\":\"http://www.w3.org/2002/07/owl#Restriction\"},{\"@id\":\"https://rest.activage.lst.tfo.upm.es/uaal/spaces/equimetrix/context/publishers/backup\",\"http://ontology.universAAL.org/Context.owl#myClassesOfEvents\":[{\"@id\":\"_:b3\"}],\"@type\":[\"http://ontology.universAAL.org/Context.owl#ContextProvider\"],\"http://ontology.universAAL.org/Context.owl#hasType\":[{\"@id\":\"http://ontology.universAAL.org/Context.owl#gauge\"}]}]";
+			try {
+				//InputStream json =  this.getClass().getClassLoader().getResource("./expand/multipleType.json").openStream();
+				ExpandJSONLD expansor = new ExpandJSONLD(jsonFromturtle);
+				expansor.expand();
+				System.out.println(expansor.getExpandedJson());
+			} catch (Exception e) {
+				e.printStackTrace();
+				assertTrue(false);
+
+			}
+	}
 	private boolean compareJsons(JsonArray expected,JsonArray given) {
 		if(!expected.equals(given)) {
 			System.out.println("expected \n"+expected);
