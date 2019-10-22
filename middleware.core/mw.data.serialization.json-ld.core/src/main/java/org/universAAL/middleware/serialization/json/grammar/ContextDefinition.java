c274cfddbb35019219300c9c38bd4f835b2b27ce
Buhid Eduardo <ebuhid@lst.tfo.upm.es>
2019-10-22 15:48:04 +0200

    updated merge context method

--- middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/grammar/ContextDefinition.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/main/java/org/universAAL/middleware/serialization/json/grammar/ContextDefinition.java
@@ -17,6 +17,7 @@ package org.universAAL.middleware.serialization.json.grammar;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map.Entry;
@@ -25,12 +26,12 @@ import java.util.Scanner;
 import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.serialization.json.JSONLDSerialization;
 import org.universAAL.middleware.serialization.json.JsonLdKeyword;
-import org.universAAL.middleware.serialization.json.algorithms.ExpandJSONLD;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 /**
  * @author amedrano
@@ -38,11 +39,8 @@ import com.google.gson.JsonParser;
  */
 public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<String, JsonElement> > {
 	private JsonObject jsonToValidate = null;
-	private JsonArray contextToMerge = null;
-	private JsonParser parser = new JsonParser();
 	private static final String BLANK_NODE ="_:";
-	private boolean state = false;
-	private ContextDefinition lastContext=null;
+	private boolean isValid=true;
 
 /**
  * 
@@ -92,13 +90,7 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
  */
 	public boolean validate() {
 		
-		if(this.contextToMerge!=null) {
-			
-			LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "keyControl", "merging contexts");
-
-			this.jsonToValidate = mergeContexts(this.contextToMerge);
-		}
-		 
+		
 		if(this.jsonToValidate!=null) {
 					for (Entry<String, JsonElement> element : this.jsonToValidate.entrySet()) {
 						//keyword control
@@ -106,15 +98,12 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
 						if( !this.keyControl(element) ) return false;
 						if( !this.valueOfKeyControl(element) ) return false;
 					}	
-					
-				
-
 		}else {
-			//TODO use logging system 
 			LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "keyControl", "null json to validate...");
 			return false;
 		}
-		return true;
+		this.isValid = true;
+		return this.isValid;
 	}
 
 
@@ -167,17 +156,17 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
 			return true;
 	}
 	//TODO a term defined in the active context expanding into an absolute IRI, or an array of any of these.
-	public boolean ActiveContextTermControl(JsonElement candidate) {
-		for (Entry<String, JsonElement> element : this.jsonToValidate.entrySet()) {
-			if(candidate.isJsonPrimitive() || candidate.isJsonArray()) {
-				//this.jsonToValidate.has(element.getKey());
-			}else {
-				//throw error
-				return false;
-			}
-		}
-		return true;
-	}
+//	public boolean ActiveContextTermControl(JsonElement candidate) {
+//		for (Entry<String, JsonElement> element : this.jsonToValidate.entrySet()) {
+//			if(candidate.isJsonPrimitive() || candidate.isJsonArray()) {
+//				//this.jsonToValidate.has(element.getKey());
+//			}else {
+//				//throw error
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
 
 	public boolean keyControl(Entry<String, JsonElement> element) {
 		if( ! IRI.isAbsolute(element.getKey().toString())) {
@@ -197,23 +186,41 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
 		return true;
 	}
 	/**
-	 * method to merge contexts grouped in array
+	 * method to merge contexts. This method will append to current json loaded all of items from new context
 	 * @param ToMerge
 	 * @return {@link JsonObject} representing the merged context
 	 */
-	public static  JsonObject mergeContexts(JsonArray ToMerge) {
-		JsonObject obj = new JsonObject();
-		if(ToMerge ==null) return null;
-		for (JsonElement item : ToMerge) {
-			if(item.isJsonObject()) {
-				for (Entry<String, JsonElement> jsonElement : item.getAsJsonObject().entrySet()) {
-					obj.add(jsonElement.getKey(),jsonElement.getValue());
-				}
-			}else
-				return null;
+	public boolean mergeContexts(JsonElement toMerge) {
+		LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "mergeContexts", "merging contexts");
+		JsonObject aux =null;
+		if(toMerge ==null) {
+			LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "mergeContexts", "null context to merge");
+			return false;
+		} 
+		if(toMerge instanceof JsonObject) {
+			aux = toMerge.getAsJsonObject();
+		}
+		if(toMerge instanceof JsonPrimitive) {
+			//remote context given
+			try {
+				ContextDefinition contextToMerge = new ContextDefinition(new URL(toMerge.getAsJsonPrimitive().getAsString()));
+				contextToMerge.validate();
+				if(!contextToMerge.isValid()) {
+					LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "mergeContexts", "Trying to merge invalid context");
+					return false; 
+				} 
+			} catch (MalformedURLException e) {
+				LogUtils.logDebug(JSONLDSerialization.owner, ContextDefinition.class, "mergeContexts", e.getLocalizedMessage());
+				e.printStackTrace();
+				return false;
+			}
+		}
+		
+		for (Entry<String, JsonElement> item : aux.entrySet()) {
+			this.jsonToValidate.add(item.getKey(), item.getValue());
 		}
 
-		return obj;
+		return true;
 	}
 
 	/**
@@ -248,9 +255,9 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
 	}
 	
 
-	public void updateElement(String key, JsonElement value) {
-		this.jsonToValidate.add(key, value);
-	}
+//	public void updateElement(String key, JsonElement value) {
+//		this.jsonToValidate.add(key, value);
+//	}
 
 	public JsonElement getTermValue(String term) {
 		
@@ -267,6 +274,9 @@ public class ContextDefinition implements JSONLDValidator, KeyControl<Entry<Stri
 		return jsonToValidate;
 	}
 
+	public boolean isValid() {
+		return this.isValid;
+	}
 	
 
 }
