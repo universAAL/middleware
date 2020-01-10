2b5cc100fe56a022eb2261e39a98ac842c4f1b7c
Buhid Eduardo <ebuhid@lst.tfo.upm.es>
2020-01-10 11:43:55 +0100

    updated test code

--- middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
+++ middleware.core/mw.data.serialization.json-ld.core/src/test/java/org/universAAL/middleware/serialization/json/ExpansionTest.java
@@ -12,10 +12,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
 public class ExpansionTest {
-
-	JsonParser parser;
-	JsonArray expected_json;
-	String expected;
+	private InputStream json;
+	private JsonParser parser;
+	private JsonArray expected_json;
+	private String expected;
+	private ExpandJSONLD expansor;
 
 	@Before
 	public void init() {
@@ -29,8 +30,8 @@ public class ExpansionTest {
 		 expected ="[{'http://schema.org/name':[{'@value':'The Empire State Building'}]}]";
 		try {
 			expected_json = parser.parse(expected).getAsJsonArray();
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/simpleKV.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/simpleKV.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(expected_json,expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -44,8 +45,8 @@ public class ExpansionTest {
 		 expected ="[{\"http://schema.org/geo\":[{\"http://schema.org/latitude\":[{\"@value\":\"40.75\",\"@type\":\"xsd:float\"}],\"http://schema.org/longitude\":[{\"@value\":\"73.98\",\"@type\":\"xsd:float\"}]}]}]\r\n";
 		try {
 			expected_json= parser.parse(expected).getAsJsonArray();
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/complexKV.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json = this.loadJson("./expand/complexKV.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(expected_json,expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -60,8 +61,8 @@ public class ExpansionTest {
 	public void typeAsIDTest() {
 		expected ="[{\"http://schema.org/image\":[{\"@id\":\"http://www.civil.usherbrooke.ca/cours/gci215a/empire-state-building.jpg\"}]}]";
 		try {
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/typeID.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/typeID.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -76,8 +77,8 @@ public class ExpansionTest {
 	public void fullExpand() {
 			expected ="[ { \"@id\": \"http://example.org/cars/for-sale#tesla\", \"@type\": [ \"http://purl.org/goodrelations/v1#Offering\" ], \"http://purl.org/goodrelations/v1#acceptedPaymentMethods\": [ { \"@id\": \"http://purl.org/goodrelations/v1#Cash\" } ], \"http://purl.org/goodrelations/v1#description\": [ { \"@value\": \"Need to sell fast and furiously\" } ], \"http://purl.org/goodrelations/v1#hasBusinessFunction\": [ { \"@id\": \"http://purl.org/goodrelations/v1#Sell\" } ], \"http://purl.org/goodrelations/v1#hasPriceSpecification\": [ { \"http://purl.org/goodrelations/v1#hasCurrency\": [ { \"@value\": \"USD\" } ], \"http://purl.org/goodrelations/v1#hasCurrencyValue\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#float\", \"@value\": \"85000\" } ] } ], \"http://purl.org/goodrelations/v1#includes\": [ { \"@type\": [ \"http://purl.org/goodrelations/v1#Individual\", \"http://www.productontology.org/id/Vehicle\" ], \"http://xmlns.com/foaf/0.1/page\": [ { \"@id\": \"http://www.teslamotors.com/roadster\" } ], \"http://purl.org/goodrelations/v1#name\": [ { \"@value\": \"Tesla Roadster\" } ] } ], \"http://purl.org/goodrelations/v1#name\": [ { \"@value\": \"Used Tesla Roadster\" } ] } ]";
 		try {
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/complex2.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/complex2.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -91,8 +92,8 @@ public class ExpansionTest {
 	public void arrayTest() {
 		expected = "[ { \"@type\": [ \"http://ontology.universAAL.org/Context.owl#ContextProvider\" ], \"http://rdf.data-vocabulary.org/#ingredients\": [ { \"@value\": \"12 fresh mint leaves\" }, { \"@value\": \"1/2 lime, juiced with pulp\" }, { \"@value\": \"1 tablespoons white sugar\" }, { \"@value\": \"1 cup ice cubes\" }, { \"@value\": \"2 fluid ounces white rum\" }, { \"@value\": \"1/2 cup club soda\" } ], \"http://rdf.data-vocabulary.org/#instructions\": [ { \"http://rdf.data-vocabulary.org/#description\": [ { \"@value\": \"Crush lime juice, mint and sugar together in glass.\" } ], \"http://rdf.data-vocabulary.org/#step\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#integer\", \"@value\": 1 } ] }, { \"http://rdf.data-vocabulary.org/#description\": [ { \"@value\": \"Fill glass to top with ice cubes.\" } ], \"http://rdf.data-vocabulary.org/#step\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#integer\", \"@value\": 2 } ] }, { \"http://rdf.data-vocabulary.org/#description\": [ { \"@value\": \"Pour white rum over ice.\" } ], \"http://rdf.data-vocabulary.org/#step\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#integer\", \"@value\": 3 } ] }, { \"http://rdf.data-vocabulary.org/#description\": [ { \"@value\": \"Fill the rest of glass with club soda, stir.\" } ], \"http://rdf.data-vocabulary.org/#step\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#integer\", \"@value\": 4 } ] }, { \"http://rdf.data-vocabulary.org/#description\": [ { \"@value\": \"Garnish with a lime wedge.\" } ], \"http://rdf.data-vocabulary.org/#step\": [ { \"@type\": \"http://www.w3.org/2001/XMLSchema#integer\", \"@value\": 5 } ] } ], \"http://rdf.data-vocabulary.org/#name\": [ { \"@value\": \"Mojito\" } ], \"http://rdf.data-vocabulary.org/#yield\": [ { \"@value\": \"1 cocktail\" } ] } ] ";
 	try {
-		InputStream json =  this.getClass().getClassLoader().getResource("./example.json").openStream();
-		ExpandJSONLD expansor = new ExpandJSONLD(json);
+		json =  this.loadJson("./example.json");
+		expansor = new ExpandJSONLD(json);
 		expansor.expand();
 		assertTrue(compareJsons(parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 		System.out.println(expansor.getExpandedJson());
@@ -106,8 +107,8 @@ public class ExpansionTest {
 	public void compactIRITest() {
 		expected="[ { \"@id\": \"http://example.org/cars/for-sale#tesla\", \"@type\": [ \"http://purl.org/goodrelations/v1/Offering\" ], \"http://purl.org/goodrelations/v1/hasBusinessFunction\": [ { \"@id\": \"http://purl.org/goodrelations/v1/Sell\" } ] } ]";
 		try {
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/compactIRI.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/compactIRI.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons( parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -123,8 +124,8 @@ public class ExpansionTest {
 	public void UAALMessajeExpandTest() {
 		expected="[ { \"@id\": \"urn:org.universAAL.middleware.context.rdf:ContextEvent#_:c0a8012ac02c0b4c:58d\", \"@type\": [ \"http://ontology.universAAL.org/Context.owl#ContextProvider\" ], \"http://ontology.universAAL.org/Context.owl#hasType\": [ { \"@id\": \"http://ontology.universAAL.org/Context.owl#gauge\", \"@type\": [ \"http://ontology.universAAL.org/Context.owl#ContextProviderType\" ] } ] } ] ";
 		try {
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/UAALmessageExample1.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/UAALmessageExample1.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -139,8 +140,8 @@ public class ExpansionTest {
 	public void multipleTypesTest() {
 		this.expected="[ { \"http://purl.org/goodrelations/v1#includes\": [ { \"@type\": [ \"http://purl.org/goodrelations/v1#Individual\", \"http://www.productontology.org/id/Vehicle\" ] } ] } ]";
 		try {
-			InputStream json =  this.getClass().getClassLoader().getResource("./expand/multipleType.json").openStream();
-			ExpandJSONLD expansor = new ExpandJSONLD(json);
+			json =  this.loadJson("./expand/multipleType.json");
+			expansor = new ExpandJSONLD(json);
 			expansor.expand();
 			assertTrue(compareJsons(parser.parse(expected).getAsJsonArray(),expansor.getExpandedJson()));
 			System.out.println(expansor.getExpandedJson());
@@ -152,13 +153,18 @@ public class ExpansionTest {
 		
 	}
 	
-
+	private InputStream loadJson(String path) throws Exception {
+		return this.getClass().getClassLoader().getResource(path).openStream();
+	}
+	
 	private boolean compareJsons(JsonArray expected,JsonArray given) {
-		if(!expected.equals(given)) {
+		boolean v =expected.equals(given);
+	
+		if(!v) {
 			System.out.println("expected \n"+expected);
 			System.out.println("given \n"+given);	
 		}
-		return expected.equals(given);
+		return v;
 	}
 	
 	
